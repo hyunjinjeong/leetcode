1d061434895ab5d665fbe01b9f6ffe0fe61b6aeb
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-08-27 16:06:37 +0900

    Time: 118 ms (72.12%), Space: 20.6 MB (97.51%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,14 +1,25 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def get_one_count(num):
-            count = 0
-            while num:
-                num &= num-1
-                count += 1
-            return count
+#         def get_one_count(num):
+#             count = 0
+#             while num:
+#                 num &= num-1
+#                 count += 1
+#             return count
         
-        result = []
-        for num in range(n+1):
-            result.append(get_one_count(num))
+#         result = []
+#         for num in range(n+1):
+#             result.append(get_one_count(num))
         
-        return result
+#         return result
+    
+        # DP도 되고.. bit도 되는데 bit도 DP 형태로 할 수 있음.
+        # 일단 엣지 케이스.
+        answer = [0]
+        # 그 담에 계산
+        for num in range(1, n+1):
+            if num&1:
+                answer.append(answer[num >> 1] + 1)
+            else:
+                answer.append(answer[num >> 1])
+        return answer
