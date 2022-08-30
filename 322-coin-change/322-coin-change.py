5dd83b36c4c9bce8bf97d7c28405b20c4da721c8
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-08-30 22:40:22 +0900

    Time: 3860 ms (7.71%), Space: 14.2 MB (62.01%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,6 +3,8 @@ class Solution:
         dp = [amount+1] * (amount+1)
         dp[0] = 0
         
+        # dp[i]가 i까지의 최솟값이라고 하면, 밑에서부터 구해져 있으니까
+        # 코인을 돌면서 최솟값을 찾아서 +1을 더하면 됨.
         for i in range(1, amount+1):
             for coin in coins:
                 if i - coin < 0:
