46dc4a4389096e23fa4f17e1bad634f9421e8328
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-09-07 22:02:36 +0900

    Time: 56 ms (33.06%), Space: 13.8 MB (97.22%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -17,11 +17,21 @@ class Solution:
         
 #         return max(dp)
 
-        # 위에꺼 좀 더 최적화해보면 저 중간에 j for문을 없앨 수 있을 듯
-        dp = [0] * (len(nums)+1)
-        dp[1] = nums[0]
+#         # 위에꺼 좀 더 최적화해보면 저 중간에 j for문을 없앨 수 있을 듯
+#         dp = [0] * (len(nums)+1)
+#         dp[1] = nums[0]
         
-        for i in range(1, len(nums)):
-            dp[i+1] = max(dp[i], dp[i-1] + nums[i])
+#         for i in range(1, len(nums)):
+#             dp[i+1] = max(dp[i], dp[i-1] + nums[i])
         
-        return dp[-1]
+#         return dp[-1]
+    
+        # 또 최적화하면, dp에서 마지막 2개만 쓰고 있으니까 (dp[i], dp[i-1]) 변수 2개로 바꿀 수 있음
+        prev, curr = 0, 0
+        
+        for num in nums:
+            temp = max(curr, prev + num)
+            prev = curr
+            curr = temp
+        
+        return curr
