d07283a599a85351ce4e4e31cb2a4300a9bb0da5
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-09-19 22:33:26 +0900

    Time: 783 ms (13.45%), Space: 16.4 MB (81.11%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,15 +2,15 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         # 그냥 DFS로 하나씩 다 찾으면 되지 않으려나?
         # 1이 나올 때마다 DFS 돌리고 갯수 +1씩 해주면 될 것 같음
-        visited = set()
+        # visited를 없앨 수도 있지 않을까? 한번 방문한 점은 필요 없으니 grid[x][y]를 0으로 만들어버리자.
         directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
         m, n  = len(grid), len(grid[0])
         
         def dfs(x, y):
-            if (x, y) in visited:
+            if grid[x][y] == "0":
                 return
             
-            visited.add((x, y))
+            grid[x][y] = "0"
             for dx, dy in directions:
                 next_x, next_y = x+dx, y+dy
                 if 0 <= next_x < m and 0 <= next_y < n and grid[next_x][next_y] == "1":
@@ -19,7 +19,7 @@ class Solution:
         ans = 0
         for x in range(m):
             for y in range(n):
-                if grid[x][y] == "1" and (x, y) not in visited:
+                if grid[x][y] == "1":
                     ans += 1
                     dfs(x, y)
         
