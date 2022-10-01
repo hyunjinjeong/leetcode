892d676239434cf07b2a8bd68eecb63d7f34d65d
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-10-01 17:29:32 +0900

    Time: 52 ms (50.14%), Space: 13.9 MB (83.01%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -3,29 +3,25 @@ class Solution:
         # 요것도 naive 하게 먼저 가보자
         visited = set()
         
-        total_counts = len(matrix) * len(matrix[0])
+        total_count = len(matrix) * len(matrix[0])
         # R, D, L, U 순서대로 시도.
         moves = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         
-        answer = []
-        
         r, c = 0, 0
         curr_direction = 0
-        while len(answer) < total_counts:
-            # 회전해야 하는 조건들. range 벗어났거나 이미 방문한 경우
-            if r < 0 or r >= len(matrix) or c < 0 or c >= len(matrix[0]) or (r, c) in visited:
-                move = moves[curr_direction]
-                before_r, before_c = r - move[0], c - move[1]
-                
-                curr_direction = (curr_direction + 1) % 4
-                move = moves[curr_direction]
-                r, c = before_r + move[0], before_c + move[1]
-                continue
-            
-            answer.append(matrix[r][c])
-            visited.add((r, c))
-            
+        answer = [matrix[r][c]]
+        visited.add((r, c))
+        while len(answer) < total_count:
             move = moves[curr_direction]
-            r, c = r + move[0], c + move[1]
+            next_r, next_c = r + move[0], c + move[1]
+            
+            # matrix 안에 있고 방문하지 않은 경우. 그대로 가면 됨
+            if 0 <= next_r < len(matrix) and 0 <= next_c < len(matrix[0]) and (next_r, next_c) not in visited:
+                answer.append(matrix[next_r][next_c])
+                visited.add((next_r, next_c))
+                r, c = next_r, next_c
+            # 조건에 안 맞으면 회전해야 됨
+            else:
+                curr_direction = (curr_direction + 1) % 4
             
         return answer
