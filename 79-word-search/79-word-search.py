0666f7368672613d4e0ac3804646d348825d1b49
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-10-02 18:18:32 +0900

    Time: 5803 ms (69.71%), Space: 14 MB (50.81%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,30 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         # 딱 봐도 backtracking 쓰는 문제 같긴 한데...
-        # 아래처럼 하면 TLE가 뜬다.
-        moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        # 방향은 맞았는데 다른 데서 시간을 많이 쓴 듯? moves for문 없애고 set 안 쓰고 하니까 됨.
+        # 근데 제대로 코드 짜도 자꾸 TLE 뜸;
+        self.board = board
+        self.word = word
+        self.found = False
         
-        def dfs(r, c, length):
-            if length == len(word):
-                return True
-            
-            if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]):
-                return False
-            if board[r][c] is True:
-                return False
-            if board[r][c] != word[length]:
-                return False
-            
-            char = board[r][c]
-            board[r][c] = True # visited
-            result = dfs(r+1, c, length+1) or dfs(r-1, c, length+1) or dfs(r, c+1, length+1) or dfs(r, c-1, length+1)
-            board[r][c] = char
-            
-            return result
-
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if dfs(r, c, 0):
+                if self.found:
                     return True
+                self.dfs(r, c, 0)
         
-        return False
+        return self.found
+    
+    def dfs(self, r, c, index):
+        if self.found:
+            return
+        
+        if index == len(self.word):
+            self.found = True
+            return
+
+        if r < 0 or c < 0 or r >= len(self.board) or c >= len(self.board[0]):
+            return
+        if self.board[r][c] != self.word[index]:
+            return
+
+        char = self.board[r][c]
+        self.board[r][c] = "-" # visited
+        self.dfs(r+1, c, index+1)
+        self.dfs(r-1, c, index+1)
+        self.dfs(r, c+1, index+1)
+        self.dfs(r, c-1, index+1)
+        self.board[r][c] = char
