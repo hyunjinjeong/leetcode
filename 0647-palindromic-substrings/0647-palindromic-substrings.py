8d66bbcab14e6bb6e8f6089d13134f7449d55572
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-10-20 22:16:58 +0900

    Time: 787 ms (14.95%), Space: 22 MB (16.78%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -4,10 +4,12 @@ class Solution:
         # dp로 palindrome이면 True로 해서 True의 갯수를 세면 될 것 같은데
         
         dp = [[False] * len(s) for _ in range(len(s))]
+        answer = 0
         
         # 길이 1짜리 초기화
         for i in range(len(s)):
             dp[i][i] = True
+            answer += 1
             
         for end in range(len(s)):
             for start in range(end-1, -1, -1):
@@ -17,11 +19,6 @@ class Solution:
                 # 시작과 끝이 같은 경우.
                 if end - start == 1 or dp[start+1][end-1]:
                     dp[start][end] = True
-        
-        answer = 0
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                if dp[i][j]:
                     answer += 1
         
         return answer
