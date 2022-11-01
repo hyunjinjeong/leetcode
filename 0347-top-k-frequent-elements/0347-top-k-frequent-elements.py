e7beeacd4dce5779bfc79f88ec639a1437b9f160
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-11-01 20:33:16 +0900

    Time: 256 ms (30.02%), Space: 18.7 MB (47.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,3 +1,20 @@
+import heapq
+
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [tup[0] for tup in collections.Counter(nums).most_common(k)]
+        # # 1. one-liner ㅎㅎ
+        # return [tup[0] for tup in collections.Counter(nums).most_common(k)]
+    
+        # 2. 표준 방법. heap을 사용하면 된다!
+        counter = collections.Counter(nums)
+        heap = []
+        for num in counter:
+            count = counter[num]
+            heapq.heappush(heap, (-count, num)) # min heap이므로 max heap으로 만들어주기 위해. 0번째 기준으로 정렬함
+        
+        result = []
+        for _ in range(k):
+            result.append(heapq.heappop(heap)[1])
+        
+        return result
