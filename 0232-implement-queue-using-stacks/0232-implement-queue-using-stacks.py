9d66831e4df8534341779cc3f33ee506ae919f33
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-11-03 21:10:16 +0900

    Time: 77 ms (5.23%), Space: 14.1 MB (23.94%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -4,20 +4,39 @@ class MyQueue:
         self._in_stack = []
         self._out_stack = []
     
-    # 1. push는 O(n), pop은 O(1) 버전
+#     # 1. push는 O(n), pop은 O(1) 버전
+#     def push(self, x: int) -> None:
+#         while self._out_stack:
+#             self._in_stack.append(self._out_stack.pop())
+#         self._in_stack.append(x)
+#         while self._in_stack:
+#             self._out_stack.append(self._in_stack.pop())
+
+#     def pop(self) -> int:
+#         return self._out_stack.pop()
+
+#     def peek(self) -> int:
+#         return self._out_stack[-1]
+
+#     def empty(self) -> bool:
+#         return not self._in_stack and not self._out_stack
+    # 2. push는 O(1), pop은 O(N) 버전
     def push(self, x: int) -> None:
-        while self._out_stack:
-            self._in_stack.append(self._out_stack.pop())
         self._in_stack.append(x)
-        while self._in_stack:
-            self._out_stack.append(self._in_stack.pop())
 
     def pop(self) -> int:
+        self._move()
         return self._out_stack.pop()
 
     def peek(self) -> int:
+        self._move()
         return self._out_stack[-1]
-
+    
+    def _move(self):
+        if not self._out_stack:
+            while self._in_stack:
+                self._out_stack.append(self._in_stack.pop())
+        
     def empty(self) -> bool:
         return not self._in_stack and not self._out_stack
 
