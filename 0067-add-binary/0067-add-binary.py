21050dfb0f0a71d59525bd429005dbbde88a9758
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-11-06 21:14:08 +0900

    Time: 86 ms (6.10%), Space: 13.9 MB (25.44%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,14 +1,15 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         carry = 0
-        answer = ""
+        answer = []
         
         list_a, list_b = list(a), list(b)
         while list_a or list_b or carry:
             num_a = int(list_a.pop()) if list_a else 0
             num_b = int(list_b.pop()) if list_b else 0
             
-            answer = str((num_a + num_b + carry) % 2) + answer
-            carry = (num_a + num_b + carry) // 2
+            temp_sum = num_a + num_b + carry
+            answer.append(str(temp_sum % 2))
+            carry = temp_sum // 2
         
-        return answer
+        return "".join(answer[::-1]) # string concat이 O(n)이니까 시간 최적화를 위해...
