46040adbe6aa12000f48dd76cee3d103c9836887
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-11-21 22:44:05 +0900

    Time: 647 ms (18.44%), Space: 16.3 MB (17.54%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,34 +1,47 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        answer = []
-        right = 0
+#         answer = []
+#         right = 0
         
-        # 우선 0 이상인 수가 나타날 때까지 right를 오른쪽으로 옮기고..
-        for num in nums:
-            if num >= 0:
-                break
-            right += 1
+#         # 우선 0 이상인 수가 나타날 때까지 right를 오른쪽으로 옮기고..
+#         for num in nums:
+#             if num >= 0:
+#                 break
+#             right += 1
             
-        # left는 끝부터 0, right는 시작부터 len(nums)-1까지 달린다
-        left = right - 1
-        while left >= 0 and right < len(nums):
-            if abs(nums[left]) < abs(nums[right]):
-                answer.append(nums[left] ** 2)
-                left -= 1
-            else:
-                answer.append(nums[right] ** 2)
-                right += 1
+#         # left는 끝부터 0, right는 시작부터 len(nums)-1까지 달린다
+#         left = right - 1
+#         while left >= 0 and right < len(nums):
+#             if abs(nums[left]) < abs(nums[right]):
+#                 answer.append(nums[left] ** 2)
+#                 left -= 1
+#             else:
+#                 answer.append(nums[right] ** 2)
+#                 right += 1
         
-        # left 혹은 right 둘 중 하나만 남음. 둘 다 처리
-        while left >= 0:
-            answer.append(nums[left] ** 2)
-            left -= 1
+#         # left 혹은 right 둘 중 하나만 남음. 둘 다 처리
+#         while left >= 0:
+#             answer.append(nums[left] ** 2)
+#             left -= 1
         
-        while right < len(nums):
-            answer.append(nums[right] ** 2)
-            right += 1
+#         while right < len(nums):
+#             answer.append(nums[right] ** 2)
+#             right += 1
         
-        return answer
+#         return answer
         
         # 요렇게 안하고 queue 사용하는 방법도 있다. 그러면 맨 위에 탐색 단계가 없어진다.
         # left는 0부터 오른쪽으로, right는 끝에서부터 왼쪽으로, 조건은 while left <= right..
+        answer = collections.deque()
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            num_left, num_right = abs(nums[left]), abs(nums[right])
+            if num_left > num_right:
+                answer.appendleft(num_left ** 2)
+                left += 1
+            else:
+                answer.appendleft(num_right ** 2)
+                right -= 1
+        
+        return list(answer)
