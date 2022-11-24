1516c9148bfd459f262281feb97e42dec1ebc215
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2022-11-24 21:02:18 +0900

    Time: 2094 ms (26.39%), Space: 20.5 MB (40.51%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -3,15 +3,12 @@ class Solution:
         # 걍 heap 써서 k개 찾으면 되는거 아닌가..?
         import heapq
         
-        def get_distance(x, y):
-            return x ** 2 + y ** 2
-        
         heap = []
         for x, y in points:
-            heapq.heappush(heap, (get_distance(x, y), (x, y)))
+            distance = x ** 2 + y ** 2
+            # min-heap을 max-heap으로 만들어서 큰 거는 빼기
+            heapq.heappush(heap, (-distance, (x, y)))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
-        answer = []
-        for _ in range(k):
-            answer.append(heapq.heappop(heap)[1])
-        
-        return answer
+        return [tup[1] for tup in heap]
