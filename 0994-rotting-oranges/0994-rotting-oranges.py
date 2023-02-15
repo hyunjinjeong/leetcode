3649b84cb33b124867c23d61d81c563d6dd97c03
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-02-15 22:54:57 +0900

    Time: 59 ms (43.69%), Space: 13.9 MB (39.70%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,37 +2,29 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         # BFS로..?
         
-        rotten = collections.deque()
+        rotting = collections.deque()
         fresh_count = 0
         # 일단 초기화
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 tomato = grid[r][c]
                 if tomato == 2:
-                    rotten.append((r, c))
+                    rotting.append((r, c))
                 elif tomato == 1:
                     fresh_count += 1
         
         minutes = 0
-        while rotten and fresh_count:
+        # rotting: 2인데 방문 안 한 토마토들, fresh_count는 남아 있는 fresh 갯수.
+        # 그래서 둘 다 체크 필요.
+        while rotting and fresh_count:
             minutes += 1
-            
-            for _ in range(len(rotten)):
-                x, y = rotten.popleft()
-                
+            for _ in range(len(rotting)):
+                x, y = rotting.popleft()
                 for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
                     next_x, next_y = x + dx, y + dy
-                    # out of range
-                    if not (0 <= next_x < len(grid)) or not (0 <= next_y < len(grid[0])):
-                        continue
-                    
-                    next_tomato = grid[next_x][next_y]
-                    # skip empty or rotten
-                    if next_tomato in (0, 2):
-                        continue
-                    
-                    fresh_count -= 1
-                    grid[next_x][next_y] = 2
-                    rotten.append((next_x, next_y))
+                    if 0 <= next_x < len(grid) and 0 <= next_y < len(grid[0]) and grid[next_x][next_y] == 1:
+                        fresh_count -= 1
+                        grid[next_x][next_y] = 2
+                        rotting.append((next_x, next_y))
         
         return minutes if fresh_count == 0 else -1
