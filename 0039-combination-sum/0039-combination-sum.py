97b9142de93178dae275e7700f438f5f69aff262
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-02-17 21:05:26 +0900

    Time: 65 ms (65.67%), Space: 13.9 MB (91.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(index, current_array, current_sum):
+        def dfs(start, current_array, current_sum):
             if current_sum > target:
                 return
             
@@ -8,7 +8,7 @@ class Solution:
                 ans.append(current_array)
                 return 
             
-            for i in range(index, len(candidates)):
+            for i in range(start, len(candidates)):
                 val = candidates[i]
                 dfs(i, current_array + [val], current_sum + val)
         
