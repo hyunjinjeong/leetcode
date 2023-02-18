ed5881a9cc27025727f0a43d2f5d63686b0906fc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-02-18 22:24:28 +0900

    Time: 49 ms (29.11%), Space: 14.2 MB (18.56%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,7 +2,7 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         def backtrack(current, visited):
             if len(current) == len(nums):
-                answer.append(current)
+                answer.append(current[:])
                 return
             
             for i in range(len(nums)):
@@ -10,8 +10,10 @@ class Solution:
                     continue
                 
                 visited.add(i)
-                backtrack(current+[nums[i]], visited)
+                current.append(nums[i])
+                backtrack(current, visited)
                 visited.remove(i)
+                current.pop()
         
         answer = []
         backtrack([], set())
