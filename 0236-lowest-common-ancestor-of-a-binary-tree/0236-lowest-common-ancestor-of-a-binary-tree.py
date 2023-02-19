1c508bfce79e8901de136a4db3f92f57fd52ebce
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-02-19 13:22:07 +0900

    Time: 67 ms (87.68%), Space: 26.3 MB (27.72%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -9,8 +9,8 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         # dfs? binary search? how?
         # root는 무조건 lca임.
-        # 그 밑으로는 한 쪽에서만 lca가 존재할 수 있으니까 아닌 쪽은 버리는 거고
-        # 그 쪽으로 dfs로 타고 가다가 왼쪽 오른쪽 둘다 lca가 아닌 친구가 나오면 그게 lca.
+        # 그 밑으로는 한 쪽에서만 후손들이 존재할 수 있으니까 그 쪽으로 타고 들어가다가
+        # 양 쪽이 모두 후손이면 그게 LCA!
         def dfs(node, p, q):
             if not node:
                 return None
@@ -20,7 +20,7 @@ class Solution:
             left = dfs(node.left, p, q)
             right = dfs(node.right, p, q)
             
-            # 노드 양쪽에서 모두 노드를 리턴하는 경우. 즉 p q를 둘 다 찾았단 얘기이고 얘가 lca임
+            # 양쪽에서 모두 노드를 리턴하는 경우. 즉 p q를 둘 다 찾았단 얘기이고 얘가 lca임
             if left and right:
                 return node
             # 한 쪽에서만 찾은 경우. 그러면 그 쪽으로 내려가야 lca가 나옴.
