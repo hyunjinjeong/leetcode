e4e1c84299970e65163a84f1a91e2e6d926b1913
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-02-19 13:42:29 +0900

    Time: 135 ms (98.91%), Space: 18 MB (78.50%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,19 +1,33 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # 시작점을 기준으로 정렬
-        intervals.sort(key=lambda interval: interval[0])
+#         # 시작점을 기준으로 정렬
+#         intervals.sort(key=lambda interval: interval[0])
         
-        left = []
-        merged = intervals[0]
-        for start, end in intervals:
-            prev_start, prev_end = merged
+#         left = []
+#         merged = intervals[0]
+#         for start, end in intervals:
+#             prev_start, prev_end = merged
             
-            # 합쳐야 하는 상황
-            if start <= prev_end:
-                merged = [prev_start, max(prev_end, end)]
-            # 안 합쳐도 되면 left 배열에 추가해주고.. merged를 갱신해줌
+#             # 합쳐야 하는 상황
+#             if start <= prev_end:
+#                 merged = [prev_start, max(prev_end, end)]
+#             # 안 합쳐도 되면 left 배열에 추가해주고.. merged를 갱신해줌
+#             else:
+#                 left.append(merged)
+#                 merged = [start, end]
+            
+#         return left + [merged]
+    
+        # 이건 Solution인데 이렇게 하면 좀 더 코드가 깔끔함!
+        # answer에 넣어두고 answer[-1]의 원소를 갱신하는 방법.
+        START, END = 0, 1
+        answer = []
+        
+        intervals.sort(key=lambda interval: interval[0])
+        for interval in intervals:
+            if not answer or interval[START] > answer[-1][END]:
+                answer.append(interval)
             else:
-                left.append(merged)
-                merged = [start, end]
-            
-        return left + [merged]
+                answer[-1][END] = max(interval[END], answer[-1][END])
+        
+        return answer
