2b86515ee83910e246cf98bb8dc64aca1eacf5e6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-01 14:09:20 +0900

    Time: 206 ms (72.11%), Space: 24.4 MB (18.19%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,35 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        # 그래프 문제로 치환할 수 있음...
+        # 이메일 -> 계정으로 hashmap 저장한 다음에 DFS로 도는 것
+        # union-find를 사용하면 더 빨라짐.
+        emails_accounts_map = {}
+        for i, account in enumerate(accounts):
+            for j in range(1, len(account)):
+                email = account[j]
+                if email in emails_accounts_map:
+                    emails_accounts_map[email].append(i)
+                else:
+                    emails_accounts_map[email] = [i]
+
+        visited_accounts = [False] * len(accounts)
+        def dfs(i, emails):
+            if visited_accounts[i]:
+                return
+            visited_accounts[i] = True
+            
+            for j in range(1, len(accounts[i])):
+                email = accounts[i][j]
+                emails.add(email)
+                for neighbor in emails_accounts_map[email]:
+                    dfs(neighbor, emails)
+        
+        res = []
+        for i, account in enumerate(accounts):
+            if visited_accounts[i]:
+                continue
+            name, emails = account[0], set()
+            dfs(i, emails)
+            res.append([name] + sorted(emails))
+            
+        return res
