6be3f60a05bbd341860c91ac74e10a8e2f07f8c5
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-03 21:34:21 +0900

    Time: 22 ms (99.78%), Space: 13.9 MB (68.61%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,27 +1,31 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        res_str = ""
         l, r = 0, 0
+
         # 1. Ignore leading whitespaces
         while l < len(s) and s[l] == " ":
             l += 1
             r = l
+
         # 2. Check the sign character
         if l < len(s) and s[l] in ("+", "-"):
-            # 찾으면 l 고정. 이제 r만 늘어남
             r += 1
+
         # 3. Read the digit characters        
         while r < len(s) and s[r].isdigit():
             r += 1
+
         # 4. Convert the digits into an integer
         res_str = s[l:r]
         try:
             res = int(res_str)
         except:
             res = 0
+
         # 5. Clamp the integer
         if res >= 2**31-1:
             res = 2**31 - 1
         elif res < -2**31:
             res = -2**31
+
         return res
