0a7281d4b59c94a3e0bd2df72353bd8f5ce9704f
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-03 22:05:30 +0900

    Time: 39 ms (54.12%), Space: 13.8 MB (68.61%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,31 +1,40 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        l, r = 0, 0
+        value, state, pos, sign = 0, 0, 0, 1
+        INT_MAX, INT_MIN = 2**31-1, -2**31
 
-        # 1. Ignore leading whitespaces
-        while l < len(s) and s[l] == " ":
-            l += 1
-            r = l
+        if len(s) == 0:
+            return 0
 
-        # 2. Check the sign character
-        if l < len(s) and s[l] in ("+", "-"):
-            r += 1
+        # DFA 버전
+        for i, c in enumerate(s):
+            if state == 0:
+                if c == " ":
+                    continue
+                elif c in ("+", "-"):
+                    sign = 1 if c == "+" else -1
+                    state = 1
+                elif c.isdigit():
+                    digit = ord(c) - ord('0') # int 구하는 트릭. 
+                    value = value * 10 + digit
+                    state = 2
+                else:
+                    return 0
+            elif state == 1:
+                if c.isdigit():
+                    digit = ord(c) - ord('0')
+                    value = value * 10 + digit
+                    state = 2
+                else:
+                    return 0
+            elif state == 2:
+                if c.isdigit():
+                    digit = ord(c) - ord('0')
+                    # overflow 체크
+                    if value > INT_MAX // 10 or (value == INT_MAX // 10 and digit > INT_MAX % 10):
+                        return INT_MAX if sign == 1 else INT_MIN
+                    value = value * 10 + digit
+                else:
+                    break
 
-        # 3. Read the digit characters        
-        while r < len(s) and s[r].isdigit():
-            r += 1
-
-        # 4. Convert the digits into an integer
-        res_str = s[l:r]
-        try:
-            res = int(res_str)
-        except:
-            res = 0
-
-        # 5. Clamp the integer
-        if res >= 2**31-1:
-            res = 2**31 - 1
-        elif res < -2**31:
-            res = -2**31
-
-        return res
+        return sign * value
