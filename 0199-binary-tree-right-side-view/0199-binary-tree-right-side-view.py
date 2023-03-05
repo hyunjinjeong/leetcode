6bcf9273e5d62efbf2bdd603fcb158f324fe8208
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-05 12:07:48 +0900

    Time: 34 ms (68.39%), Space: 13.8 MB (60.96%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,23 +7,19 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         # level 별로 가장 오른쪽에 있는 노드를 보여주면 된다!
-        # 그러면 어떻게.. level-order traversal 한 다음에 가장 오른쪽에 있는 걸 찾으면 되지 않을까.
+        # 그러면 어떻게.. pre-order에서 node -> right -> left 순으로 찾는 거임.
         def traverse(node, level):
             if not node:
                 return
             
-            if len(levels) <= level:
-                levels.append([])
+            # 가장 오른쪽에 있는 노드가 처음으로 여기 들어오게 된다
+            if level == len(ans):
+                ans.append(node.val)
             
-            levels[level].append(node.val)
             traverse(node.right, level+1)
             traverse(node.left, level+1)
             
-        levels = []
-        traverse(root, 0)
-        
         ans = []
-        for level in levels:
-            ans.append(level[0])
+        traverse(root, 0)
         
         return ans
