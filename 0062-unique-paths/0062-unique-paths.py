0e3b52922722b516c8125932629950456438c581
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-06 21:12:34 +0900

    Time: 35 ms (52.01%), Space: 14.8 MB (5.80%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,24 +1,5 @@
 class Solution:
+    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        # dp[i][j]를 grid[i][j]에서의 unique path 갯수라고 하면..
-        # 기본적으로 dp[i][j] = dp[i-1][j] + dp[i][j-1] 가 될건데
-        # 코너 케이스는 i나 j가 0인 경우? 다 1로 처리해주기.
-        
-        # 애초에 처음부터 1로 초기화하면 되는구나..
-#         dp = [[1] * n for _ in range(m)]
-        
-#         for i in range(1, m):
-#             for j in range(1, n):
-#                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        
-#         return dp[-1][-1]
-    
-        # 이건 O(n) 공간 버전. DP 배열에서 바로 전 row만 사용한다는 점 이용
-        dp = [1] * n
-        
-        for _ in range(1, m):
-            for j in range(1, n):
-                # row 단위로 계속 덮어 씌우면서 업데이트하는 느낌.
-                dp[j] += dp[j-1]
-        
-        return dp[-1]
+        if m == 1 or n == 1: return 1
+        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
