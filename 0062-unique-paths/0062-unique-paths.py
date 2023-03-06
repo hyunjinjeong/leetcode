d51f08ccb3c2404597f3512f8de3468b1088e2dd
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-06 21:01:12 +0900

    Time: 35 ms (52.01%), Space: 13.8 MB (97.31%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,10 +5,19 @@ class Solution:
         # 코너 케이스는 i나 j가 0인 경우? 다 1로 처리해주기.
         
         # 애초에 처음부터 1로 초기화하면 되는구나..
-        dp = [[1] * n for _ in range(m)]
+#         dp = [[1] * n for _ in range(m)]
         
-        for i in range(1, m):
+#         for i in range(1, m):
+#             for j in range(1, n):
+#                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+#         return dp[-1][-1]
+    
+        # 이건 O(n) 공간 버전. DP 배열에서 바로 전 row만 사용한다는 점 이용
+        dp = [1] * n
+        
+        for _ in range(1, m):
             for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                dp[j] += dp[j-1]
         
-        return dp[-1][-1]
+        return dp[-1]
