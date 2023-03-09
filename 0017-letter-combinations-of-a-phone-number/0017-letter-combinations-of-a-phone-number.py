917ecbc6ef289360757277ccee5044f5188811a4
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-09 22:30:03 +0900

    Time: 36 ms (37.86%), Space: 13.9 MB (68.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,30 +2,18 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
+        
+        dt = { "2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz" }
         result = []
-        self.helper(digits, "", result)
-        return result
-
-    def helper(self, digits, s, result):
-        if not digits:
-            result.append(s)
-            return
         
-        chars = self.get_chars(digits[0])
-        for c in chars:
-            self.helper(digits[1:], s+c, result)
-        
-    def get_chars(self, n):
-        num = int(n)
-        s = "abcdefghijklmnopqrstuvwxyz"
-        
-        if num <= 6:
-            return s[(num-2)*3:(num-2)*3+3]
-        if num == 7:
-            return "pqrs"
-        if num == 8:
-            return "tuv"
-        if num == 9:
-            return "wxyz"
-        
+        def dfs(index, current):
+            if index == len(digits):
+                result.append(current)
+                return
             
+            letters = dt[digits[index]]
+            for letter in letters:
+                dfs(index + 1, current + letter)
+            
+        dfs(0, "")
+        return result
