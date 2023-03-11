392ae3727cd6c4473fc72d478631a202b1c88389
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-11 22:09:56 +0900

    Time: 325 ms (34.29%), Space: 15.2 MB (73.68%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,19 +1,15 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         ans = []
-        # 초기화. len(p) 만큼은 채워 놓음.
+        # 초기화. len(p)-1 만큼 채워 놓음. 스페셜 케이스 없애기 위해...
         p_counter = collections.Counter(p)
-        curr_counter = collections.Counter(s[:len(p)])
+        curr_counter = collections.Counter(s[:len(p)-1])
         
-        # 스페셜 케이스
-        if p_counter == curr_counter:
-            ans.append(0)
-        
-        for right in range(len(p), len(s)):
-            curr_counter[s[right - len(p)]] -= 1
+        for right in range(len(p)-1, len(s)):
             curr_counter[s[right]] += 1
-            
+            left = right - len(p) + 1
             if p_counter == curr_counter:
-                ans.append(right - len(p) + 1)
+                ans.append(left)
+            curr_counter[s[left]] -= 1
             
         return ans
