af799db64d5b185b9f7ea22f106309b05e2892ca
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-11 22:03:03 +0900

    Time: 317 ms (35.60%), Space: 15.1 MB (97.32%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,26 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # 투 포인터 같은데..?
         ans = []
-        p_counter = collections.defaultdict(int)
-        for c in p:
-            p_counter[c] += 1
-            
-        sorted_p = sorted(p)
+        # 초기화. len(p) 만큼은 채워 놓음.
+        p_counter = collections.Counter(p)
+        curr_counter = collections.Counter(s[:len(p)])
         
-        left = 0
-        # right - left == len(p)면 조건에 따라 ans에 넣고
-        # left를 증가시키고, left를 p_counter에 하나 증가시키고
-        # 그 전에는 right를 p_counter에 하나씩 빼고...
-        for right in range(len(s)):
-            if s[right] not in p_counter:
-                left = right + 1
-                continue
-            
-            if right - left + 1 == len(p):
-                curr = s[left:right+1]
-                if sorted(curr) == sorted_p:
-                    ans.append(left)
-                left += 1
+        # 스페셜 케이스
+        if p_counter == curr_counter:
+            ans.append(0)
         
+        for right in range(len(p), len(s)):
+            curr_counter[s[right - len(p)]] -= 1
+            curr_counter[s[right]] += 1
+            
+            if p_counter == curr_counter:
+                ans.append(right - len(p) + 1)
+            
         return ans
