080fcef15778675828407f4c254b418765595549
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-15 20:53:14 +0900

    Time: 1303 ms (99.53%), Space: 28.4 MB (77.06%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,7 +3,8 @@ class Solution:
         # naive 솔루션은 자기보다 오른쪽에 있는거 다 찾는거고.. O(n^2)임.
         # 당연히 이건 안 될거고..
         # 투 포인터? dp?
-        # 아 monotonic stack이란게 있다.
+        # 아 그냥 stack에다가 넣으면 되는구나...
+        # 들어가있는 것보다 더 큰 온도가 나오면 다 업데이트해주기.
         
         ans = [0] * len(temperatures)
         
