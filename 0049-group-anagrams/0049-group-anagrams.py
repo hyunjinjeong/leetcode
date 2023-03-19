ffe46a491281da082a5427bdbce3a86a561b8354
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-19 15:46:03 +0900

    Update 0049-group-anagrams.py

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # 매번 sort 하니까 TLE 뜸... hashmap 이용해서 sorted된 값을 넣으면 된다.
+        # grouping은 어떻게 하지..?
+        # hash table 써서 정렬한 str을 key로 하면 되지 않을까
         dt = {}
-        
-        for word in strs:
-            sorted_word = "".join(sorted(word))
-            
-            if sorted_word in dt:
-                dt[sorted_word].append(word)
+        for string in strs:
+            key = "".join(sorted(string))
+            if key in dt:
+                dt[key].append(string)
             else:
-                dt[sorted_word] = [word]
+                dt[key] = [string]
         
-        return [anagrams for anagrams in dt.values()]
-
-                    
-                
+        return dt.values()
