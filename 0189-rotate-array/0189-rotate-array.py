7fed2d304159ec6e95060053df0a30a16d430411
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-03-26 20:37:17 +0900

    Time: 219 ms (73.54%), Space: 25.4 MB (25.41%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,22 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # k를 생각해보면
-        # k % len(nums) 경우만 고려하면 되지 않을까? 
-        # 즉 len(nums)가 7이면
-        # 0, 7
-        # 1, 8 ... 뭐 이런 식으로
+#         # 1. 얘는 시간은 O(n)이긴 하고 대신 공간을 O(n) 만큼 먹음.
+#         steps = k % len(nums)
+        
+#         tmp = []
+#         for i in range(len(nums)-steps):
+#             tmp.append(nums[i])
+        
+#         for i in range(steps):
+#             j = len(nums) - steps + i
+#             nums[i] = nums[j]
+        
+#         for i in range(steps, len(nums)):
+#             j = i - steps
+#             nums[i] = tmp[j]
+        # 2. reverse 이용. 왼쪽 오른쪽 각각 뒤집고 또 전체를 뒤집으면 됨
+        def reverse(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
         
         steps = k % len(nums)
+        # 각각 reverse
+        reverse(0, len(nums)-steps-1)
+        reverse(len(nums)-steps, len(nums)-1)
+        # 전체 reverse
+        reverse(0, len(nums)-1)
         
-        tmp = []
-        for i in range(len(nums)-steps):
-            tmp.append(nums[i])
-        
-        for i in range(steps):
-            j = len(nums) - steps + i
-            nums[i] = nums[j]
-        
-        for i in range(steps, len(nums)):
-            j = i - steps
-            nums[i] = tmp[j]
