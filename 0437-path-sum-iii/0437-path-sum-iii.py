b076902d611cc8bcc3c8bcde10a40879520d06e6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-04-10 22:20:14 +0900

    Time: 47 ms (91.39%), Space: 15.5 MB (18.31%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,26 +6,49 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        # 뭔가 자식 tree를 어떻게 재활용할 수 있을 것 같은디
-        # 일단 brute force 버전. 공간은 O(n) 시간은 O(n^2).
-        def dfs(node):
+#         # 뭔가 자식 tree를 어떻게 재활용할 수 있을 것 같은디
+#         # 일단 brute force 버전. 공간은 O(n) 시간은 O(n^2).
+#         def dfs(node):
+#             if not node:
+#                 return
+            
+#             validate(node, 0)
+#             dfs(node.left)
+#             dfs(node.right)
+            
+#         def validate(node, curr_sum):
+#             if not node:
+#                 return
+            
+#             if curr_sum + node.val == targetSum:
+#                 self.ans += 1
+            
+#             validate(node.left, curr_sum + node.val)
+#             validate(node.right, curr_sum + node.val)
+            
+#         self.ans = 0
+#         dfs(root)
+#         return self.ans
+
+        # 다음으론 개선된 버전.. two sum 아이디어 활용.. 공간 O(N) 시간 O(N)
+        def dfs(node, curr_sum):
             if not node:
                 return
             
-            validate(node, 0)
-            dfs(node.left)
-            dfs(node.right)
-            
-        def validate(node, curr_sum):
-            if not node:
-                return
-            
-            if curr_sum + node.val == targetSum:
+            new_sum = curr_sum + node.val
+            if new_sum == targetSum:
                 self.ans += 1
+            # 예를 들어 1->2->3 상황에서 curr_sum 3, node.val 3이고 target이 3이라면 3이 cache에 있는지 찾는 거.
+            self.ans += self.count[new_sum - targetSum]
             
-            validate(node.left, curr_sum + node.val)
-            validate(node.right, curr_sum + node.val)
+            self.count[new_sum] += 1
             
+            dfs(node.left, new_sum)
+            dfs(node.right, new_sum)
+            # backtracking. 현재 node에서 벗어나면 count 하나 빼줌.
+            self.count[new_sum] -= 1
+        
         self.ans = 0
-        dfs(root)
+        self.count = collections.defaultdict(int)
+        dfs(root, 0)
         return self.ans
