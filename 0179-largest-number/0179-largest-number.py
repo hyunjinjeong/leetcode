9c64343326ef087e7e913365903643e02d30f48f
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-04-13 22:09:53 +0900

    Time: 34 ms (93.04%), Space: 13.9 MB (55.46%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        # sorting & greedy라..?
+        # 그럼 무슨 방법을 정해서 그 순서대로 쭉 하면 된다는 것.
+        # string으로 변환 후 비교까진 맞았는데... 단순히 두개 비교하면 됐었음.
+        
+        def compare(a, b):
+            str_a, str_b = str(a), str(b)
+            
+            if str_a + str_b < str_b + str_a:
+                return 1 # 1이면 sorting 후 후순위. 즉 뒤로 온다는 뜻
+            else:
+                return -1
+        
+        # 엣지 케이스. [0, 0] 같은 경우 처리.
+        if not any(nums):
+            return "0"
+        
+        sorted_nums = sorted(nums, key=functools.cmp_to_key(compare))
+        return "".join([str(item) for item in sorted_nums])
