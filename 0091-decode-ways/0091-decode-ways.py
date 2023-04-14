69654f3ad81a9530d4658dbdc02d6bc8ef9bb1ac
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-04-14 19:26:10 +0900

    Time: 40 ms (19.72%), Space: 13.9 MB (36.03%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,10 +1,6 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # dp[n]을 s[:n]에서의 갯수라고 하면,
-        # 1. s[n] > 6이면 dp[n] = dp[n-1]
-        # 2. s[n] <= 6이면 dp[n] = dp[n-1] + dp[n-2] ? 이건가..
-        # 이 때 s[n-2:n] 요게 10 ~ 26 사이여야 위 식이 성립하고 아니면 그대로 dp[n] = dp[n-1]
-        # 요게 아니고 0 < s[n] <= 9 이면 dp[n] += dp[n-1]
+        # 0 < s[n] <= 9 이면 dp[n] += dp[n-1]
         # 그리고 10 <= s[n-2:n] <= 26 이면 dp[n] += dp[n-2]
         # 둘을 각각 따로 계산하는 게 핵심...
         
