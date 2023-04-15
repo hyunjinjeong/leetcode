2424b6a4bf39f908d32f2bd71150e74465045b67
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-04-15 14:33:40 +0900

    Time: 26 ms (95.96%), Space: 14 MB (8.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,5 +1,22 @@
 class Solution:
     def reverse(self, x: int) -> int:
+#         ans = 0
+        
+#         # 음수 처리
+#         if x < 0:
+#             sign = -1
+#             x = -x
+#         else:
+#             sign = 1
+        
+#         while x:
+#             ans = ans * 10 + x % 10
+#             x //= 10
+            
+#         ans *= sign
+#         return ans if -2**31 <= ans <= 2**31 - 1 else 0
+    
+        # 위 코드는 좋지만 64비트 자료형을 사용하기 때문에...
         ans = 0
         
         # 음수 처리
@@ -9,7 +26,14 @@ class Solution:
         else:
             sign = 1
         
+        MAX_INT, MIN_INT = 2**31-1, -2**31
         while x:
+            # MAX_INT: 2147483647 이니까.
+            if sign == 1 and (ans > MAX_INT // 10 or ans == MAX_INT // 10 and x % 10 > 7):
+                return 0
+            if sign == -1 and (-ans < MIN_INT // 10 or -ans == MIN_INT // 10 and -(x % 10) > 8):
+                return 0
+            
             ans = ans * 10 + x % 10
             x //= 10
             
