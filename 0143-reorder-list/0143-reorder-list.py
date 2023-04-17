a1bf455847312e5087b1fa648731ad10be050e22
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-04-17 15:28:38 +0900

    Time: 88 ms (75.40%), Space: 24 MB (57.33%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -13,35 +13,30 @@ class Solution:
         # 그담에 하나씩 순서대로 연결하면 되지 않을까?
         
         # 1. 중간 찾기
-        prev, slow, fast = None, head, head
-        while fast and fast.next:
-            prev = slow
+        slow, fast = head, head
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
+        # slow.next를 잡으면 홀수, 짝수 모두 잘 처리됨.
+        mid = slow.next
         # circular 방지
-        if prev:
-            prev.next = None
+        slow.next = None
         
         # 2. 오른쪽 뒤집기
-        prev, curr = None, slow
+        prev, curr = None, mid
         while curr:
             temp_next = curr.next
+            
             curr.next = prev
-
             prev = curr
+            
             curr = temp_next
 
         # 3. 하나씩 순서대로 연결하기
         left, right = head, prev
-        tail = None
         while left and right:
             tmp_left_next, tmp_right_next = left.next, right.next
-            tail = right
             left.next = right
             right.next = tmp_left_next
             
             left, right = tmp_left_next, tmp_right_next
-        
-        # 홀수개일 경우 right가 1개 더 많음
-        if right:
-            tail.next = right
