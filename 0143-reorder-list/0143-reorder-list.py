7a7ee0c654bebab4bc4916931c2f0a2b55b62e4f
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-04-17 15:31:40 +0900

    Time: 94 ms (47.87%), Space: 23.9 MB (92.94%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,35 +8,55 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # 중간을 찾아서
-        # 오른쪽 뒤집고
-        # 그담에 하나씩 순서대로 연결하면 되지 않을까?
-        
-        # 1. 중간 찾기
+        # 3단계로 나눠서 풀기. 1. mid 구하기, 2. 오른쪽 반 reverse, 3. merge
+        # 1. mid 구하기. slow가 mid가 됨
         slow, fast = head, head
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-        # slow.next를 잡으면 홀수, 짝수 모두 잘 처리됨.
-        mid = slow.next
-        # circular 방지
-        slow.next = None
         
-        # 2. 오른쪽 뒤집기
-        prev, curr = None, mid
+        # 2. 오른쪽 반 뒤집기. 짝수일 때 생각해보면 slow.next부터 뒤집어야 함.
+        # 홀수일 때도 slow가 중간이니까 상관 없고..
+        prev, curr = None, slow.next
         while curr:
-            temp_next = curr.next
+            tmp_next = curr.next
             
             curr.next = prev
             prev = curr
             
-            curr = temp_next
-
-        # 3. 하나씩 순서대로 연결하기
-        left, right = head, prev
-        while left and right:
-            tmp_left_next, tmp_right_next = left.next, right.next
-            left.next = right
-            right.next = tmp_left_next
+            curr = tmp_next
+        # 사이클 방지. 다 뒤집었으면 slow.next는 끝이 됨.
+        slow.next = None
+        
+        # 3. 두 LL 병합하기
+        head1, head2 = head, prev
+        while head1 and head2:
+            next1, next2 = head1.next, head2.next
             
-            left, right = tmp_left_next, tmp_right_next
+            head1.next = head2
+            head1 = next1
+            
+            head2.next = head1
+            head2 = next2
+        
+#         # 아니면 스택을 이용할 수도 있음. 다 array에 집어넣고 ...
+#         s = []
+#         curr = head
+#         while curr:
+#             s.append(curr)
+#             curr = curr.next
+        
+#         curr = head
+#         mid = len(s) // 2
+#         for _ in range(mid):
+#             tmp = curr.next
+            
+#             curr.next = s.pop()
+#             curr = curr.next
+#             curr.next = tmp
+            
+#             curr = curr.next
+        
+#         # 사이클 방지
+#         if curr != None:
+#             curr.next = None
