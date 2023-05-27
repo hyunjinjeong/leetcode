c5fad1c87e74813b4481fb056603fbb81ca95c44
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-05-27 16:09:27 +0900

    Time: 101 ms (85.27%), Space: 20.1 MB (35.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,22 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # 포인터가 k개가 있는데
-        # 그러면 k개 중 최솟값을 찾아서 계속 돌려야 함.
-        # brute force로 하면 매번 k번 도니까.. O(kn)일 건데
-        # 생각해보니까 어차피 이거 다 돌아야 하는구나.. 그럼 시간은 저게 최소인듯?
-        # 다 heap에 넣어서 빼는 건 어떨까..
+        # heap을 사용하는데 k개만큼만 넣으면서 하는 방법도 있다!
+        # 이 경우 전체 갯수를 n개라고 하면 시간은 O(nlogk), 공간은 O(k).
         heap = []
-        for node in lists:
-            n = node
-            while n:
-                heapq.heappush(heap, n.val)
-                n = n.next
+        for i, node in enumerate(lists):
+            # index를 넣는건 heapq 모듈이 tuple 순서대로 비교하기 떄문에..
+            # 값이 같은 경우 tie breaker임.
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
         
         dummy_head = ListNode()
         node = dummy_head
         while heap:
-            node.next = ListNode(heapq.heappop(heap))
+            # 가장 작은 원소를 꺼내서 연결하고...
+            _val, index, temp_node = heapq.heappop(heap)
+            
+            node.next = temp_node
             node = node.next
+            # 그 다음꺼 heap에다가 넣기
+            if node.next:
+                heapq.heappush(heap, (node.next.val, index, node.next))
         
         return dummy_head.next
