d066cfd3a63206846e74ff4056e55efd76e01443
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-05-30 17:27:27 +0900

    Time: 303 ms (94.07%), Space: 25 MB (23.20%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -1,25 +1,46 @@
 class FreqStack:
-
     def __init__(self):
-        # heap을 써야 할 것 같은데...
-        # top에 가까운건 뭘로 판단하지?
-        # 걍 push할 때마다 index 넣으면 되지 않나..
-        self.heap = []
-        self.index = 0
+        # stack of stacks 이라는 방법이 있다.
         self.freq = collections.defaultdict(int)
+        self.stacks = [[]] # 1부터 시작하도록 넣어주자..
 
     def push(self, val: int) -> None:
         self.freq[val] += 1
-        self.index += 1
         
-        # min-heap이니까 negate
-        element = (-self.freq[val], -self.index, val)
-        heapq.heappush(self.heap, element)
+        if self.freq[val] >= len(self.stacks):
+            self.stacks.append([])
+        
+        self.stacks[self.freq[val]].append(val)
 
     def pop(self) -> int:
-        _freq, _index, val = heapq.heappop(self.heap)
+        val = self.stacks[-1].pop()
         self.freq[val] -= 1
+        
+        if not self.stacks[-1]:
+            self.stacks.pop()
+            
         return val
+
+#     def __init__(self):
+#         # heap을 써야 할 것 같은데...
+#         # top에 가까운건 뭘로 판단하지?
+#         # 걍 push할 때마다 index 넣으면 되지 않나..
+#         self.heap = []
+#         self.index = 0
+#         self.freq = collections.defaultdict(int)
+
+#     def push(self, val: int) -> None:
+#         self.freq[val] += 1
+#         self.index += 1
+        
+#         # min-heap이니까 negate
+#         element = (-self.freq[val], -self.index, val)
+#         heapq.heappush(self.heap, element)
+
+#     def pop(self) -> int:
+#         _freq, _index, val = heapq.heappop(self.heap)
+#         self.freq[val] -= 1
+#         return val
 
 
 # Your FreqStack object will be instantiated and called as such:
