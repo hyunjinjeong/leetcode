a0f33f3230874c819e1e7d7dca9881b1eab1fecc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-05-31 22:11:22 +0900

    Time: 111 ms (24.56%), Space: 16.6 MB (42.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,21 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        if len(nums1) < len(nums2):
+        if len(nums1) > len(nums2):
+            # 항상 num1이 더 짧도록.
             return self.findMedianSortedArrays(nums2, nums1)
         
-        lo, hi = 0, len(nums2) * 2
-        while lo <= hi:
-            mid2 = (lo + hi) // 2
-            mid1 = len(nums1) + len(nums2) - mid2
-
-            l1 = float("-inf") if mid1 == 0 else nums1[(mid1-1)//2]
-            l2 = float("-inf") if mid2 == 0 else nums2[(mid2-1)//2]
-            r1 = float("inf") if mid1 == len(nums1) * 2 else nums1[mid1//2]
-            r2 = float("inf") if mid2 == len(nums2) * 2 else nums2[mid2//2]
-
-            if l1 > r2:
-                lo = mid2 + 1
-            elif l2 > r1:
-                hi = mid2 - 1
+        total_length = len(nums1) + len(nums2)
+        left_partition_size = (total_length + 1) // 2
+        
+        left, right = 0, len(nums1)
+        while left <= right:
+            mid = (left + right) // 2
+            nums1_size = mid
+            nums2_size = left_partition_size - mid
+            
+            # boundary number 4개 구하기
+            nums1_left = nums1[nums1_size-1] if nums1_size > 0 else float("-inf")
+            nums1_right = nums1[nums1_size] if nums1_size < len(nums1) else float("inf")
+            nums2_left = nums2[nums2_size-1] if nums2_size > 0 else float("-inf")
+            nums2_right = nums2[nums2_size] if nums2_size < len(nums2) else float("inf")
+            
+            if nums1_left > nums2_right:
+                right = mid - 1
+            elif nums2_left > nums1_right:
+                left = mid + 1
             else:
-                return (max(l1, l2) + min(r1, r2)) / 2
+                if total_length % 2 == 1:
+                    return max(nums1_left, nums2_left)
+                else:
+                    return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
