9a1cdbe0eb950276959d4ee1b59770fd92e934bb
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-02 22:07:56 +0900

    Time: 50 ms (66.80%), Space: 17.1 MB (17.61%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -3,15 +3,16 @@ class Solution:
         stack = []
         
         ans = 0
-        stack.append(-1)
+        # stack에는 현재 index에서 valid한 마지막 위치가 들어감.
+        stack.append(-1) 
         for i, c in enumerate(s):
             if c == "(":
                 stack.append(i)
             else:
                 stack.pop()
-                if stack:
+                if stack: # stack이 남아 있으면 valid란 소리..
                     ans = max(ans, i - stack[-1])
-                else:
+                else: # 이제 invalid. 그러면 새 index를 push
                     stack.append(i)
         
         return ans
