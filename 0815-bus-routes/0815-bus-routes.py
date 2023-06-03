bcfef7b0034bb1399a0c9e9e7da0b53191c93dd4
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-03 17:12:16 +0900

    Time: 1557 ms (8.83%), Space: 40 MB (58.26%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,14 +1,6 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        # 가중치가 없는 그래프.
-        # 단방향..? 근데 loop는 있음.
-        # 그럼 그래프를 만든 뒤에 BFS 돌리면 안되려나
-        # 아 근데 bus stop을 최소화하는게 아니라... bus를 최소화해야 함
-        # 
-        # 일단 그래프를 만들어보자
-        if source == target:
-            return 0
-        
+        # bus stop을 최소화하는게 아니라... bus를 최소화해야 함
         stop_to_bus = collections.defaultdict(list)
         for bus, stops in enumerate(routes):
             for stop in stops:
