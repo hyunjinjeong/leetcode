1ef9872d4617eac621ef1bdaa0756c0bcfb6aaf1
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-05 21:32:30 +0900

    Time: 51 ms (84.35%), Space: 17.6 MB (23.39%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,21 +5,28 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # k만큼 가서 reverse?
-        # k 되기 전에 end가 나오면 안 하고..
-        count = 0
+        # 요건 iterative solution. 이래야 공간이 O(1).
+        total_count = 0
         node = head
-        while node and count < k:
+        while node:
+            total_count += 1
             node = node.next
-            count += 1
         
-        if count < k:
-            return head
+        dummy = ListNode()
+        dummy.next = head
         
-        # 아 head가 tail이 되니까.. new_tail은 굳이 없어도 됨.
-        new_head, next_new_tail = self.reverse(head, k)
-        head.next = self.reverseKGroup(next_new_tail, k)
-        return new_head
+        prev, curr = dummy, dummy.next
+        while total_count >= k:
+            new_head, new_tail, next_new_tail = self.reverse(curr, k)
+            
+            prev.next = new_head
+            curr.next = next_new_tail
+            prev = new_tail
+            curr = next_new_tail
+            
+            total_count -= k
+            
+        return dummy.next
     
     # 요건 단순 reverse
     def reverse(self, node, k):
@@ -33,5 +40,37 @@ class Solution:
             prev = curr
             curr = temp_next
         
-        head, next_tail = prev, curr
-        return head, next_tail
+        head, tail, next_tail = prev, node, curr
+        return head, tail, next_tail
+        
+        
+#         # k만큼 가서 reverse?
+#         # k 되기 전에 end가 나오면 안 하고..
+#         count = 0
+#         node = head
+#         while node and count < k:
+#             node = node.next
+#             count += 1
+        
+#         if count < k:
+#             return head
+        
+#         # 아 head가 tail이 되니까.. new_tail은 굳이 없어도 됨.
+#         new_head, next_new_tail = self.reverse(head, k)
+#         head.next = self.reverseKGroup(next_new_tail, k)
+#         return new_head
+    
+#     # 요건 단순 reverse
+#     def reverse(self, node, k):
+#         prev, curr = None, node
+#         count = 0
+#         while curr and count < k:
+#             count += 1
+#             temp_next = curr.next
+#             curr.next = prev
+            
+#             prev = curr
+#             curr = temp_next
+        
+#         head, next_tail = prev, curr
+#         return head, next_tail
