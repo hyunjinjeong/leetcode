7c3ebf921b0f637ba4757e73a2a96383d7e43cd6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-07 21:50:09 +0900

    Time: 442 ms (24.25%), Space: 30.1 MB (67.20%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,17 +1,21 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # 일단 MP 범위는 1 <= MP <= len(nums)+1임. 
-        # tortoise hare 알고리즘으로 되려나..?
-        # 근데 -가 있을 수 있고, num이 len(nums)보다 클 수 있음.
         # 일단 공간을 non-constant 쓰는 알고리즘을 생각해보자.
-        # sort 후 찾는건 시간 O(nlogn)...
         # 1 ~ len(nums)까지를 key로 하는 dict 만들어서
         # 2-pass로 한번은 저장, 한번은 찾으면 공간 O(n?)
-        dt = {}
-        for num in nums:
-            if 1 <= num <= len(nums)+1:
-                dt[num] = True
+        # 저장하는걸 nums 에다가 하면 되겠는데
         
-        for num in range(1, len(nums)+2):
-            if num not in dt:
-                return num
+        # 각 숫자를 해당 index로 move. e.g. 1->0, 5->4 ...
+        # 여기서 잘 안됐는데 nums[i]를 num으로 변수 저장해둔게 문제였음..
+        for i in range(len(nums)):
+            while 1 <= nums[i] <= len(nums) and nums[i] != nums[nums[i]-1]:
+                tmp = nums[i] - 1
+                nums[i], nums[tmp] = nums[tmp], nums[i]
+        
+        for i, num in enumerate(nums):
+            if num != i + 1:
+                return i + 1
+        
+        # for문에서 안 걸리면 1~len(nums)까지 꽉 차있는거
+        return len(nums) + 1
