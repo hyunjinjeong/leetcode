e7f3ea0279b8b939d138f6fd55adadb1c4e5d967
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-13 20:38:14 +0900

    Time: 113 ms (70.31%), Space: 21.3 MB (34.55%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,14 +6,24 @@ class Solution:
         # # 1. one-liner ㅎㅎ
         # return [tup[0] for tup in collections.Counter(nums).most_common(k)]
     
-        # 2. 표준 방법. heap을 사용하면 된다!
-        counter = collections.Counter(nums)
-        heap = []
-        # min heap 을 사용하면 O(nlogk)가 될 것 같은데?
-        for num in counter:
-            count = counter[num]
-            heapq.heappush(heap, (count, num))
-            if len(heap) == k + 1:
-                heapq.heappop(heap)
+#         # 2. 표준 방법. heap을 사용하면 된다!
+#         counter = collections.Counter(nums) # O(N)
+#         heap = []
         
-        return [tup[1] for tup in heap]
+#         for num in counter: # O(NlogK)
+#             count = counter[num]
+#             heapq.heappush(heap, (count, num))
+#             if len(heap) == k + 1:
+#                 heapq.heappop(heap)
+        
+#         return [tup[1] for tup in heap] # O(K)
+    
+        counter = collections.Counter(nums) # O(N)
+        heap = [(-count, num) for num, count in counter.items()] # O(N)
+        heapq.heapify(heap) # O(N)
+        
+        ans = []
+        for _ in range(k): # O(KlogN)
+            ans.append(heapq.heappop(heap)[1])
+        
+        return ans
