5a3ae7d13357fb771c20268b8869f852d99ba217
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-13 20:53:34 +0900

    Time: 121 ms (38.39%), Space: 21.9 MB (11.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,3 @@
-import heapq
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # # 1. one-liner ㅎㅎ
@@ -18,12 +15,25 @@ class Solution:
         
 #         return [tup[1] for tup in heap] # O(K)
     
-        counter = collections.Counter(nums) # O(N)
-        heap = [(-count, num) for num, count in counter.items()] # O(N)
-        heapq.heapify(heap) # O(N)
+#         counter = collections.Counter(nums) # O(N)
+#         heap = [(-count, num) for num, count in counter.items()] # O(N)
+#         heapq.heapify(heap) # O(N)
+        
+#         ans = []
+#         for _ in range(k): # O(KlogN)
+#             ans.append(heapq.heappop(heap)[1])
+        
+#         return ans
+        # 3. bucket sort를 이용하면 O(N)에 가능
+        counter = collections.Counter(nums)
+        
+        bucket = [[] for _ in range(len(nums)+1)]
+        for num, freq in counter.items():
+            bucket[freq].append(num)
         
         ans = []
-        for _ in range(k): # O(KlogN)
-            ans.append(heapq.heappop(heap)[1])
-        
-        return ans
+        for i in range(len(nums), -1, -1):
+            for num in bucket[i]:
+                ans.append(num)
+                if len(ans) == k:
+                    return ans
