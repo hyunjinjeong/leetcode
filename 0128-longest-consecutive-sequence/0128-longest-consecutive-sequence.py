7a7acdaa194d13d0fe54b1dafbfc2aa6d823ce07
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-22 22:06:30 +0900

    Time: 784 ms (45.83%), Space: 66.5 MB (5.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,47 +1,56 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-#         # 1. 일단 set 활용한 거
-#         s, ans = set(nums), 0
+#         longest_streak = 0
         
-#         for num in s:
-#             # 여기서 -1 체크하는 이유는 +1 ...로 검사하기 때문에.
-#             if num - 1 in s:
+#         num_set = set(nums)
+        
+#         # 여기 nums 쓸때랑 num_set 쓸 때랑 시간 차이가 엄청 나는데.. set에서 iterate 하는게 더 빠른가?
+#         # 아 문제 조건 중에 모든 값이 고유하다는 조건이 없구나. 중복값이 있어서 그렇구만
+#         for num in num_set:
+#             # 요 if문 없으면 TLE 걸림. 1 2 3 4 5 6 7 ... 이런 케이스 생각해보면 된다
+#             if num - 1 in num_set:
 #                 continue
             
-#             j = 1
-#             while num + j in s:
-#                 j += 1
+#             current_num = num
+#             current_streak = 1
             
-#             ans = max(ans, j)
+#             while current_num + 1 in num_set:
+#                 current_num += 1
+#                 current_streak += 1
+            
+#             longest_streak = max(longest_streak, current_streak)
         
-#         return ans
+#         return longest_streak
         
-        # 2. Union-Find 활용.
-        # Union-Find 함수 정의
-        def find(x):
-            if parents[x] != x:
-                parents[x] = find(parents[x])
-            return parents[x]
-        
-        def union(a, b):
-            root_a, root_b = find(a), find(b)
-            if root_a != root_b:
-                parents[root_b] = root_a
-                size[root_a] += size[root_b]
-        
+        # 그래프로 풀기... Union-Find 이용
         if not nums:
             return 0
         
         num_set = set(nums)
         
+        # 자료구조 정의해서 쓰는 대신에 dict 사용해서 각각 관리
+        group, size = {}, {}
+        
         # disjoint set 초기화
-        parents, size = {}, {}
         for num in num_set:
-            parents[num] = num
+            group[num] = num
             size[num] = 1
         
-        # Union
+        # Union-Find 함수 정의
+        def find(a):
+            if group[a] != a:
+                group[a] = find(group[a])
+            return group[a]
+        
+        def union(a, b):
+            r_a, r_b = find(a), find(b)
+            if r_a != r_b:
+                group[r_b] = r_a
+                size[r_a] += size[r_b]
+        
         for num in num_set:
+            if num - 1 in num_set:
+                union(num, num-1)
             if num + 1 in num_set:
                 union(num, num+1)
         
