035edeefc35231e1d1a1650eee2af98916ade10d
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-24 22:37:08 +0900

    Time: 1297 ms (82.59%), Space: 20.6 MB (45.13%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        answer = set()
+        answer = []
         
         nums.sort()
         
         for left in range(len(nums)):
             # 하나를 고르고 나서.. 그 다음에 two-sum 적용하면 된다!
             # 중복은 매번 체크하면 TLE가 떠서 정렬 후 투 포인터 사용...
-            if left > 1 and nums[left] == nums[left-1]:
+            if left > 0 and nums[left] == nums[left-1]:
                 continue
             
             target = 0 - nums[left]
@@ -15,12 +15,13 @@ class Solution:
             while mid < right:
                 current_sum = nums[mid] + nums[right]
                 if current_sum == target:
-                    answer.add((nums[left], nums[mid], nums[right]))
+                    answer.append([nums[left], nums[mid], nums[right]])
                     mid += 1
-                    right -= 1
+                    while mid < right and nums[mid] == nums[mid-1]:
+                        mid += 1
                 elif current_sum < target:
                     mid += 1
                 else:
                     right -= 1
             
-        return list(answer)
+        return answer
