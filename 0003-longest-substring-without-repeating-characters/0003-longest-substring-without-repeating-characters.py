6ddb101121b0764e2479a8dd6108c5f28fd476bf
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-27 22:42:07 +0900

    Time: 65 ms (92.57%), Space: 16.5 MB (44.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # counter 정의해서... sliding window?
-        pos = {}
+        last_pos = {}
         ans = 0
 
         left = 0
         for right, c in enumerate(s):
-            if c not in pos:
+            if c not in last_pos:
                 ans = max(right - left + 1, ans)
             else:
-                if pos[c] < left:
+                if last_pos[c] < left:
                     ans = max(right - left + 1, ans)
                 else:
-                    left = pos[c] + 1
+                    left = last_pos[c] + 1
             
-            pos[c] = right
+            last_pos[c] = right
 
         return ans
