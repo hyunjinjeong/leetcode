14529f497c6d028e844b13ee59efdb6d3122b1ab
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-06-27 22:39:36 +0900

    Time: 67 ms (89.58%), Space: 16.3 MB (83.7%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,15 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # silding window... 마지막으로 c가 사용된 위치 저장하면 된당
-        last_used = {}
-        max_length, start = 0, 0
-        
-        for i, c in enumerate(s):
-            if c in last_used and start <= last_used[c]:
-                start = last_used[c] + 1
+        # counter 정의해서... sliding window?
+        pos = {}
+        ans = 0
+
+        left = 0
+        for right, c in enumerate(s):
+            if c not in pos:
+                ans = max(right - left + 1, ans)
             else:
-                max_length = max(max_length, i-start+1)
+                if pos[c] < left:
+                    ans = max(right - left + 1, ans)
+                else:
+                    left = pos[c] + 1
             
-            last_used[c] = i
-        
-        return max_length
+            pos[c] = right
+
+        return ans
