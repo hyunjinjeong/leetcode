be3277c6432c1beede36216c3def412c4f6d29fc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-01 14:34:16 +0900

    Time: 163 ms (50.02%), Space: 17.1 MB (64.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,22 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         t_counter = collections.Counter(t)
-        # 1. 우선 substring을 찾을 때까지 r을 늘리다가 (끝까지 가도 없으면 "" 리턴)
-        # 2. 찾고 나면 또 조건을 만족할 때까지 l을 늘려주면 된다!
-        # 3. l을 늘리다가 조건에서 벗어나면 다시 1-2번 반복.
-        ans = ""
-        l = 0
-        for r, c in enumerate(s):
-            if c in t_counter:
-                t_counter[c] -= 1
 
-            while all([v <= 0 for v in t_counter.values()]):
-                if r+1-l < len(ans) or ans == "":
-                    ans = s[l:r+1]
-                
-                if s[l] in t_counter:
-                    t_counter[s[l]] += 1
-                l += 1
-                
-        return ans
+        ans = (0, 0)
+        ans_count = 0
+
+        left = 0
+        for right, c in enumerate(s):
+            if t_counter[c] > 0:
+                ans_count += 1
+            t_counter[c] -= 1
+
+            while ans_count == len(t):
+                # (0, 0)은 엣지 케이스...
+                if right - left + 1 < ans[1] - ans[0] or ans == (0, 0):
+                    ans = (left, right + 1)
+
+                t_counter[s[left]] += 1
+                if t_counter[s[left]] > 0:
+                    ans_count -= 1
+
+                left += 1
         
+        return s[ans[0]:ans[1]]
