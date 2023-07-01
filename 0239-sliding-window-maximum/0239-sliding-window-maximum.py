ca5f6322dc52c33bab3fb0bfdbff23a4ae0ce449
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-01 18:43:43 +0900

    Time: 1564 ms (99.23%), Space: 33.4 MB (11.65%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,16 +4,18 @@ class Solution:
         
         # decreasing deque였나...
         for i in range(k-1):
-            while dq and nums[dq[-1]] < nums[i]: # 현재 num보다 작은 element들은 모두 pop
+            while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
             dq.append(i)
 
         ans = []
         for i in range(k-1, len(nums)):
-            if dq and dq[0] == i - k: # 맨 왼쪽이 범위 벗어나면 pop
+            # 맨 왼쪽이 범위 벗어나면 pop. i - k + 1이 가장 왼쪽이니까...
+            if dq and dq[0] == i - k:
                 dq.popleft()
 
-            while dq and nums[dq[-1]] < nums[i]: # 현재 num보다 작은 element들은 모두 pop
+            # 현재 num보다 작은 element들은 모두 pop. decreasing 이기 때문에 오른쪽에서부터.
+            while dq and nums[dq[-1]] < nums[i]: 
                 dq.pop()
              
             dq.append(i)
