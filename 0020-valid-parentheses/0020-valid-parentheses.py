ad8fa4a346a0ca523f6a59637500b45c8abf54f4
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-02 15:51:32 +0900

    Time: 49 ms (45.93%), Space: 16.2 MB (90.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         # 단순하게 스택 이용해서 구현
-        open_parens = []
+        stack = []
         pairs = { "}": "{", "]": "[", ")": "(" }
         
         for c in s:
-            if c == "(" or c == "[" or c == "{":
-                open_parens.append(c)
+            if c in ("(", "[", "{"):
+                stack.append(c)
             else:
-                if not open_parens or pairs[c] != open_parens[-1]:
+                if not stack or pairs[c] != stack[-1]:
                     return False
-                open_parens.pop()
+                stack.pop()
                 
-        return not open_parens
+        return not stack
         
