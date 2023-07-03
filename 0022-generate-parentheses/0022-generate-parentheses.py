ac26c185278f816a976461187c100b907521af19
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-03 21:30:48 +0900

    Time: 50 ms (62.25%), Space: 16.8 MB (25.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
+        # DFS, Backtracking 류 같은데...
+        ans = []
+
         def dfs(left, right, curr):
             if left == n and right == n:
-                res.append("".join(curr))
+                ans.append("".join(curr))
                 return
             
             if left < n:
                 curr.append("(")
                 dfs(left+1, right, curr)
                 curr.pop()
-            if right < left:
+            if right < left: # 여기가 중요...
                 curr.append(")")
                 dfs(left, right+1, curr)
                 curr.pop()
 
-        res = []
         dfs(0, 0, [])
-        return res
-    
-    
+        return ans
