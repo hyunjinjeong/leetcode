d102d35d1d860e6156b4b392e534b4d8c5293879
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-04 21:45:37 +0900

    Time: 1276 ms (95.47%), Space: 30.9 MB (64.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,18 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # naive 솔루션은 자기보다 오른쪽에 있는거 다 찾는거고.. O(n^2)임.
-        # 당연히 이건 안 될거고..
-        # 투 포인터? dp?
-        # 아 그냥 stack에다가 넣으면 되는구나...
-        # 들어가있는 것보다 더 큰 온도가 나오면 다 업데이트해주기.
-        
+        # stack 활용하면 될 듯..?
         ans = [0] * len(temperatures)
-        
+
         stack = []
-        for curr_day, curr_temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < curr_temp:
-                prev_day = stack.pop()
-                ans[prev_day] = curr_day - prev_day
-            stack.append(curr_day)
+        for index, temperature in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temperature:
+                prev_index = stack.pop()
+                ans[prev_index] = index - prev_index
+            
+            stack.append(index)
         
         return ans
