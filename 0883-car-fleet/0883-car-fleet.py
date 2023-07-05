ff112a6fed450133f02c4f5c92cc3e438897f696
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-05 22:36:08 +0900

    Time: 900 ms (78.84%), Space: 38.4 MB (58.25%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,15 +1,17 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # monotonic increasing stack...
-        stack = []
+        # monotonic increasing stack... 과 비슷한 개념
+        ans = 0
+        last_time = -1
         # position 순으로 정렬한 뒤, 뒤에서부터 남은 시간을 계산하면
-        # 뒤에 나온 친구(position이 낮으면)가 time이 더 큰 경우에만 append.
+        # 뒤에 나온 친구(position이 낮으면)가 time이 더 큰 경우에만 더하기.
         # 더 낮으면 합쳐져서 fleet이 된 거니까...
         for pos, v in sorted(zip(position, speed), reverse=True):
             dist = target - pos
             time = dist / v 
             
-            if not stack or time > stack[-1]:
-                stack.append(time)
+            if time > last_time:
+                ans += 1
+                last_time = time
 
-        return len(stack)
+        return ans
