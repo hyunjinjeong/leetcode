b463e3fc009436a4f5c45fd62de60e3607f77fbf
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-08 17:21:13 +0900

    Time: 49 ms (91.17%), Space: 16.6 MB (85.48%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,21 +1,21 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # binary search를 응용하면 될 것 같은데
-        # rotate 된 부분을 기준으로
-        # left도 정렬, right도 정렬
-        # mid가 right보다 크면 오른쪽, 아니면 왼쪽으로 가면 됨.
-        # 1 2 3 4 5
-        # 5 1 2 3 4
-        # 4 5 1 2 3
-        # 3 4 5 1 2
-        # 2 3 4 5 1
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            
-            if nums[mid] > nums[right]:
-                left = mid + 1
-            else:
-                right = mid
+        # sorted array면 binary search...
+        # rotated면 pivot 기준으로 왼쪽도 단조 증가, 오른쪽도 단조 증가한다.
+        # nums[left] > nums[right]면 중간 어딘가에 pivot이 있다는 의미
+        # 반대로 nums[left] < nums[right]면 그 구간은 sorted된 상태인 건데...
+        # 그러면 min을 찾는 방법은 또 뭘까
+        # mid를 고려해보자
+        # nums[mid] > nums[right]면 최솟값이 오른쪽 구간에 있는 건가?
+        # 이 조건으로만 봐도 되는 건가?
         
-        return nums[left]
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return nums[lo]
