6392685966c50fe36340b120eb2156d8db33fdc9
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-08 12:12:11 +0900

    Time: 61 ms (52.34%), Space: 16.9 MB (26.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,24 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # 딱봐도 binary search인데...
-        # 길이가 12면 mid가 6일 때
         # mid를 matrix[r][c]로 치환할 수 있을까?
         # matrix[mid // C][mid % C] 요건가?
-        
-        R, C = len(matrix), len(matrix[0])
-        left, right = 0, R*C
-        
-        while left < right:
+        M, N = len(matrix), len(matrix[0])
+
+        left, right = 0, M * N - 1
+        while left <= right:
             mid = (left + right) // 2
-            item = matrix[mid // C][mid % C]
-            
-            if item == target:
+            num = matrix[mid // N][mid % N]
+            if target == num:
                 return True
             
-            if item < target:
-                left = mid + 1
+            if target < num:
+                right = mid - 1
             else:
-                right = mid
+                left = mid + 1
         
         return False
-            
