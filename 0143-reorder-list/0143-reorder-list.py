1c3ada8779aff4dc12c9017367f1fd109a8f5c44
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-12 22:39:41 +0900

    Time: 113 ms (18.78%), Space: 26 MB (93.18%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,55 +8,44 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # 3단계로 나눠서 풀기. 1. mid 구하기, 2. 오른쪽 반 reverse, 3. merge
-        # 1. mid 구하기. slow가 mid가 됨
-        slow, fast = head, head
-        while fast.next and fast.next.next:
-            slow = slow.next
+        # 3단계로 나눠야 할 듯?
+        # 1. mid 찾기
+        # 2. 오른쪽 리스트는 reverse
+        # 3. 왼쪽/오른쪽 리스트 합치기
+        # 코너 케이스
+        if not (head and head.next):
+            return head
+        
+        # 1. find mid
+        prev, slow, fast = None, head, head
+        while fast and fast.next:
+            prev = slow
             fast = fast.next.next
+            slow = slow.next
         
-        # 2. 오른쪽 반 뒤집기. 짝수일 때 생각해보면 slow.next부터 뒤집어야 함.
-        # 홀수일 때도 slow가 중간이니까 상관 없고..
-        prev, curr = None, slow.next
+        # slow == prev.next가 mid.
+        # 연결 끊기
+        mid = prev.next
+        prev.next = None
+        
+        # 2. 오른쪽 list reverse
+        prev, curr = None, mid
         while curr:
-            tmp_next = curr.next
-            
+            tmp = curr.next
             curr.next = prev
+            
             prev = curr
-            
-            curr = tmp_next
-        # 사이클 방지. 다 뒤집었으면 slow.next는 끝이 됨.
-        slow.next = None
+            curr = tmp
         
-        # 3. 두 LL 병합하기
-        head1, head2 = head, prev
-        while head1 and head2:
-            next1, next2 = head1.next, head2.next
-            
-            head1.next = head2
-            head1 = next1
-            
-            head2.next = head1
-            head2 = next2
-        
-#         # 아니면 스택을 이용할 수도 있음. 다 array에 집어넣고 ...
-#         s = []
-#         curr = head
-#         while curr:
-#             s.append(curr)
-#             curr = curr.next
-        
-#         curr = head
-#         mid = len(s) // 2
-#         for _ in range(mid):
-#             tmp = curr.next
-            
-#             curr.next = s.pop()
-#             curr = curr.next
-#             curr.next = tmp
-            
-#             curr = curr.next
-        
-#         # 사이클 방지
-#         if curr != None:
-#             curr.next = None
+        # 이 시점에서 prev가 이제 오른쪽 list의 head가 됨
+        # 3. 왼쪽/오른쪽 리스트 합치기
+        l1, l2 = head, prev
+        while l1 and l2:
+            tmp_l1_next = l1.next
+            tmp_l2_next = l2.next
+            l1.next = l2
+            # l2가 1개 더 많은 경우가 있기 때문에, 코너 케이스.
+            l2.next = tmp_l1_next if tmp_l1_next else tmp_l2_next
+
+            l1 = tmp_l1_next
+            l2 = tmp_l2_next
