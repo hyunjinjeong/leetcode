b0854130c40bed38c7c587a86cff9151eab8faf5
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-15 13:53:08 +0900

    Time: 82 ms (49.62%), Space: 16.4 MB (69.17%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,23 +4,34 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode()
-        node = dummy
-        
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry, number = 0, 0
+        dummy_head = ListNode()
+
         n1, n2 = l1, l2
-        carry = 0
-        while n1 or n2 or carry:
-            n1_val = n1.val if n1 else 0
-            n2_val = n2.val if n2 else 0
-            _sum = n1_val + n2_val + carry
-            remainder, carry = _sum % 10, _sum // 10
-            
-            node.next = ListNode(val=remainder)
-            node = node.next
+        new_node = dummy_head
+        while n1 and n2:
+            _sum = n1.val + n2.val + carry
+            carry, number = _sum // 10, _sum % 10
+
+            new_node.next = ListNode(number)
+            new_node = new_node.next
+
+            n1, n2 = n1.next, n2.next
+        
+        while n1 or n2:
+            _sum = (n1.val if n1 else n2.val) + carry
+            carry, number = _sum // 10, _sum % 10
+
+            new_node.next = ListNode(number)
+            new_node = new_node.next
+
             if n1:
                 n1 = n1.next
             if n2:
                 n2 = n2.next
-        
-        return dummy.next
+
+        if carry:
+            new_node.next = ListNode(carry)
+
+        return dummy_head.next
