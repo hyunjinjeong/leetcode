450aa08701fe2520cbe70207ee1c3c08fe7a6de9
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-16 18:00:57 +0900

    Time: 829 ms (60.44%), Space: 78.7 MB (13.95%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,73 +1,47 @@
 class LRUCache:
     # dict랑.. doubly linked list 쓰면 될 듯.
     def __init__(self, capacity: int):
-        self.ll = LinkedList()
-        # key -> Node 형태로...
-        self.dt = {}
-        self.capacity = capacity
+        self._capacity = capacity
+        self._dt = {}
+        self._head = Node(None, None)
+        self._tail = Node(None, None)
+        self._head.next = self._tail
+        self._tail.prev = self._head
 
     def get(self, key: int) -> int:
-        if key not in self.dt:
+        if key not in self._dt:
             return -1
         
-        node = self.dt[key]
-        self.ll.remove_node(node)
-        self.ll.append_node(node)
-        
+        node = self._dt[key]
+        self._remove(node)
+        self._append(node)
         return node.val
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.dt:
-            node = Node(key, value)
-            self.dt[key] = node
-            self.ll.append_node(node)
-        else:
-            # dt 업데이트 하고..
-            node = self.dt[key]
-            node.val = value
-            self.ll.remove_node(node)
-            self.ll.append_node(node)
+        if key in self._dt:
+            self._remove(self._dt[key]) # LL에서만 갱신하기 위해...
         
-        if len(self.dt) > self.capacity:
-            head_key = self.ll.head.key
-            self.dt.pop(head_key)
-            self.ll.remove_head()
-    
+        node = Node(key, value)
+        self._append(node)
+        self._dt[key] = node
         
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+        if len(self._dt) > self._capacity:
+            first = self._head.next
+            self._remove(first)
+            self._dt.pop(first.key)
     
-    def remove_head(self):
-        self.remove_node(self.head)
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
     
-    def remove_node(self, node):
-        if node is self.head and node is self.tail:
-            self.head = None
-            self.tail = None
-        elif node is self.head:
-            node_next = node.next
-            node_next.prev = None
-            self.head = node_next
-        elif node is self.tail:
-            node_prev = node.prev
-            node_prev.next = None
-            self.tail = node_prev
-        else:
-            node_next, node_prev = node.next, node.prev
-            node_next.prev = node_prev
-            node_prev.next = node_next
-    
-    def append_node(self, node):
-        if self.head is None:
-            self.head = node
-            self.tail = node
-            return
+    def _append(self, node):
+        prev_tail = self._tail.prev
         
-        node.prev = self.tail
-        self.tail.next = node
-        self.tail = node
+        prev_tail.next = node
+        node.next = self._tail
+        node.prev = prev_tail
+        self._tail.prev = node
+        
 
 class Node:
     def __init__(self, key, val):
@@ -75,6 +49,7 @@ class Node:
         self.val = val
         self.prev = None
         self.next = None
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
