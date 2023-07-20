3fd3feeb505723b36252f4a1a3864693c2e27a29
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-20 19:10:30 +0900

    Time: 135 ms (51.28%), Space: 17.5 MB (80.94%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,43 +6,29 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        self.ans = False
+        self.dfs(root, subRoot)
+        return self.ans
+    
+    def dfs(self, root, subRoot):
+        if not root:
+            return
         
-        def is_same(n1, n2):
-            if not n1 and not n2:
-                return True
-            if n1 and not n2:
-                return False
-            if not n1 and n2:
-                return False
-            if n1.val != n2.val:
-                return False
-            
-            return is_same(n1.left, n2.left) and is_same(n1.right, n2.right)
+        if self.is_same(root, subRoot):
+            self.ans = True
+            return
         
-#         s = [root]
-#         while s:
-#             n = s.pop()
-#             if is_same(n, subRoot):
-#                 return True
-            
-#             if n.left:
-#                 s.append(n.left)
-#             if n.right:
-#                 s.append(n.right)
+        self.dfs(root.left, subRoot)
+        self.dfs(root.right, subRoot)
         
-#         return False
+    def is_same(self, n1, n2):
+        if not n1 and not n2:
+            return True
+        if n1 and not n2:
+            return False
+        if not n1 and n2:
+            return False
         
-        self.is_sub_tree = False
-        def dfs(node):
-            if not node or self.is_sub_tree:
-                return
-            
-            if is_same(node, subRoot):
-                self.is_sub_tree = True
-                return
-            
-            dfs(node.left)
-            dfs(node.right)
-            
-        dfs(root)
-        return self.is_sub_tree
+        if n1.val != n2.val:
+            return False
+        return self.is_same(n1.left, n2.left) and self.is_same(n1.right, n2.right)
