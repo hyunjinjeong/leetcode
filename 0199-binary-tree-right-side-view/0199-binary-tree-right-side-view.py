48dd7fdd344cfd22813974827771c8500952db1e
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-23 11:12:45 +0900

    Time: 54 ms (24.14%), Space: 16.3 MB (44.92%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,38 +6,23 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-#         # 레벨 별로 가장 오른쪽에 있는 노드를 보여주면 된다!
-#         # 그러면 어떻게.. pre-order에서 node -> right -> left 순으로 찾는 거임.
-#         def traverse(node, level):
-#             if not node:
-#                 return
-            
-#             # 레벨 별로 가장 오른쪽에 있는 노드가 처음으로 여기 들어오게 된다
-#             if level == len(ans):
-#                 ans.append(node.val)
-            
-#             traverse(node.right, level+1)
-#             traverse(node.left, level+1)
-            
-#         ans = []
-#         traverse(root, 0)
+        # lever order traversal에서 맨 오른쪽꺼만 넣으면 되지 않나?
+        if not root:
+            return []
         
-#         return ans
-
-        # 요건 BFS 버전. 좀 더 직관적인 듯?
         ans = []
-        q = collections.deque()
-        if root:
-            q.append(root)
+        q = collections.deque([root])
 
         while q:
-            for i in range(len(q)):
+            level_size = len(q)
+            for i in range(level_size):
                 node = q.popleft()
-                if i == 0:
-                    ans.append(node.val) # 레벨별로 맨 왼쪽에 있는 친구가 맨 오른쪽 노드
-                if node.right:
-                    q.append(node.right)
+                if i == level_size - 1:
+                    ans.append(node.val)
+                
                 if node.left:
                     q.append(node.left)
+                if node.right:
+                    q.append(node.right)
 
         return ans
