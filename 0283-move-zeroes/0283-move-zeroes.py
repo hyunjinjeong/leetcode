46d7cf7d91346d446e2c7300144ade11ae6b04ec
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-07-24 15:42:04 +0900

    Time: 170 ms (86.55%), Space: 18 MB (52.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,52 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-#         # 1. 요게 젤 간단한 버전.
-#         zero_count = 0
-#         for num in nums:
-#             if num == 0:
-#                 zero_count += 1
-        
-#         tmp = []
-#         for num in nums:
-#             if num != 0:
-#                 tmp.append(num)
-        
-#         for _ in range(zero_count):
-#             tmp.append(0)
-        
-#         i = 0
-#         while i < len(nums):
-#             nums[i] = tmp[i]
-#             i += 1
-        
-#         # 2. 간단하게
-#         pos_last_non_zero = 0
-        
-#         # 일단 0이 아닌 애들을 미리 앞으로 땡겨준다. 
-#         for i in range(len(nums)):
-#             if nums[i] != 0:
-#                 nums[pos_last_non_zero] = nums[i]
-#                 pos_last_non_zero += 1
-        
-#         # non-zero는 이미 앞에 다 와있음. 나머지는 0으로 채워준다.
-#         for i in range(pos_last_non_zero, len(nums)):
-#             nums[i] = 0
-        
-        # # 3. two-pointer 사용하는 방법도 있다..ㄷㄷ
-        # slow = 0
-        # # 이러면 모든 non-zero가 앞으로 오고, 0은 다 뒤로 가게 됨.
-        # for fast in range(len(nums)):
-        #     if nums[fast] != 0:
-        #         nums[fast], nums[slow] = nums[slow], nums[fast]
-        #         slow += 1
-        
-        # 4. 3번에서 operation 수를 줄인 버전.
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != 0 and nums[slow] == 0:
-                nums[fast], nums[slow] = nums[slow], nums[fast]
-                slow += 1
-            
-            if nums[slow] != 0:
-                slow += 1
+        # 이러면 non-zero 갯수만큼만 연산.. 최적해.
+        last_non_zero_index = 0
+        for i, num in enumerate(nums):
+            if num != 0:
+                nums[last_non_zero_index], nums[i] = nums[i], nums[last_non_zero_index]
+                last_non_zero_index += 1
