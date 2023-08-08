8dc761e3b9a90ab18d3b07a83e0156164ff0c192
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-08 19:54:21 +0900

    Time: 81 ms (98.98%), Space: 20.1 MB (46.83%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,25 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # heap을 사용하는데 k개만큼만 넣으면서 하는 방법도 있다!
-        # 이 경우 전체 갯수를 n개라고 하면 시간은 O(nlogk), 공간은 O(k).
-        heap = []
-        for i, node in enumerate(lists):
-            # index를 넣는건 heapq 모듈이 tuple 순서대로 비교하기 떄문에..
-            # 값이 같은 경우 tie breaker임.
-            if node:
-                heapq.heappush(heap, (node.val, i, node))
-        
+        # heap에다가 넣고 하나씩 뽑으면 되지 않을까..?
         dummy_head = ListNode()
-        node = dummy_head
+
+        heap = []
+        for index, node in enumerate(lists):
+            if node:
+                # index는 tie breaker... 
+                heapq.heappush(heap, (node.val, index, node))
+                index += 1
+
+        curr = dummy_head
         while heap:
-            # 가장 작은 원소를 꺼내서 연결하고...
-            _val, index, temp_node = heapq.heappop(heap)
-            
-            node.next = temp_node
-            node = node.next
-            # 그 다음꺼 heap에다가 넣기
+            val, index, node = heapq.heappop(heap)
             if node.next:
                 heapq.heappush(heap, (node.next.val, index, node.next))
-        
+
+            curr.next = node
+            curr = curr.next
+
         return dummy_head.next
