6000d5ee160d4e057d79bccd2c92d7a4b12363bf
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-15 17:26:16 +0900

    Time: 456 ms (86.6%), Space: 29.6 MB (54.97%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,47 +1,28 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-#         # 1. 일단 min heap 버전
-#         # 시간은 nlogk? 공간은 O(k).
-#         heap = []
-#         # 먼저 k개만큼 넣어서 크기 k로 만들어주고...
-#         for i in range(k):
-#             heapq.heappush(heap, nums[i])
-#         # 다음엔 하나씩 넣고 뺴고 하다보면 작은 순서로 쭉쭉 빠져나가니까 k번째 큰 수가 남음.
-#         for i in range(k, len(nums)):
-#             heapq.heappushpop(heap, nums[i])
-        
-#         # 이제 root가 k번째 큰 원소
-#         return heapq.heappop(heap)
-        
-        # 2. quickselect 버전
-        return self.quick_select(nums, 0, len(nums)-1, k)
-    
-    def quick_select(self, nums, start, end, k):
-        
-        def partition(left, right):
-            pivot_index = random.randint(left, right)
-            # swap을 쉽게 하기 위해 맨 마지막으로 보냄
-            nums[right], nums[pivot_index] = nums[pivot_index], nums[right]
-            
-            pivot = nums[right]
-            for i in range(left, right+1):
-                if nums[i] >= pivot: # kth largest니까 pivot보다 큰 친구들을 왼쪽으로 보냄
-                    nums[left], nums[i] = nums[i], nums[left]
-                    left += 1
-            
-            # 이 시점에서 left는 pivot보다 작은 첫 번째 원소 위치 (swap 후 +1 되므로)
-            # 따라서 -1을 해야 pivot의 위치가 됨
-            return left - 1
-        
-        l, r = 0, len(nums)-1
+        # Heap 이용하면 O(nlogk)으로 해결 가능하지만..
+        # O(n)으로 가능한 quickselect를 구현해보자.
+        left, right = 0, len(nums) - 1
         while True:
-            pivot_index = partition(l, r)
-            
-            # zero-based index니까 k-1 사용.
-            if pivot_index == k-1:
+            pivot_index = self.partition(nums, left, right)
+            if pivot_index == k - 1: # zero-based index라 k-1
                 return nums[pivot_index]
             
-            if pivot_index < k-1:
-                l = pivot_index + 1
-            else:
-                r = pivot_index - 1
+            if pivot_index < k - 1: # pivot보다 큰 원소가 k개보다 적은 경우. 즉 답이 오른쪽 그룹(pivot보다 작은)에 있음.
+                left = pivot_index + 1
+            else: # 반대로 답이 왼쪽 그룹(pivot보다 큰)에 있는 경우.
+                right = pivot_index - 1
+
+    def partition(self, nums, left, right):
+        pivot = random.randint(left, right) # 무작위로 선택
+        nums[right], nums[pivot] = nums[pivot], nums[right] # swap 편하도록 맨 뒤로
+
+        pivot_num = nums[right]
+        for i in range(left, right+1):
+            if nums[i] >= pivot_num: # 문제 조건이 kth largest니까 pivot과 같거나 큰 원소를 왼쪽으로 보냄
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+            
+        # 이 시점에서 left는 pivot보다 작은 첫 번째 원소 위치 (swap 후 +1 되므로)
+        # 따라서 -1을 해야 pivot의 위치가 됨
+        return left - 1
