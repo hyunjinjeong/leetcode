0e7c584d1f5c0c001c780fca44ed77cd2ee65715
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-16 22:29:50 +0900

    Time: 527 ms (68.62%), Space: 16.9 MB (48.58%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,47 +1,18 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-#         # 1. 갯수가 많은 task 순서대로 사이클 돌리면서 greedy하게 빼내야 되나 보다.
-#         # 그러면 갯수가 많은 task 순서대로 n+1개 뽑기 <- 이걸 해야 하는데
-#         # counter.most_common을 써서 일단 해보자.
-        
-#         ans = 0
-#         while True:
-#             most_common_tasks = counter.most_common(n+1)
-#             len_most_common_tasks = len(most_common_tasks)
-#             for task, _count in most_common_tasks:
-#                 counter[task] -= 1
-#                 ans += 1
-#                 if counter[task] == 0:
-#                     del counter[task]
-            
-#             if not counter:
-#                 return ans
-                
-#             if len_most_common_tasks < n+1:
-#                 ans += n+1 - len_most_common_tasks
-#
-#         # 2. 허허.. 식으로 풀 수도 있다.
-#         # 결국 시간은 max frequency를 가진 task에 의해 결정됨. n+1개씩 max_freq-1번 반복.
-#         # 마지막에는 max frequency를 가진 task의 갯수와 같고...
-#         freq = Counter(tasks)
-#         freq_values = freq.values()
-        
-#         max_freq = max(freq_values)
-#         freq = list(freq_values)
-#         last_row = freq.count(max_freq)
-        
-#         ans = (max_freq - 1) * (n + 1) + last_row
-#         return max(len(tasks), ans)
-        
-#         counter = collections.Counter(tasks)
-        # 3. 1번을 heap을 사용해서 직접 구현해보기
+        # heap을 사용하면..
+        # 스케줄된 task index를 t_i라고 했을 때
+        # heap에다가 t_i를 넣고
+        # 현재 i - n > t_i 인지를 검사하면 되려나?
+        # 아 이렇게하면 안 되는게 가장 갯수가 많은 task를 최대한 빨리 줄여야 함.
+        # greedy. n개씩 사이클을 돌려야 함
+
         ans = 0
         heap = []
         
         counter = Counter(tasks)
-        # heap 초기화
-        for task, freq in counter.items():
-            heapq.heappush(heap, freq * -1)
+        for freq in counter.values():
+            heapq.heappush(heap, freq * -1) # freq가 높은 task가 우선 순위가 높도록
         
         while heap:
             temp = []
@@ -59,4 +30,3 @@ class Solution:
                 heapq.heappush(heap, negative_freq)
         
         return ans
-                        
