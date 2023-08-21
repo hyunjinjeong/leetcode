8c55f4b77c52a3f410a7bff634466a4bde818155
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-21 21:55:54 +0900

    Time: 461 ms (69.73%), Space: 38.2 MB (69.5%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,30 +1,27 @@
 class MedianFinder:
-    # 오...  max heap과 min heap을 둘 다 쓰는게 포인트였음.
-    # max heap은 small half, min heap은 large half.
+    # min_heap과 max_heap을 둘 다 써서 구현...
+    # min_heap은 right half, max_heap은 left half
+    # 빼고 넣고 반복하면 min_heap은 큰 원소 n개가 남고 max_heap은 작은 원소 n개
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.max_heap = [] # small half
-        self.min_heap = [] # large half
-        
+        self.min_heap = []
+        self.max_heap = []
 
     def addNum(self, num: int) -> None:
-        # 먼저 max_heap에 넣고, 조정
-        heapq.heappush(self.max_heap, -num)
-        heapq.heappush(self.min_heap, -heappop(self.max_heap))
-        # max_heap에 원소가 1개 더 들어가도록 함.
-        if len(self.min_heap) > len(self.max_heap):
-            heappush(self.max_heap, -heappop(self.min_heap))
+        # 홀수개면 1개는 min_heap으로 넣도록.
+        heapq.heappush(self.max_heap, -1 * num)
+        heapq.heappush(self.min_heap, -1 * heapq.heappop(self.max_heap))
+        
+        if len(self.min_heap) > len(self.max_heap) + 1: # 차이가 2개 이상 벌어지면.. 즉 기존에 min_heap이 1개 더 많았던거
+            heapq.heappush(self.max_heap, -1 * heapq.heappop(self.min_heap))
 
     def findMedian(self) -> float:
-        # 원소 갯수가 even인 경우
-        if len(self.max_heap) == len(self.min_heap):
-            return (-self.max_heap[0] + self.min_heap[0]) / 2
-        else: # 원소 갯수가 odd인 경우
-            return float(-self.max_heap[0])
-    
-        
+        if len(self.min_heap) == len(self.max_heap):
+            return (self.min_heap[0] + -1 * self.max_heap[0]) / 2
+        else:
+            return self.min_heap[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
