4b8e4f757e171a0f618963980efcc90d841ab70e
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-25 21:25:13 +0900

    Time: 36 ms (79.34%), Space: 16.2 MB (83.14%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,7 +3,7 @@ class Solution:
         if not digits:
             return []
 
-        mappings = { "2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"], "5": ["j", "k", "l"], "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
+        mappings = { "2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         return self.backtrack(0, [], mappings, digits, [])
     
     def backtrack(self, index, curr, mappings, digits, ans):
@@ -11,8 +11,8 @@ class Solution:
             ans.append("".join(curr))
             return ans
         
-        for c in mappings[digits[index]]:
-            curr.append(c)
+        for letter in mappings[digits[index]]:
+            curr.append(letter)
             self.backtrack(index + 1, curr, mappings, digits, ans)
             curr.pop()
         
