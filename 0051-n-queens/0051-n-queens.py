33ae55dfb819e7b989746845ee129bfe5f4084b5
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-26 20:17:12 +0900

    Time: 87 ms (34.55%), Space: 16.9 MB (47.37%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,40 +1,48 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # 대표적인 backtracking 문제..
-        # 퀸은 대각선 포함 모든 방향으로 이동할 수 있음.
-        # valid면 놓고 아니면 빼고...
-        # row 단위로 이동하기.
+        empty = [["."] * n for _ in range(n)]
+        return self.backtrack(0, empty, n, [])
+    
+    # row 단위로...
+    # 왼쪽 대각선 위, 오른쪽 대각선 위, 위로 있는지 세 가지 경우 확인 필요.
+    def backtrack(self, row, curr, n, ans):
+        if row == n:
+            ans.append(["".join(s) for s in curr])
+            return ans
         
-        def is_valid(row, col):
-            # upper column
-            for r in range(row):
-                if board[r][col] == "Q":
-                    return False
-            # upper left diagonal
-            for i in range(n):
-                if row - i >= 0 and col - i >= 0:
-                    if board[row-i][col-i] == "Q":
-                        return False
-            # upper right diagonal
-            for i in range(n):
-                if row - i >= 0 and col + i < n:
-                    if board[row-i][col+i] == "Q":
-                        return False
-            return True
-        
-        def dfs(row):
-            if row == n:
-                ans.append(["".join(row) for row in board])
-                return
-            
-            for col in range(n):
-                if is_valid(row, col):
-                    board[row][col] = "Q"
-                    dfs(row + 1)
-                    board[row][col] = "."
-        
-        ans = []
-        board = [["."] * n for _ in range(n)]
-        dfs(0)
+        for col in range(n):
+            if self.can_place_queen(row, col, curr):
+                curr[row][col] = "Q"
+                self.backtrack(row + 1, curr, n, ans)
+                curr[row][col] = "."
         
         return ans
+    
+    def can_place_queen(self, row, col, curr):
+        return self.check_upper(row, col, curr) and self.check_left_diagonal(row, col, curr) and self.check_right_diagonal(row, col, curr)
+    
+    def check_upper(self, row, col, curr):
+        r = row - 1
+        while r >= 0:
+            if curr[r][col] == "Q":
+                return False
+            r -= 1
+        return True
+    
+    def check_left_diagonal(self, row, col, curr):
+        r, c = row - 1, col - 1
+        while r >= 0 and c >= 0:
+            if curr[r][c] == "Q":
+                return False
+            r -= 1
+            c -= 1
+        return True
+    
+    def check_right_diagonal(self, row, col, curr):
+        r, c = row - 1, col + 1
+        while r >= 0 and c < len(curr):
+            if curr[r][c] == "Q":
+                return False
+            r -= 1
+            c += 1
+        return True
