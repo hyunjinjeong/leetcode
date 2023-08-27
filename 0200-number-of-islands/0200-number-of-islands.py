2c72e7d10e85172f889597cf3c61ff54457c49ed
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-27 20:07:07 +0900

    Time: 287 ms (79.37%), Space: 18.7 MB (88.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        def dfs(row, col):
-            if grid[row][col] == "0":
-                return
-            
-            grid[row][col] = "0"
-            
-            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            for dx, dy in directions:
-                next_row, next_col = row + dx, col + dy
-                if 0 <= next_row < len(grid) and 0 <= next_col < len(grid[0]):
-                    dfs(next_row, next_col)
-            
-        result = 0
+        ans = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                num = grid[row][col]
-                if num == "1":
-                    result += 1
-                    dfs(row, col)
+                if self.is_island(row, col, grid):
+                    ans += 1
+        return ans
+    
+    def is_island(self, row, col, grid):
+        val = grid[row][col]
+        if val != "1":
+            return False
         
-        return result
+        grid[row][col] = "#" # visited
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            next_row, next_col = row + dx, col + dy
+            if 0 <= next_row < len(grid) and 0 <= next_col < len(grid[0]) and grid[next_row][next_col] == "1":
+                self.is_island(next_row, next_col, grid)
+
+        return True
