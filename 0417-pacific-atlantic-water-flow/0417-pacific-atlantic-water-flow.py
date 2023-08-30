1bbd46ad5389902106b59756f2ea93b1bd43d9e8
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-30 21:10:43 +0900

    Time: 229 ms (95.55%), Space: 19.5 MB (9.94%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,44 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ans = []
-        for x in range(len(heights)):
-            for y in range(len(heights[0])):
-                if self.is_pacific_valid(x, y, heights) and self.is_atlantic_valid(x, y, heights):
-                    ans.append([x, y])
-        return ans
+        M, N = len(heights), len(heights[0])
+        p_visited, a_visited = set(), set()
+
+        for row in range(M):
+            self.dfs(row, 0, p_visited, heights)
+            self.dfs(row, N - 1, a_visited, heights)
+        
+        for col in range(N):
+            self.dfs(0, col, p_visited, heights)
+            self.dfs(M - 1, col, a_visited, heights)
+        
+        return [[dup[0], dup[1]] for dup in p_visited & a_visited]
     
-    def is_pacific_valid(self, x, y, heights):
-        if x == 0 or y == 0:
-            return True
-
-        ans = False
-
-        current_val = heights[x][y]
-        heights[x][y] = "#"
+    # 조건을 반대로 해서 edge에서 각 점에 도달 가능하면 됨. p, a 모두 도달한 점들이 답.
+    def dfs(self, x, y, visited, heights):
+        if (x, y) in visited:
+            return
+        
+        visited.add((x, y))
         for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             next_x, next_y = x + dx, y + dy
-            if 0 <= next_x < len(heights) and 0 <= next_y < len(heights[0]) and heights[next_x][next_y] != "#" and heights[next_x][next_y] <= current_val:
-                ans = self.is_pacific_valid(next_x, next_y, heights) or ans
-                if ans:
-                    break
-        heights[x][y] = current_val
-        
-        return ans
-    
-    def is_atlantic_valid(self, x, y, heights):
-        if x == len(heights) - 1 or y == len(heights[0]) - 1:
-            return True
-
-        ans = False
-
-        current_val = heights[x][y]
-        heights[x][y] = "#"
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            next_x, next_y = x + dx, y + dy
-            if 0 <= next_x < len(heights) and 0 <= next_y < len(heights[0]) and heights[next_x][next_y] != "#" and heights[next_x][next_y] <= current_val:
-                ans = self.is_atlantic_valid(next_x, next_y, heights) or ans
-                if ans:
-                    break
-        heights[x][y] = current_val
-        
-        return ans
+            if 0 <= next_x < len(heights) and 0 <= next_y < len(heights[0]) and (next_x, next_y) not in visited and heights[next_x][next_y] >= heights[x][y]:
+                self.dfs(next_x, next_y, visited, heights)
