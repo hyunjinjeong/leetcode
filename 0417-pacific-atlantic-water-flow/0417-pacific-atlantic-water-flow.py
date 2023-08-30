81f2a7a25f5b5cf28bd6254ac669e9af24d84098
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-08-30 21:12:22 +0900

    Time: 241 ms (81.03%), Space: 19.7 MB (8.11%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -15,10 +15,8 @@ class Solution:
     
     # 조건을 반대로 해서 edge에서 각 점에 도달 가능하면 됨. p, a 모두 도달한 점들이 답.
     def dfs(self, x, y, visited, heights):
-        if (x, y) in visited:
-            return
-        
         visited.add((x, y))
+
         for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             next_x, next_y = x + dx, y + dy
             if 0 <= next_x < len(heights) and 0 <= next_y < len(heights[0]) and (next_x, next_y) not in visited and heights[next_x][next_y] >= heights[x][y]:
