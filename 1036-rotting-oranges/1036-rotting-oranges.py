4301af12b3277d88e01533a235b4b1b933d88530
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-01 09:34:45 +0900

    Time: 62 ms (48.55%), Space: 16.4 MB (22.49%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -0,0 +1,34 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # 이건 BFS 같은데?
+        # 2인 cell들을 처음에 골라서 넣어두고 N번 돌고..
+        # 근데도 1인 cell이 있으면 -1인거
+        M, N = len(grid), len(grid[0])
+        
+        q = collections.deque()
+        fresh_orange_count = 0
+        
+        for r in range(M):
+            for c in range(N):
+                orange = grid[r][c]
+                if orange == 2:
+                    q.append((r, c))
+                elif orange == 1:
+                    fresh_orange_count += 1
+        
+        if fresh_orange_count == 0:
+            return 0
+        
+        minute = -1
+        while q:
+            minute += 1
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    next_r, next_c = r + dr, c + dc
+                    if 0 <= next_r < M and 0 <= next_c < N and grid[next_r][next_c] == 1:
+                        grid[next_r][next_c] = 2
+                        fresh_orange_count -= 1
+                        q.append((next_r, next_c))
+        
+        return minute if fresh_orange_count == 0 else -1
