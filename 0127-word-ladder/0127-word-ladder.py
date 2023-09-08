548b7c7580d7684bc4d16439b3e51aed64f9e7bc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-08 21:42:51 +0900

    Time: 412 ms (39.08%), Space: 17.9 MB (76.41%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,11 +1,8 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # dict에 일단 저장해서 BFS를 돌린다..?
-        # 저장하는 형식은 어떻게 하지
-        # 일단 1글자만 다른 것들을 찾을 수 있어야 하는데...
-        # hot이라고 치면 hot: [dot, lot] 이런 식.
-        
-        # 먼저 hash table을 저장하고 하니까 O(n^2 * m)이고.. TLE 뜸
+        # 어떻게든 그래프를 만들어놓고 BFS 돌리기?
+        # beginWord에서 시작해서.. 1글자씩만 다른 친구들이 neighbor인데
+        # 어떻게 구하지
         # 미리 만들어둘 필요가 없었다.
         # 알파벳 소문자로만 이루어져 있다는 조건을 이용.
         # 가능한 모든 문자열을 돌면서 테스트.. (그래봐야 26개. O(1)이니까.)
@@ -30,4 +27,3 @@ class Solution:
                         word_set.remove(candidate)
 
         return 0
-                
