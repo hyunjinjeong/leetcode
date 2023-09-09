e05a832a01f6b2dc9304638912cfe855de15191c
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-09 17:14:18 +0900

    Time: 79 ms (92.75%), Space: 16.6 MB (91.75%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -6,36 +6,24 @@ class Solution:
         # 아니면 heap 사용하는 방법도 있을 것 같음. 
         # DFS로 돌면서 가능한 첫번째 리턴하면 그게 정답..?
         # 정렬 + deque 써보자..
+        # => 그냥 stack으로만 풀 수 있었음.
         graph = collections.defaultdict(list)
         
         # 그래프 초기화. O(N)
-        for _from, _to in tickets:
-            graph[_from].append(_to)
+        for src, dst in tickets:
+            graph[src].append(dst)
         
-        # 정렬 + deque. O(N * NlogN)
-        for key in graph:
-            graph[key].sort()
-            graph[key] = collections.deque(graph[key])
+        # 정렬. O(N * NlogN). pop() 연산을 효율적으로 하기 위해 reverse 정렬.
+        for src in graph:
+            graph[src].sort(reverse=True)
         
-        self.ans = []
-        self.ans_found = False
-        self.dfs(["JFK"], graph, tickets)
-        return self.ans
-    
-    def dfs(self, curr_path, graph, tickets):
-        if self.ans_found:
-            return
-        if len(curr_path) == len(tickets) + 1:
-            for key in curr_path:
-                self.ans.append(key)
-            self.ans_found = True
-            return
-
-        key = curr_path[-1]
-        dests = graph[key]
-        for _ in range(len(dests)):
-            next_key = dests.popleft()
-            curr_path.append(next_key)
-            self.dfs(curr_path, graph, tickets)
-            curr_path.pop()
-            dests.append(next_key)
+        ans = []
+        stack = ["JFK"]
+        while stack:
+            next_src = stack[-1]
+            if graph[next_src]: # 여기 핵심 아이디어는 더 이상 갈 곳이 없는 경로는 뒤에 추가 되어야 한다는 점.
+                stack.append(graph[next_src].pop())
+            else:
+                ans.append(stack.pop())
+        
+        return ans[::-1] # 따라서 마지막 결과를 reverse 함
