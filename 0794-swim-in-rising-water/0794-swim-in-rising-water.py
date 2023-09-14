c9066a72286809182e9c202ef47126e4b1cd3f43
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-14 22:44:40 +0900

    Time: 119 ms (41.47%), Space: 16.9 MB (67.51%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -4,32 +4,27 @@ class Solution:
         # 시간에 따라서 슉슉 합치고
         # (0, 0), (n-1, n-1)이 같은 부모가 되면 해당 시간 리턴하는 식으로...
         # heap 쓰면 매번 돌지 않고 뽑아낼 수 있음...
-        def get_pos(r, c):
+        # 아 grid[i][j] 값이 0이랑 n^2 사이라서 heap 없이 그냥 돌면 됨.
+        # 그 제약조건 때문에 DFS로 타고 들어갈 필요도 없고...
+        def flatten(r, c):
             return r * N + c
 
-        def merge(r, c):
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                next_r, next_c = r + dr, c + dc
-                if 0 <= next_r < N and 0 <= next_c < N and grid[next_r][next_c] <= grid[r][c]:
-                    pos = get_pos(r, c)
-                    next_pos = get_pos(next_r, next_c)
-                    if uf.union(pos, next_pos):
-                        merge(next_r, next_c)
-
         N = len(grid)
-
-        # heap 만들기
-        heap = []
+        pos = {}
+        
         for r in range(N):
             for c in range(N):
-                heap.append((grid[r][c], r, c))
-        heapq.heapify(heap)
+                pos[grid[r][c]] = (r, c)
 
         uf = UnionFind(N * N)
-        while heap:
-            time, r, c = heapq.heappop(heap)
-            merge(r, c)
-            if uf.find(get_pos(0, 0)) == uf.find(get_pos(N-1, N-1)):
+        for time in range(N * N):
+            r, c = pos[time]
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                next_r, next_c = r + dr, c + dc
+                if 0 <= next_r < N and 0 <= next_c < N and grid[next_r][next_c] <= time:
+                    uf.union(flatten(r, c), flatten(next_r, next_c))
+            
+            if uf.find(flatten(0, 0)) == uf.find(flatten(N-1, N-1)):
                 return time
 
 class UnionFind:
@@ -45,13 +40,11 @@ class UnionFind:
     def union(self, u, v):
         pu, pv = self.find(u), self.find(v)
         if pu == pv:
-            return False
-        
+            return
+
         if self.size[pu] > self.size[pv]:
             self.parent[pv] = pu
             self.size[pu] += self.size[pv]
         else:
             self.parent[pu] = pv
             self.size[pv] += self.size[pu]
-        
-        return True
