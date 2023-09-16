37c033892c494c9ffbd90e0a43f493a8731647bf
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-16 14:48:51 +0900

    Time: 40 ms (56.52%), Space: 16.2 MB (69.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,37 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-#         # dp[n]을 n일 때의 최댓값이라고 하면.. dp[n] = max(dp[n-1], dp[n-2]+nums[n]) 이렇게 되지 않을까
-#         # 아.. 2, 1, 1, 2 이런 경우는 이게 안되네.
-#         # 그럼 dp[n-1]이랑 dp[n-2] + nums[n], dp[n-3] + nums[n]... 이렇게 다 비교해야 할 듯?
-#         dp = [0] * len(nums)
-        
-#         # 엣지 케이스
-#         dp[0] = nums[0]
-#         if len(nums) > 1:
-#             dp[1] = nums[1]
-        
-#         for i in range(2, len(nums)):
-#             for j in range(i-2, -1, -1):
-#                 # 중간에 덮어 씌워질 수 있어서 자기 자신과도 비교해야     
-#                 dp[i] = max(dp[i], dp[i-1], dp[j]+nums[i])
-        
-#         return max(dp)
+        # dp. dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        N = len(nums)
+        # 엣지 케이스
+        if N == 1:
+            return nums[0]
+        if N == 2:
+            return max(nums[0], nums[1])
 
-#         # 위에꺼 좀 더 최적화해보면 저 중간에 j for문을 없앨 수 있을 듯
-#         dp = [0] * (len(nums)+1)
-#         dp[1] = nums[0]
+        dp = [0] * N
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, N):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
         
-#         for i in range(1, len(nums)):
-#             dp[i+1] = max(dp[i], dp[i-1] + nums[i])
+        return dp[N-1]
+            
+
         
-#         return dp[-1]
-    
-        # 또 최적화하면, dp에서 마지막 2개만 쓰고 있으니까 (dp[i], dp[i-1]) 변수 2개로 바꿀 수 있음
-        prev, curr = 0, 0
-        
-        for num in nums:
-            temp = max(curr, prev + num)
-            prev = curr
-            curr = temp
-        
-        return curr
