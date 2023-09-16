0c4268c1bdecc7317af7537b053ed9af614552bc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-16 13:02:59 +0900

    Time: 62 ms (59.24%), Space: 16.3 MB (87.36%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -2,19 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # dp...
         # 근데 0에서 시작할 수도 있고 1에서 시작할 수도 있음
-        # dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        # 뒤에서부터 거꾸로 돌면 됨
         N = len(cost)
-
-        # dp = [0] * N
-        # dp[N-1] = cost[N-1]
-        # dp[N-2] = cost[N-2]
-
-        # for i in range(N-3, -1, -1):
-        #     dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        
-        # return min(dp[0], dp[1])
-
         dp = [0] * N
         dp[0] = cost[0]
         dp[1] = cost[1]
