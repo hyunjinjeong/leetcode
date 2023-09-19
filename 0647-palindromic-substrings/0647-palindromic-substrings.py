7cb180dd3c9ea1f3ae75e84228ca859ef6a9bc98
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-19 23:08:04 +0900

    Time: 213 ms (25.09%), Space: 24.3 MB (18.59%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,24 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # 아까 문제랑 같은거 아닌가?
-        # dp로 palindrome이면 True로 해서 True의 갯수를 세면 될 것 같은데
-        
-        dp = [[False] * len(s) for _ in range(len(s))]
-        answer = 0
-        
-        # 길이 1짜리 초기화
-        for i in range(len(s)):
-            dp[i][i] = True
-            answer += 1
-            
-        for end in range(len(s)):
-            for start in range(end-1, -1, -1):
-                if s[start] != s[end]:
-                    continue
-                
-                # 시작과 끝이 같은 경우.
-                if end - start == 1 or dp[start+1][end-1]:
+        N = len(s)
+        dp = [[False] * N for _ in range(N)]
+
+        ans = 0
+        for start in range(N-1, -1, -1):
+            for end in range(start, N):
+                if start == end:
                     dp[start][end] = True
-                    answer += 1
+                elif start + 1 == end:
+                    dp[start][end] = s[start] == s[end]
+                else:
+                    dp[start][end] = s[start] == s[end] and dp[start+1][end-1]
+                
+                if dp[start][end]:
+                    ans += 1
         
-        return answer
+        return ans
