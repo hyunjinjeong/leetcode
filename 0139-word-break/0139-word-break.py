d325de50e4d9665e15494f9ab47e33e9383f85e8
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-21 15:25:13 +0900

    Time: 51 ms (22.06%), Space: 16.4 MB (56.76%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -6,18 +6,28 @@ class Solution:
         dp = [False] * (N + 1)
         dp[0] = True
 
-        for end in range(N + 1):
-            for word in wordDict:
-                word_len = len(word)
-                start = end - word_len
-                if start < 0:
-                    continue
-                if not dp[start]:
-                    continue
+        # for end in range(N + 1):
+        #     for word in wordDict:
+        #         word_len = len(word)
+        #         start = end - word_len
+        #         if start < 0:
+        #             continue
+        #         if not dp[start]:
+        #             continue
                 
-                sub_str = s[start:end]
-                if sub_str in word_dict_set:
+        #         sub_str = s[start:end]
+        #         if sub_str in word_dict_set:
+        #             dp[end] = True
+        #             break
+
+        # 이렇게 하는게 더 깔끔한 듯?
+        for start in range(N):
+            if not dp[start]:
+                continue
+            
+            for end in range(start + 1, N + 1):
+                word = s[start:end]
+                if word in word_dict_set:
                     dp[end] = True
-                    break
 
         return dp[N]
