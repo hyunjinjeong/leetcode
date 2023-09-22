ba9871284484d650ac863c1e2615bc397975c910
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-22 20:43:47 +0900

    Time: 2560 ms (18.95%), Space: 16.3 MB (99.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,24 +1,12 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        
-        def binary_search(target, left, right):
-            while left < right:
-                mid = (left + right) // 2
-                if tails[mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid
+        N = len(nums)
+        # dp
+        dp = [1] * len(nums)
+
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[j] + 1, dp[i])
             
-            return left
-            
-        
-        # binary search를 이용하면 O(nlogn) 가능
-        # tails[i]는 길이가 i+1인 LIS의 tail을 의미함.
-        tails = []
-        for num in nums:
-            idx = binary_search(num, 0, len(tails))
-            if idx == len(tails):
-                tails.append(num)
-            else:
-                tails[idx] = num
-        return len(tails)
+        return max(dp)
