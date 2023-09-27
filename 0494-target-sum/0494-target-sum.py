fa0baf1ab7b10521718e41f8996fc345971fd4c2
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-27 15:25:58 +0900

    Time: 298 ms (18.25%), Space: 43.7 MB (10.66%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # brute force로 풀면 2^n이고...
+        # top down으로 풀면 쉬움
+
+        dp = {}
+
+        def dfs(i, curr_value):
+            if (i, curr_value) in dp:
+                return dp[(i, curr_value)]
+            if i == len(nums):
+                dp[(i, curr_value)] = 1 if curr_value == target else 0
+                return dp[(i, curr_value)]
+
+            positive = dfs(i + 1, curr_value + nums[i])
+            negative = dfs(i + 1, curr_value - nums[i])
+            
+            dp[(i, curr_value)] = positive + negative
+            return dp[(i, curr_value)]
+        
+        return dfs(0, 0)
