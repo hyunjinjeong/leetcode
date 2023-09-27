96d19683aea56508ff8ca56df4a35119f5023a9d
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-09-27 15:02:36 +0900

    Time: 183 ms (60.7%), Space: 16.3 MB (99.5%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # dp.
+
+        dp = [0] * (amount + 1)
+        dp[0] = 1 # 베이스 케이스
+
+        for coin in coins:
+            for i in range(1, amount + 1):
+                prev = i - coin
+                if prev < 0:
+                    continue
+                
+                dp[i] += dp[prev]
+
+        return dp[amount]
+
+        # 1: 1 => 1
+        # 2: 1+1, 2 => 2
+        # 3: 1+1+1, 2+1 => 2
+        # 4: 1+1+1+1, 2+2, 2+1+1 => 3
+        # 5: 1+1+1+1+1, 2+2+1, 2+1+1+1, 5 => 4
