62ed66fc0cb544257e7fd819f3f4e5c4940fd2e4
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-15 16:08:25 +0900

    Time: 385 ms (96.23%), Space: 17.5 MB (45.29%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,29 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_index = 0
+        # # dp?
+        # N = len(nums)
+
+        # dp = [False] * N
+        # dp[N - 1] = True
+
+        # for i in range(N - 2, -1, -1):
+        #     max_len = nums[i]
+
+        #     if i + max_len >= N - 1:
+        #         dp[i] = True
+        #     else:
+        #         for length in range(1, max_len + 1):
+        #             dp[i] = dp[i + length] or dp[i]
+        #             if dp[i]:
+        #                 break
         
-        for i, length in enumerate(nums):
-            if i > max_index:
-                return False
-            
-            max_index = max(max_index, i + length)
+        # return dp[0]
+
+        # greedy가 됨. 항상 마지막 인덱스까지 볼 필요 없이 중간중간 갱신하면서...
+        last_index = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= last_index:
+                last_index = i
         
-        return True
+        return last_index == 0
