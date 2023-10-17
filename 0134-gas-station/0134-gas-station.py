0f67ca432975efef83003f94edf754756e50dcf6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-17 17:48:11 +0900

    Time: 1091 ms (30.36%), Space: 22.1 MB (69.24%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,44 +1,25 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-#         # 일단 naive로 하면?
-#         # 0부터 시작해서 0까지 돌아오는 것. -> 1 to 1 -> ... 이렇게 해보면 됨.
-#         # 이렇게 하면 시간 복잡도는 n^2
-#         # 이건 TLE 뜸.
-#         for start in range(len(gas)):
-#             cnt = 0
-#             curr_gas = 0
-#             while cnt < len(gas):
-#                 dest = (start + cnt) % len(gas)
-#                 curr_gas += gas[dest]
-#                 curr_gas -= cost[dest]
-                
-#                 if curr_gas < 0:
-#                     break
-                    
-#                 cnt += 1
-            
-#             if curr_gas >= 0:
-#                 return start
+        # 0번쨰부터 n-1번째까지 그냥 돌아야 하나?
+        # 0에서 시작한다고 하면
+        # gas[0]으로 시작. gas[0] - cost[1] < 0 이면 못감
+        # 그렇게 해서 + gas[1]. 해서 - cost[2] < 0인지 또 체크...
+        # 이러면 O(n^2)인데
+        # greedy하게 diff가 > 0 인지 체크하면 됐었음..
         
-#         return -1
-        # 뭔가 DP로 최적화할 수 있을 것 같다. 근데 시작지점에 따라 값이 변해서... 최적 부분 문제를 어떻게 찾지
-        # ? 그리디였음..
-        # total gas > total cost면 정답이 존재. 없으면 -1
-        # 그 다음에는 시작 지점을 찾을 때 효율적인 방법이 있었음.
-        
-        total_surplus = 0
+        # 답이 없는 경우 확인
+        total_diff = 0
         for i in range(len(gas)):
-            total_surplus += gas[i] - cost[i]
-        if total_surplus < 0:
+            total_diff += gas[i] - cost[i]
+        
+        if total_diff < 0:
             return -1
-        
-        start, surplus = 0, 0
+
+        start, diff = 0, 0
         for i in range(len(gas)):
-            # 부분합이 0보다 작아지면 그 사이는 모두 답이 안 되므로 바로 다음 인덱스로 건너뜀.
-            # 왜냐면 그 사이 구간에 surplus가 있는데 안 됐다는 거니까...
-            surplus += gas[i] - cost[i]
-            if surplus < 0:
-                surplus = 0
-                start = i+1
-                
+            diff += gas[i] - cost[i]
+            if diff < 0:
+                start = i + 1
+                diff = 0
+
         return start
