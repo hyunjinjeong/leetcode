a4db980efb9bad8a755222e9fd4ece72bec85361
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-18 22:56:01 +0900

    Time: 184 ms (49.7%), Space: 18 MB (76.89%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -11,24 +11,44 @@ class Solution:
         hand.sort()
 
         counter = collections.Counter(hand)
-        while counter:
-            deleted_count = 0
-            for key in counter.keys():
-                found = True
-                for candidate in range(key, key + groupSize):
-                    found = candidate in counter and found
-                    if not found:
-                        break
-                
-                if found:
-                    for candidate in range(key, key + groupSize):
-                        counter[candidate] -= 1
-                        if counter[candidate] == 0:
-                            del counter[candidate]
-                        deleted_count += 1
-                    break
-            
-            if deleted_count == 0:
-                return False
+        
+        # 여기 체크하는 로직을 이렇게 복잡하게 할 필요가 없었음
+        # 매번 최솟값만 뽑으면 됨.
 
+        i = 0
+        while i < len(hand):
+            card = hand[i]
+            for candidate in range(card, card + groupSize):
+                if candidate not in counter:
+                    return False
+                
+                counter[candidate] -= 1
+                if counter[candidate] == 0:
+                    del counter[candidate]
+                
+            while i < len(hand) and hand[i] not in counter:
+                i += 1
+        
         return True
+
+        # while counter:
+        #     deleted_count = 0
+        #     for key in counter.keys():
+        #         found = True
+        #         for candidate in range(key, key + groupSize):
+        #             found = candidate in counter and found
+        #             if not found:
+        #                 break
+                
+        #         if found:
+        #             for candidate in range(key, key + groupSize):
+        #                 counter[candidate] -= 1
+        #                 if counter[candidate] == 0:
+        #                     del counter[candidate]
+        #                 deleted_count += 1
+        #             break
+            
+        #     if deleted_count == 0:
+        #         return False
+
+        # return True
