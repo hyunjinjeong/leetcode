99f7abfd261e4ee26bb0b73371f1645c37fa0cc7
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-19 23:55:56 +0900

    Time: 1761 ms (71.2%), Space: 61.6 MB (15.13%) - LeetHub

---
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # 각각 최대를 찾으면 안됨..
+        # 그럼 중간중간 단계에서 최댓값을 구해서 사용해야 하는데
+        # 어떻게 돌려야 중간 최댓값이 정답인지 알 수 있을까
+
+        ans = [0, 0, 0]
+        for t in triplets:
+            if t[0] <= target[0] and t[1] <= target[1] and t[2] <= target[2]:
+                ans = [max(ans[0], t[0]), max(ans[1], t[1]), max(ans[2], t[2])]
+        
+        return ans == target
