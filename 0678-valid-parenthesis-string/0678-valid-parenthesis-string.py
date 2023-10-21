af07daca88bc7250c48dbf43cc93bdb012528a08
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-21 18:11:20 +0900

    Time: 34 ms (81.83%), Space: 16.4 MB (27.34%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,28 +1,48 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        # * 처리를 해야 하는데..
-        # *를 (, ), ""로 각각 나눠서 DFS 돌리기?
+        # # * 처리를 해야 하는데..
+        # # *를 (, ), ""로 각각 나눠서 DFS 돌리기?
 
-        cache = {}
+        # cache = {}
 
-        def dfs(i, curr_count):
-            if curr_count < 0:
-                return False
-            if i == len(s):
-                return curr_count == 0
+        # def dfs(i, curr_count):
+        #     if curr_count < 0:
+        #         return False
+        #     if i == len(s):
+        #         return curr_count == 0
             
-            if (i, curr_count) in cache:
-                return cache[(i, curr_count)]
+        #     if (i, curr_count) in cache:
+        #         return cache[(i, curr_count)]
             
-            c = s[i]
-            if c == "(":
-                ans = dfs(i + 1, curr_count + 1)
-            elif c == ")":
-                ans = dfs(i + 1, curr_count - 1)
-            else:
-                ans = dfs(i + 1, curr_count + 1) or dfs(i + 1, curr_count - 1) or dfs(i + 1, curr_count)
+        #     c = s[i]
+        #     if c == "(":
+        #         ans = dfs(i + 1, curr_count + 1)
+        #     elif c == ")":
+        #         ans = dfs(i + 1, curr_count - 1)
+        #     else:
+        #         ans = dfs(i + 1, curr_count + 1) or dfs(i + 1, curr_count - 1) or dfs(i + 1, curr_count)
             
-            cache[(i, curr_count)] = ans
-            return ans
+        #     cache[(i, curr_count)] = ans
+        #     return ans
         
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        # greedy도 됨!
+        cmax, cmin = 0, 0
+        for c in s:
+            if c == "(":
+                cmax += 1
+                cmin += 1
+            elif c == ")":
+                cmax -= 1
+                cmin -= 1
+            else:
+                cmax += 1 # * -> (
+                cmin -= 1 # * -> )
+
+            if cmax < 0:
+                return False
+
+            cmin = max(cmin, 0) # 음수는 될 수 없음
+        
+        return cmin == 0
