ab48a9a99fcb20de077ec87fd644eaa63f35d7b8
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-21 19:47:00 +0900

    Time: 78 ms (81.28%), Space: 19.8 MB (60.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,22 +1,14 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # 어렵게 생각했는데 구현 문제에 가까움..
-        # 왼쪽, 오른쪽에 각각 추가하고, 중간에 들어갈 것들은 인덱스를 추적.
         left, right = [], []
-        mid_start, mid_end = newInterval
-        
+        curr = newInterval
+
         for interval in intervals:
-            # 안 겹치는 경우엔 현재 interval을 각각 왼쪽, 오른쪽에 추가하면 됨
-            if interval[0] > mid_end:
-                right.append(interval)
-            elif interval[1] < mid_start:
+            if interval[1] < curr[0]:
                 left.append(interval)
+            elif interval[0] > curr[1]:
+                right.append(interval)
             else:
-                # 겹치는 경우엔 merge. mid_start, mid_end를 구해줘야 한다.
-                mid_start = min(mid_start, interval[0])
-                mid_end = max(mid_end, interval[1])
+                curr = [min(interval[0], curr[0]), max(interval[1], curr[1])]
         
-        return left + [[mid_start, mid_end]] + right
-                
-                
-            
+        return left + [curr] + right
