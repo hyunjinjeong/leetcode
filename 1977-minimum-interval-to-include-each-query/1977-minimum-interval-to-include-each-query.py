cb2d99e5db9218026cf1e00e854458d90777efd0
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-24 21:53:43 +0900

    Time: 1415 ms (65.74%), Space: 59.3 MB (39.51%) - LeetHub

--- 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
@@ -4,22 +4,22 @@ class Solution:
         # 다만 이 경우 시간 및 메모리가 너무 큼.
         # 최적화할 방법이 있을까?
         # 정렬한 후에 heap을 이용하는 방법이 있다..
-        intervals.sort(reverse=True) # pop()을 O(1)로 하기 위해 거꾸로
+        intervals.sort()
 
         min_sizes = {}
         heap = []
+        i = 0 # intervals index
 
         for query in sorted(set(queries)): # 같은 수는 다 결과가 같으니...
+            while i < len(intervals) and intervals[i][0] <= query: # left <= query
+                left, right = intervals[i]
+                size = right - left + 1
+                heapq.heappush(heap, (size, right))
+                i += 1
+
             # 더 이상 범위가 없는 경우는 heap에서 뺌. right < query
             while heap and heap[0][1] < query:
                 heapq.heappop(heap)
-
-            while intervals and intervals[-1][0] <= query: # left <= query
-                left, right = intervals.pop()
-                # query가 정렬되어 있으니까 이 경우에만 넣으면 됨. right < query이면 해당 interval에 포함되는 query가 없는 것
-                if right >= query:
-                    size = right - left + 1
-                    heapq.heappush(heap, (size, right))
             
             if heap:
                 size, _ = heap[0]
