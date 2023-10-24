4be3b96de323e5414227aacb4f494797bbdc8522
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-24 21:50:51 +0900

    Time: 1318 ms (99.92%), Space: 57.7 MB (91.29%) - LeetHub

--- 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
+++ 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.py
@@ -10,18 +10,16 @@ class Solution:
         heap = []
 
         for query in sorted(set(queries)): # 같은 수는 다 결과가 같으니...
+            # 더 이상 범위가 없는 경우는 heap에서 뺌. right < query
             while heap and heap[0][1] < query:
                 heapq.heappop(heap)
 
-            # 더 이상 범위가 없는 아이템은 뺌
-            while intervals and intervals[-1][0] <= query:
+            while intervals and intervals[-1][0] <= query: # left <= query
                 left, right = intervals.pop()
+                # query가 정렬되어 있으니까 이 경우에만 넣으면 됨. right < query이면 해당 interval에 포함되는 query가 없는 것
                 if right >= query:
                     size = right - left + 1
                     heapq.heappush(heap, (size, right))
-            
-            # while heap and heap[0][1] < query:
-            #     heapq.heappop(heap)
             
             if heap:
                 size, _ = heap[0]
