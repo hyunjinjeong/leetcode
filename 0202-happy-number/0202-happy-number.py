19d011c99165486b02fd6217960cd2094ebefc84
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-25 22:52:02 +0900

    Time: 43 ms (51.07%), Space: 16.1 MB (95.39%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,21 +4,36 @@ class Solution:
         # 근데 아닌 경우를 찾아내야 함
         # 사이클이라는거 보면 중복된 값이 나오는지 찾으면 될 듯
 
-        seen = set()
+        # seen = set()
 
-        def happy(num):
+        # def happy(num):
+        #     result = 0
+        #     while num:
+        #         result += (num % 10) ** 2
+        #         num = num // 10
+            
+        #     if result == 1:
+        #         return True
+            
+        #     if result in seen:
+        #         return False
+            
+        #     seen.add(result)
+        #     return happy(result)
+        
+        # return happy(n)
+
+        # Floyd 알고리즘도 쓸 수 있음 ㄷ
+        def calc(num):
             result = 0
             while num:
                 result += (num % 10) ** 2
                 num = num // 10
-            
-            if result == 1:
-                return True
-            
-            if result in seen:
-                return False
-            
-            seen.add(result)
-            return happy(result)
+            return result
         
-        return happy(n)
+        slow, fast = calc(n), calc(calc(n))
+        while slow != fast:
+            slow = calc(slow)
+            fast = calc(calc(fast))
+        
+        return True if slow == 1 else False
