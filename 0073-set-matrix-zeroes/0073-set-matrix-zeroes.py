a09e9b90345b1d5aea2e2962712bd72b44572bc9
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-29 16:51:10 +0900

    Time: 118 ms (48.3%), Space: 17.3 MB (8.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,40 +3,36 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        first_row_has_zero, first_col_has_zero = False, False
-        for c in range(len(matrix[0])):
-            if matrix[0][c] == 0:
-                first_row_has_zero = True
+        M, N = len(matrix), len(matrix[0])
+
+        zero_in_first_row, zero_in_first_col = False, False
+        for i in range(M):
+            if matrix[i][0] == 0:
+                zero_in_first_col = True
                 break
-        for r in range(len(matrix)):
-            if matrix[r][0] == 0:
-                first_col_has_zero = True
+        for i in range(N):
+            if matrix[0][i] == 0:
+                zero_in_first_row = True
                 break
         
-        # 0번째 row, col에 기록해두기
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    matrix[r][0] = 0
+        for i in range(1, M):
+            for j in range(1, N):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
         
-        # 0부터 보면 덮어씌워져서 1부터 기록. 어차피 0번째는 위에서 0으로 해놓음.
-        for c in range(1, len(matrix[0])):
-            if matrix[0][c] == 0:
-                for r in range(len(matrix)):
-                    matrix[r][c] = 0
+        for i in range(1, M):
+            if matrix[i][0] == 0:
+                for j in range(1, N):
+                    matrix[i][j] = 0
+        for j in range(1, N):
+            if matrix[0][j] == 0:
+                for i in range(1, M):
+                    matrix[i][j] = 0
         
-        for r in range(1, len(matrix)):
-            if matrix[r][0] == 0:
-                for c in range(1, len(matrix[0])):
-                    matrix[r][c] = 0
-        
-        if first_row_has_zero:
-            for c in range(len(matrix[0])):
-                matrix[0][c] = 0
-        if first_col_has_zero:
-            for r in range(len(matrix)):
-                matrix[r][0] = 0
-                
-        
-            
+        if zero_in_first_row:
+            for i in range(N):
+                matrix[0][i] = 0
+        if zero_in_first_col:
+            for i in range(M):
+                matrix[i][0] = 0
