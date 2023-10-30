8cbbdfa04ab1697204f4e0d8aaeaa7b579c32300
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-30 17:57:13 +0900

    Time: 35 ms (86.2%), Space: 16.1 MB (66.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,8 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # 아.. 길이가 32인 binary string 인거에 주목!
-        result = 0
+        ans = 0
+
         for i in range(32):
-            result = (result << 1) + (n & 1)
-            n = n >> 1
-        return result
+            ans <<= 1
+            bit = n & 1
+
+            ans += bit
+            n >>= 1
+        
+        return ans
