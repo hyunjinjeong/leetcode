0a8c8d0b255307f81e40f94c4a1ec9a13b3d8c24
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-30 17:47:58 +0900

    Time: 43 ms (26.54%), Space: 16.2 MB (32.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -2,6 +2,10 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         ans = 0
         while n:
-            ans += n & 1
-            n >>= 1
+            # ans += n & 1
+            # n >>= 1
+            # n & n - 1을 하는 방법도 있음. 그러면 가장 오른쪽의 1이 사라짐.
+            # e.g. 1100 & 1011 == 1000
+            n = n & n - 1
+            ans += 1
         return ans
