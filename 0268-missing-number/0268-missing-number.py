1a775a1c2100e786ea6a210546abc280ad588a02
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-30 18:01:19 +0900

    Time: 114 ms (94.96%), Space: 17.5 MB (97.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,42 +1,15 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-#         # 1. 일단 naive한 방법... 정렬한 다음에 이렇게 비교하면 된당. 공간은 O(1) 시간은 O(nlogn)
-#           binary search 쓰면 탐색 속도는 빨라지지만 정렬 때문에 결국 big-o는 차이 없음
-#         nums.sort()
-        
-#         for i in range(len(nums)):
-#             if i != nums[i]:
-#                 return i
-        
-#         return len(nums)
-    
-#         # 2. Follow-up으로 O(1) 공간과 O(n) 시간 복잡도로 해결해야 함.
-#         # 일단 O(n) 공간 O(n) 시간은 set 써서 이런 식으로 된당.
-#         s = set([i for i in range(len(nums)+1)])
-#         s2 = set(nums)
-        
-#         # 원소 한개만 남았을 거
-#         diff = s - s2
-        
-#         return list(diff)[0]
+        # 요것도 xor 쓰는거 같은디
+        # 0-n까지 한번씩 xor 돌려놓고 다시 nums랑 돌리면
+        # 나머지는 2번씩 해서 0이 되고 없는 것만 1번이라서 남음
 
-#         # 3. SUM을 사용하는 방법이 있네 ;;
-#         total_sum = sum(range(len(nums)+1)) # 아니면 n*n+1//2 요거 써도 됨
-#         for num in nums:
-#             total_sum -= num # 아니면 걍 sum(nums) 빼도 되고...
+        ans = 0
+        for num in range(0, len(nums) + 1):
+            ans ^= num
+
+        for num in nums:
+            ans ^= num
         
-#         # 그럼 이제 total_sum이 없는 숫자가 됨
-#         return total_sum
-#         
-        # 목적인 bit manipulation으로 오면 XOR을 사용하면 된다!
-        result = len(nums) # range 범위에서 마지막 숫자를 도달 못하기 때문에 요걸로 초기화.
-        for i, num in enumerate(nums):
-            # 이렇게 하면 i는 0, 1, ..., nums-1까지고 num은 0, ..., nums 까지에 하나 빠진 값.
-            # 즉 하나 빠진 값 제외하고는 다 XOR을 2번씩 수행, i^i = 0이 되어버리고 하나 남는건 i^0 = i
-            # 따라서 빠진 값이 나오게 됨.
-            result ^= i
-            result ^= num
-            # 0 1 2 3에서 1이 빠져있다고 하면.. nums는 0 2 3.
-            # 3^0^1^2^3^0^2^3 => 0^0^1^2^2^3^3 == 1. 이런 식으로.. 
-            # 3이 빠져 있으면 3^0^1^2^0^1^2 => 0^0^1^1^2^2^3 == 3.
-        return result
+        return ans
+            
