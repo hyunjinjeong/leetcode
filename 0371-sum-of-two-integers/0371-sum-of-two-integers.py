9c147317f3f9613e30eccda61fa813c6ce1131a9
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-10-31 22:07:19 +0900

    Time: 32 ms (88.83%), Space: 16.2 MB (74.36%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # bit operation인뎅
+        # carry는 and고... remainder는 xor
+        if not a:
+            return b
+        if not b:
+            return a
+        
+        # mask 쓰는 이유는 https://leetcode.com/problems/sum-of-two-integers/discuss/776952/Python-BEST-LeetCode-371-Explanation-for-Python 참고..
+        mask = 0xffffffff
+        while b:
+            _sum = (a^b) & mask
+            carry = ((a&b) << 1) & mask
+            
+            a = _sum
+            b = carry
+        
+        if (a>>31) & 1: # If a is negative in 32 bits sense
+            return ~(a^mask)
+        return a
