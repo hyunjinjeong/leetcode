984416928fe031fc0cae87269fa99396afcb5f3a
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-07 22:50:51 +0900

    Time: 319 ms (24.57%), Space: 25.1 MB (76.93%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,15 +1,15 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         # 그냥 푸는건 간단한데... follow up을 어떻게 하지?
-        # constant면 bit 연산을 사용해야 하나?
+        # constant면 bit 연산을 사용해야 하나? -> 딱히 떠오르는게 없음
+        # 1은 0번째, 2는 1번째, ... 이렇게 가도록 배열을 수정하면? -> 이건 O(n)에 안 됨
 
-        # 간단하게 hashmap. 공간 n 시간 n
+        # -로 마킹하는 방법이 있음 ㄷ
         ans = []
 
-        counter = collections.Counter(nums)
-
-        for num in range(1, len(nums) + 1):
-            if counter.get(num, 0) == 0:
-                ans.append(num)
-
-        return ans
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] > 0:
+                nums[index] *= -1
+        
+        return [i + 1 for i, num in enumerate(nums) if num > 0]
