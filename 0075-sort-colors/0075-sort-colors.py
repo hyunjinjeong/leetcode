b576e8197c306f105e85d499316ea62708818a37
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-09 23:36:50 +0900

    Time: 35 ms (89.74%), Space: 16.3 MB (8.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,11 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # two-pass는 카운터 만들어서 하면 될듯
-        counter = collections.Counter(nums)
+        # # two-pass는 카운터 만들어서 하면 될듯
+        # counter = collections.Counter(nums)
         
-        i = 0
-        for num in range(3):
-            for _ in range(counter[num]):
-                nums[i] = num
-                i += 1
+        # i = 0
+        # for num in range(3):
+        #     for _ in range(counter[num]):
+        #         nums[i] = num
+        #         i += 1
+
+        # 1-pass. 
+        zero, one, two = 0, 0, len(nums) - 1
+        
+        while one <= two:
+            if nums[one] == 0:
+                nums[zero], nums[one] = nums[one], nums[zero]
+                zero += 1
+                one += 1
+            elif nums[one] == 1:
+                one += 1
+            else: # nums[one] == 2
+                nums[one], nums[two] = nums[two], nums[one]
+                two -= 1
