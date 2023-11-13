e215b436279956fce9667c701bfc1905d0a5a4da
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-13 22:29:30 +0900

    Time: 433 ms (93.29%), Space: 16.8 MB (98.04%) - LeetHub

--- 1117-as-far-from-land-as-possible/1117-as-far-from-land-as-possible.py
+++ 1117-as-far-from-land-as-possible/1117-as-far-from-land-as-possible.py
@@ -1,39 +1,18 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
         M, N = len(grid), len(grid[0])
-        # cache = {}
 
-        # 적당히 dfs 돌리면 될 거 같은디
-        # 이 방식으론 안 됨... 4방향 탐색할 때 무한 재귀... bfs로 가보자.
-        # def dfs(i, j):
-        #     print(i, j)
-
-        #     if not (0 <= i < M and 0 <= j < N):
-        #         return 0
-        #     if grid[i][j] == 1:
-        #         return 0
-        #     if (i, j) in cache:
-        #         return cache[(i, j)]
-            
-        #     # 상하좌우
-        #     left = dfs(i - 1, j)
-        #     right = dfs(i + 1, j)
-        #     up = dfs(i, j - 1)
-        #     down = dfs(i, j + 1)
-
-        #     cache[(i, j)] = max(left, right, up, down) + 1
-        #     return res
         q = collections.deque()
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == 1:
                     q.append((i, j))
         
-        steps = -1
+        steps = -1 # 무조건 1번은 도니까
         while q:
             steps += 1
 
-            # 아 여기를 한 번에 하나씩 하면 안 되고, 1에서 동시에 돌려야 함
+            # 여기를 한 번에 하나씩 하면 안 되고, 동시에 돌려야 함
             for _ in range(len(q)):
                 i, j = q.popleft()
 
