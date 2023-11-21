29d7e17e3313ec4f685d3ba2577a06c636811bc6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-21 17:39:32 +0900

    Time: 201 ms (43.62%), Space: 18.7 MB (25.41%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,47 +1,41 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-#         answer = []
-#         right = 0
+        # O(n)을 생각해보자.
+        # 투 포인터? 음수, 양수 각각 나눠서
+        # 하나씩 비교해가면서 하면 되지 않을까...
         
-#         # 우선 0 이상인 수가 나타날 때까지 right를 오른쪽으로 옮기고..
-#         for num in nums:
-#             if num >= 0:
-#                 break
-#             right += 1
+        # 1. 전부 양수인 경우
+        if nums[0] >= 0:
+            return [num ** 2 for num in nums]
+        
+        # 2. 전부 음수인 경우:
+        if nums[-1] <= 0:
+            return [num ** 2 for num in sorted(nums, reverse=True)]
             
-#         # left는 끝부터 0, right는 시작부터 len(nums)-1까지 달린다
-#         left = right - 1
-#         while left >= 0 and right < len(nums):
-#             if abs(nums[left]) < abs(nums[right]):
-#                 answer.append(nums[left] ** 2)
-#                 left -= 1
-#             else:
-#                 answer.append(nums[right] ** 2)
-#                 right += 1
+        # 3. 일반적인 경우
+        l, r = 0, 0
+        for i, num in enumerate(nums):
+            if num >= 0:
+                l, r = i - 1, i
+                break
         
-#         # left 혹은 right 둘 중 하나만 남음. 둘 다 처리
-#         while left >= 0:
-#             answer.append(nums[left] ** 2)
-#             left -= 1
-        
-#         while right < len(nums):
-#             answer.append(nums[right] ** 2)
-#             right += 1
-        
-#         return answer
-        
-        # 요렇게 안하고 queue 사용하는 방법도 있다. 그러면 맨 위에 탐색 단계가 없어진다.
-        # left는 0부터 오른쪽으로, right는 끝에서부터 왼쪽으로, 조건은 while left <= right..
-        answer = collections.deque()
-        left, right = 0, len(nums)-1
-        
-        while left <= right:
-            num_left, num_right = abs(nums[left]), abs(nums[right])
-            if num_left > num_right:
-                answer.appendleft(num_left ** 2)
-                left += 1
+        # 투 포인터 돌기
+        ans = []
+        while l >= 0 and r < len(nums):
+            left, right = nums[l], nums[r]
+            if abs(left) >= abs(right):
+                ans.append(right ** 2)
+                r += 1
             else:
-                answer.appendleft(num_right ** 2)
-                right -= 1
+                ans.append(left ** 2)
+                l -= 1
         
-        return list(answer)
+        # 남은거 마저 돌기
+        while l >= 0:
+            ans.append(nums[l] ** 2)
+            l -= 1
+        while r < len(nums):
+            ans.append(nums[r] ** 2)
+            r += 1
+        
+        return ans
