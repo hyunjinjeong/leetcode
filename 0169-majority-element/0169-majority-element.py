7076040cdd355d3de70ac27ac7775fad8dd60843
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-23 22:01:42 +0900

    Time: 145 ms (82.03%), Space: 17.8 MB (35.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # 1. O(n) 공간 쓰는건 쉬움
-        counter = collections.defaultdict(int)
+#         # 1. O(n) 공간 쓰는건 쉬움
+#         counter = collections.defaultdict(int)
+        
+#         for num in nums:
+#             counter[num] += 1
+#             if counter[num] > len(nums) // 2:
+#                 return num
+
+        # 2. O(1) 공간은?
+        count = 0
+        candidate = None
         
         for num in nums:
-            counter[num] += 1
-            if counter[num] > len(nums) // 2:
-                return num
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
