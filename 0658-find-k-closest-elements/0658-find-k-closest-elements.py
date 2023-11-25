1bf74ea37b0cfe66c2070304de9a72a99fc45c9e
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-25 15:02:42 +0900

    Time: 263 ms (47.69%), Space: 18.1 MB (24.87%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,37 +1,42 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # binary search 아니면 two pointer 같은디
-        # binary search로 x의 index를 찾고 k개만큼 채우는 건 two pointer로 하면 될 듯?
+        # -2 -1 0 1 2 이렇게 있을 수도 있음.
+        # 그러면 0을 찾는다고 하면
+        # 0에서 가까운거 4개면 -2 -1 0 1 인데
+        # binary search로 찾아서 two pointer? deque 쓰면 되고
         
-        # 1. Find the index of x using binary search
-        def find_x():
-            left, right = 0, len(arr)-1
+        def bisect():
+            lo, hi = 0, len(arr)
             
-            while left < right:
-                mid = (left + right) // 2
+            while lo < hi:
+                mid = (lo + hi) // 2
                 
                 if arr[mid] < x:
-                    left = mid + 1
+                    lo = mid + 1
                 else:
-                    right = mid
-
-            return left
+                    hi = mid
+            
+            return lo
         
-        # 2. Make the answer array. 아까 찾은 x의 index를 right로. 조건에 따라 작은 것들부터 찾아야 함.
-        right = find_x()
-        left = right - 1
+        pos = bisect()
+        ans = collections.deque()
         
-        while k:
-            if left < 0:
-                right += 1
-            elif right >= len(arr):
-                left -= 1
+        l, r = pos - 1, pos
+        while len(ans) < k:
+            if r >= len(arr):
+                ans.appendleft(arr[l])
+                l -= 1
+            elif l < 0:
+                ans.append(arr[r])
+                r += 1
             else:
-                num_left, num_right = arr[left], arr[right]
-                if abs(num_left-x) <= abs(num_right-x):
-                    left -= 1
+                # 비교 필요
+                left, right = arr[l], arr[r]
+                if abs(left - x) <= abs(right - x):
+                    ans.appendleft(arr[l])
+                    l -= 1
                 else:
-                    right += 1      
-            k -= 1
-        # 여기서 left와 right를 각각 마지막에 -1, +1 한 것들은 제외해줘야 함.
-        return arr[left+1:right]
+                    ans.append(arr[r])
+                    r += 1
+            
+        return ans
