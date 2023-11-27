069acdcefdffa445fba4412e5f92939d645b27d6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-27 16:33:24 +0900

    Time: 246 ms (36.11%), Space: 24.5 MB (13.55%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -6,7 +6,7 @@ class Solution:
         M, N = len(rooms), len(rooms[0])
         
         def dfs(i, j, distance):
-            rooms[i][j] = min(distance, rooms[i][j])
+            rooms[i][j] = distance
             
             for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 ni, nj = i + di, j + dj
@@ -18,21 +18,3 @@ class Solution:
             for j in range(N):
                 if rooms[i][j] == 0:
                     dfs(i, j, 0)
-        
-#         q = collections.deque()
-#         for i in range(M):
-#             for j in range(N):
-#                 if rooms[i][j] == 0:
-#                     q.append((i, j))
-        
-#         distance = 0
-#         while q:
-#             for _ in range(len(q)):
-#                 i, j = q.popleft()
-#                 rooms[i][j] = min(distance, rooms[i][j])
-#                 for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-#                     ni, nj = i + di, j + dj
-#                     if 0 <= ni < M and 0 <= nj < N and rooms[ni][nj] > distance:
-#                         q.append((ni, nj))
-            
-#             distance += 1
