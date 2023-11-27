283003b2c8b33f6c28d5c80924b524b780a1c74d
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-11-27 16:33:14 +0900

    Time: 239 ms (41.50%), Space: 24.6 MB (13.55%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -5,23 +5,34 @@ class Solution:
         """
         M, N = len(rooms), len(rooms[0])
         
-        # gate에서부터 시작하는게 더 쉬울듯?
-        # dfs보단 bfs가 편할 것 같고
+        def dfs(i, j, distance):
+            rooms[i][j] = min(distance, rooms[i][j])
+            
+            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                ni, nj = i + di, j + dj
+                # 일종의 visited. wall, gate도 걸러냄.
+                if 0 <= ni < M and 0 <= nj < N and rooms[ni][nj] > distance + 1:
+                    dfs(ni, nj, distance + 1)
         
-        q = collections.deque()
         for i in range(M):
             for j in range(N):
                 if rooms[i][j] == 0:
-                    q.append((i, j))
+                    dfs(i, j, 0)
         
-        distance = 0
-        while q:
-            for _ in range(len(q)):
-                i, j = q.popleft()
-                rooms[i][j] = min(distance, rooms[i][j])
-                for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < M and 0 <= nj < N and rooms[ni][nj] > distance:
-                        q.append((ni, nj))
+#         q = collections.deque()
+#         for i in range(M):
+#             for j in range(N):
+#                 if rooms[i][j] == 0:
+#                     q.append((i, j))
+        
+#         distance = 0
+#         while q:
+#             for _ in range(len(q)):
+#                 i, j = q.popleft()
+#                 rooms[i][j] = min(distance, rooms[i][j])
+#                 for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+#                     ni, nj = i + di, j + dj
+#                     if 0 <= ni < M and 0 <= nj < N and rooms[ni][nj] > distance:
+#                         q.append((ni, nj))
             
-            distance += 1
+#             distance += 1
