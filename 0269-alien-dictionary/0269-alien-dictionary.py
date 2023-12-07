b1fc21679dae697ed22e285987d24bc348481077
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-12-07 22:37:21 +0900

    Time: 50 ms (8.82%), Space: 16.4 MB (53.94%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -6,17 +6,18 @@ class Solution:
         edges = []
         for i in range(1, len(words)):
             prev, curr = words[i-1], words[i]
-            has_broken = False
-            for j in range(min(len(prev), len(curr))):
+            min_len = min(len(prev), len(curr))
+            
+            # 엣지 케이스, e.g. "abc", "ab"
+            if prev[:min_len] == curr[:min_len] and len(prev) > len(curr):
+                return ""
+            
+            for j in range(min_len):
                 # 근데 여기서 길이만 다른건 어떻게 처리하지? e.g. abc, abcd
                 # 아무런 관계가 없으니 pass가 맞겠다.
                 if prev[j] != curr[j]:
                     edges.append([prev[j], curr[j]])
-                    has_broken = True
                     break
-
-            if not has_broken and len(prev) > len(curr): # 엣지 케이스, e.g. "abc", "ab"
-                return ""
         
         letters = set(c for word in words for c in word)
         graph = {c: set() for c in letters}
