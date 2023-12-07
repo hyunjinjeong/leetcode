f4622d97139c2ba63d20b8086ec4497b33e21dfd
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-12-07 22:38:17 +0900

    Time: 34 ms (93.75%), Space: 16.4 MB (53.94%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -3,7 +3,10 @@ class Solution:
         # 모르겠다!
         # sol: Where two words are adjacent, we need to look for the first difference between them.
         # 이거 규칙만 발견하면 쉽넹... topological sort임
-        edges = []
+        letters = set(c for word in words for c in word)
+        graph = {c: set() for c in letters}
+        in_degree = {c: 0 for c in letters}
+        
         for i in range(1, len(words)):
             prev, curr = words[i-1], words[i]
             min_len = min(len(prev), len(curr))
@@ -16,17 +19,11 @@ class Solution:
                 # 근데 여기서 길이만 다른건 어떻게 처리하지? e.g. abc, abcd
                 # 아무런 관계가 없으니 pass가 맞겠다.
                 if prev[j] != curr[j]:
-                    edges.append([prev[j], curr[j]])
+                    u, v = prev[j], curr[j]
+                    if v not in graph[u]: # 중복이 있을 수 있음
+                        graph[u].add(v)
+                        in_degree[v] += 1
                     break
-        
-        letters = set(c for word in words for c in word)
-        graph = {c: set() for c in letters}
-        in_degree = {c: 0 for c in letters}
-        
-        for u, v in edges:
-            if v not in graph[u]: # 중복이 있을 수 있음
-                graph[u].add(v)
-                in_degree[v] += 1
                 
         ans = []
         q = collections.deque()
