f0a36a4922f9e3647e1f53955ca310d755f9dc01
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2023-12-14 21:10:07 +0900

    Time: 140 ms (56.49%), Space: 17.5 MB (59.81%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,17 +1,12 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # 왼쪽 오른쪽 각각 prefix sum 만들어두고 비교하면 되지 않을까
-        left_sum, right_sum = [0], [0]
+        # 추가 공간이 필요 없었음
+        total_sum = sum(nums)
+        left_sum = 0
 
-        for num in nums + [0]:
-            left_sum.append(left_sum[-1] + num)
-        for num in nums[::-1] + [0]:
-            right_sum.append(right_sum[-1] + num)
-        right_sum.reverse()
-
-        for i in range(1, len(nums)+1):
-            left, right = left_sum[i-1], right_sum[i+1]
-            if left == right:
-                return i - 1
+        for i, num in enumerate(nums):
+            if left_sum == total_sum - left_sum - num:
+                return i
+            left_sum += num
         
         return -1
