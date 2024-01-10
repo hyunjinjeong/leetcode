5810c01bcf8d7bf278a48dd1b2d7bc9903165fb4
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-01-10 21:27:19 +0900

    Time: 754 ms (94.42%), Space: 37.8 MB (60.15%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        # brute force로 하면 걍 모든 조합 돌면서 구하는거고..
+        # 모르겠다.. 아니 이게 medium?
+        merged = sorted(zip(nums1, nums2), key=lambda pair: pair[1], reverse=True)
+
+        # nums2를 돌면 nums2[i]가 min으로 고정됨
+        # 그러면 nums1에서의 최대를 구해야 하는데, 이걸 min heap으로..!
+        ans = 0
+        nums1_sum = 0
+        heap = []
+
+        for i in range(k - 1):
+            num1, num2 = merged[i]
+            nums1_sum += num1
+            heapq.heappush(heap, num1)
+
+        for i in range(k - 1, len(merged)):
+            num1, num2 = merged[i]
+
+            nums1_sum += num1
+            heapq.heappush(heap, num1)
+            ans = max(nums1_sum * num2, ans)
+            
+            nums1_sum -= heapq.heappop(heap)
+
+        return ans
+        
