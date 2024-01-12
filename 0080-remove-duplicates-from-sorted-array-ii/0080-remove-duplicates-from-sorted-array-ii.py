5f073143f96f739e14e9d6d4ebd1ca6b7b816919
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-01-12 17:11:59 +0900

    Time: 57 ms (57.56%), Space: 17.4 MB (8.54%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,16 +1,23 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # non_dup_index = 0
+
+        # # nums[i+1], nums[i]가 같은지 유무로 판단?
+        # for i in range(len(nums)):
+        #     if i == 0 or nums[i] != nums[i-1]:
+        #         if i + 1 < len(nums) and nums[i+1] == nums[i]:
+        #             nums[non_dup_index] = nums[i]
+        #             nums[non_dup_index + 1] = nums[i]
+        #             non_dup_index += 2
+        #         else:
+        #             nums[non_dup_index] = nums[i]
+        #             non_dup_index += 1
+
+        # return non_dup_index
+
         non_dup_index = 0
-
-        # nums[i+1], nums[i]가 같은지 유무로 판단?
-        for i in range(len(nums)):
-            if i == 0 or nums[i] != nums[i-1]:
-                if i + 1 < len(nums) and nums[i+1] == nums[i]:
-                    nums[non_dup_index] = nums[i]
-                    nums[non_dup_index + 1] = nums[i]
-                    non_dup_index += 2
-                else:
-                    nums[non_dup_index] = nums[i]
-                    non_dup_index += 1
-
+        for i, num in enumerate(nums):
+            if non_dup_index <= 1 or nums[non_dup_index - 2] != num:
+                nums[non_dup_index] = num
+                non_dup_index += 1
         return non_dup_index
