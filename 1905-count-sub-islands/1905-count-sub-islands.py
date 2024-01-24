a5934c8e64ee487c7b5476b26c789c65fae38ee2
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-01-24 21:50:56 +0900

    Time: 2026 ms (90.11%), Space: 38.3 MB (74.98%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -4,14 +4,13 @@ class Solution:
         # grid1에도 동시에 돌아서 grid2가 1일때 grid1도 1인지 확인하면 될 듯?
         # 둘 중 하나가 0이면 빠져나오면 되고..
         M, N = len(grid1), len(grid1[0])
-        visited = set()
 
         def dfs(i, j, is_valid):
-            visited.add((i, j))
-        
+            grid2[i][j] = 0
+
             for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 next_i, next_j = i + di, j + dj
-                if 0 <= next_i < M and 0 <= next_j < N and grid2[next_i][next_j] == 1 and (next_i, next_j) not in visited:
+                if 0 <= next_i < M and 0 <= next_j < N and grid2[next_i][next_j] == 1:
                     is_valid = dfs(next_i, next_j, grid1[next_i][next_j] == 1) and is_valid
             
             return 1 if is_valid else 0
@@ -19,9 +18,7 @@ class Solution:
         ans = 0
         for i in range(M):
             for j in range(N):
-                if grid2[i][j] == 1 and (i, j) not in visited:
+                if grid2[i][j] == 1:
                     ans += dfs(i, j, grid1[i][j] == 1)
         
-        print(visited)
-
         return ans
