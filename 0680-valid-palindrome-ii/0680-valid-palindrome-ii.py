a0558b86c74dd86f8223d94c3fe6550e55f7c575
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-01-25 23:18:31 +0900

    Time: 96 ms (73.39%), Space: 17 MB (63.28%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -3,17 +3,23 @@ class Solution:
         # two pointer로 돌면서 palindrome이면 계속 진행하고
         # 아니면 left+1, right랑 left, right-1이랑 비교.
         # 최대 1번만 가능하니까... 이 값을 어디에 저장해야 하고
-        def is_palindrome(l, r, has_deleted):
-            if l >= r:
-                return True
-            
-            c1, c2 = s[l], s[r]
-            if c1 == c2:
-                return is_palindrome(l+1, r-1, has_deleted)
-            
-            if has_deleted:
-                return False
+        # 매번 함수를 실행할 필요 없이 한 번만 하면 되는구나
+        def is_palindrome(l, r):
+            while l < r:
+                if s[l] == s[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
 
-            return is_palindrome(l+1, r, True) or is_palindrome(l, r-1, True)
+            return True
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+            else:
+                return is_palindrome(l+1, r) or is_palindrome(l, r-1)
         
-        return is_palindrome(0, len(s)-1, False)
+        return True
