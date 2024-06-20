726cfe99a380c1bcbfa51dd15a32c668d5075d4a
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-06-21 00:04:52 +0900

    Time: 74 ms (66.46%), Space: 17.7 MB (92.91%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,36 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         # 누가 봐도 binary search 문제임
-        def bisect_left(t):
-            lo, hi = 0, len(nums)
+        def bisect_left():
+            index = -1
+            lo, hi = 0, len(nums) - 1
             
-            while lo < hi:
+            while lo <= hi:
                 mid = (lo + hi) // 2
-                if nums[mid] < t:
+                if nums[mid] == target:
+                    index = mid
+                    hi = mid - 1
+                elif nums[mid] < target:
                     lo = mid + 1
                 else:
-                    hi = mid
+                    hi = mid - 1
             
-            return lo
+            return index
         
-        ans = [-1, -1]
+        def bisect_right():
+            index = -1
+            lo, hi = 0, len(nums) - 1
+            
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if nums[mid] == target:
+                    index = mid
+                    lo = mid + 1
+                elif nums[mid] < target:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            
+            return index
         
-        ans[0] = bisect_left(target)
-        ans[1] = bisect_left(target + 1) - 1
-        
-        if ans[0] <= ans[1]:
-            return ans
-        
-        return [-1, -1]
+        return [bisect_left(), bisect_right()]
