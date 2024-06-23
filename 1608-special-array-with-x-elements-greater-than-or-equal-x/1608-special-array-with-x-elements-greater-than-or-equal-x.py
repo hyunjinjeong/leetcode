3d391592a86047b3d213e091fbccc0755961e965
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-06-23 17:25:06 +0900

    Time: 43 ms (36.84%), Space: 16.6 MB (18.8%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,9 +1,8 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         # 숫자가 0~1000이니까 다 시도해볼 순 있는데... 더 최적화할 수 있지 않을까
-        # 일단 brute force로
-
-        for x in range(max(nums) + 1):
+        
+        for x in range(len(nums) + 1): # 여기는 length만큼만 해도 되는구나
             cnt = 0
             broken = False
             for num in nums:
