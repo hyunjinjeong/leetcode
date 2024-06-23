57b7d3f506f280ea37534cf2bf090ff45acb9e54
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-06-23 17:30:55 +0900

    Time: 40 ms (57.1%), Space: 16.7 MB (5.7%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,18 +1,17 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        # 숫자가 0~1000이니까 다 시도해볼 순 있는데... 더 최적화할 수 있지 않을까
+        N = len(nums)
         
-        for x in range(len(nums) + 1): # 여기는 length만큼만 해도 되는구나
-            cnt = 0
-            broken = False
-            for num in nums:
-                if num >= x:
-                    cnt += 1
-                if cnt > x:
-                    broken = True
-                    break
-            if cnt == x and not broken:
-                return x
+        # counting sort..!
+        freq = [0] * (N + 1)
+        for num in nums:
+            freq[min(num, N)] += 1
+        
+        nums_greater_than_equal = 0
+        for i in range(N, 0, -1):
+            nums_greater_than_equal += freq[i]
+            if i == nums_greater_than_equal:
+                return i
         
         return -1
             
