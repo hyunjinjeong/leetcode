a27f539ad7989291ba7cec9c3f0c52be362bc6d8
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-06-28 16:30:13 +0900

    Time: 47 ms (6.17%), Space: 16.8 MB (7.76%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -2,7 +2,6 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         # 바로 떠오르는건 stack? 왜냐면 ".."를 만나면 바로 앞의 경로를 빼줘야 함
         # ".."랑 "..." 구분 때문에 "/"로 스플릿하는게 편하긴 한데... 쓰면 안 되겠지?
-        # 그럼 현재 "."의 개수를 세고 있으면 되려나?
         total_path = []
         current_path = []
 
@@ -14,7 +13,7 @@ class Solution:
                 current_path = []
                 while i < len(path) and path[i] == "/":
                     i += 1
-            elif path[i] == ".":
+            elif path[i] == "." and not current_path:
                 if i == len(path) - 1 or path[i+1] == "/": # "."로 끝난 경우
                     # 현재 경로니까 아무 것도 안 넣으면 됨. i만 적당히 증가
                     i += 1
