de3a757e3d1a0c468b0a704bf152b9117a554120
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-06-30 14:43:45 +0900

    Time: 138 ms (88.2%), Space: 16.9 MB (86.84%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -5,22 +5,47 @@ class Solution:
         # graph coloring 이라고 한다. 뭐여 이게...
         # 인접한 노드들은 서로 다른 색을 갖도록 하는것?
         
+        # colors = [0] * len(graph)
+        
+        # def dfs(node, color):
+        #     if colors[node] != 0:
+        #         return colors[node] == color # 이미 다른 색으로 칠해진 경우
+            
+        #     colors[node] = color
+        #     for neighbor in graph[node]:
+        #         if not dfs(neighbor, -color):
+        #             return False
+            
+        #     return True
+        
+        # for i in range(len(graph)):
+        #     if colors[i] == 0:
+        #         if not dfs(i, 1):
+        #             return False
+        
+        # return True
+
         colors = [0] * len(graph)
-        
-        def dfs(node, color):
-            if colors[node] != 0:
-                return colors[node] == color # 이미 다른 색으로 칠해진 경우
-            
-            colors[node] = color
-            for neighbor in graph[node]:
-                if not dfs(neighbor, -color):
-                    return False
-            
-            return True
-        
+
         for i in range(len(graph)):
-            if colors[i] == 0:
-                if not dfs(i, 1):
-                    return False
-        
+            if colors[i] != 0:
+                continue
+            
+            colors[i] = 1
+            q = collections.deque([i])
+            while q:
+                node = q.popleft()
+                for neighbor in graph[node]:
+                    if colors[neighbor] == colors[node]:
+                        return False
+                    
+                    if colors[neighbor] == 0:
+                        colors[neighbor] = -colors[node]
+                        q.append(neighbor)
+            
         return True
+                        
+
+            
+
+        
