e60361d31479b1a468c37858d22216a3ba1980dc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-06-30 14:58:50 +0900

    Time: 81 ms (74.42%), Space: 18.3 MB (39.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,14 +1,29 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # 너무 dp 문젠데
-        M, N = len(grid), len(grid[0])
+        # # 너무 dp 문젠데
+        # M, N = len(grid), len(grid[0])
 
-        @cache
-        def dfs(i, j):
-            right = dfs(i + 1, j) if i + 1 < M else float("inf")
-            down = dfs(i, j + 1) if j + 1 < N else float("inf")
+        # @cache
+        # def dfs(i, j):
+        #     right = dfs(i + 1, j) if i + 1 < M else float("inf")
+        #     down = dfs(i, j + 1) if j + 1 < N else float("inf")
             
-            sub_min = min(right, down) if min(right, down) != float("inf") else 0
-            return sub_min + grid[i][j]
+        #     sub_min = min(right, down) if min(right, down) != float("inf") else 0
+        #     return sub_min + grid[i][j]
 
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        # bottom up으로 풀어보자
+        M, N = len(grid), len(grid[0])
+        dp = [[float("inf")] * (N+1) for _ in range(M+1)]
+
+        # dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+        # 33 
+        for i in range(M-1, -1, -1):
+            for j in range(N-1, -1, -1):
+                if i + 1 < M or j + 1 < N:
+                    dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
+                else:
+                    dp[i][j] = grid[i][j]
+        
+        return dp[0][0]
