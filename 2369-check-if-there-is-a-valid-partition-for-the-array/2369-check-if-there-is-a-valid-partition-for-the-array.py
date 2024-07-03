5647881028b40ec4642f695f33d60d88c3de7129
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-03 22:43:03 +0900

    Time: 752 ms (47.41%), Space: 30.3 MB (97%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,25 +1,14 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        # 3가지 경우 중에 하나인지를 dp로 계속해서 찾으면 되지 않을까?
-        # 우선 top down으로 해보자...
         N = len(nums)
+        dp = [False] * N + [True]
 
-        @cache
-        def dp(i):
-            if i > N - 2:
-                return False
-            if i == N - 2:
-                return nums[i] == nums[i+1]
-            if i == N - 3:
-                return nums[i] == nums[i+1] - 1 and nums[i+1] == nums[i+2] - 1 or nums[i] == nums[i+1] and nums[i+1] == nums[i+2]
-            
-            # 1. 2개 같은거
-            result = False
-            if nums[i] == nums[i+1] - 1 and nums[i+1] == nums[i+2] - 1 or nums[i] == nums[i+1] and nums[i+1] == nums[i+2]:
-                result = result or dp(i+3)
-            if nums[i] == nums[i+1]:
-                result = result or dp(i+2)
-            
-            return result
+        for i in range(N-1, -1, -1):
+            if i >= 1:
+                if nums[i] == nums[i-1]:
+                    dp[i-1] |= dp[i+1]
+            if i >= 2:
+                if nums[i] == nums[i-1] + 1 and nums[i-1] == nums[i-2] + 1 or nums[i] == nums[i-1] and nums[i-1] == nums[i-2]:
+                    dp[i-2] |= dp[i+1]
         
-        return dp(0)
+        return dp[0]
