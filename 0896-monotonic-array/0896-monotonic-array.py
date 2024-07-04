d7de15780008fd00715a3eb224e029cd12ef5009
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-04 23:48:54 +0900

    Time: 708 ms (70.83%), Space: 30.9 MB (67.76%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -6,11 +6,9 @@ class Solution:
             if nums[i] == nums[i-1]:
                 continue
             if nums[i] > nums[i-1]:
-                if is_increasing: continue
                 if is_decreasing: return False
                 is_increasing = True
             if nums[i] < nums[i-1]:
-                if is_decreasing: continue
                 if is_increasing: return False
                 is_decreasing = True
         
