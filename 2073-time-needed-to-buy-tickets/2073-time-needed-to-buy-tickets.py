d962f0fad9bc95ad61cb5e8431c86f6add75658a
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-10 23:55:36 +0900

    Time: 34 ms (90.52%), Space: 16.5 MB (25.76%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,15 +1,11 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        # deque로 만들고 0이 되면 append를 안하면 될 듯?
-        deque_tickets = collections.deque(enumerate(tickets))
-
+        # one pass로 계산할 수가 있구나
         time = 0
-        while deque_tickets:
-            i, ticket_count = deque_tickets.popleft()
-            time += 1
-            
-            if i == k and ticket_count == 1:
-                return time
-            
-            if ticket_count > 1:
-                deque_tickets.append((i, ticket_count - 1))
+        for i, ticket_count in enumerate(tickets):
+            if i <= k:
+                time += min(ticket_count, tickets[k])
+            else:
+                time += min(ticket_count, tickets[k] - 1)
+        
+        return time
