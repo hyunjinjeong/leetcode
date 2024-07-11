1ac453d0f48b0a033484606e56d535da808c5a73
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-11 22:23:39 +0900

    Time: 44 ms (82.62%), Space: 16.5 MB (79.35%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -2,12 +2,12 @@ class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         # 각각의 stone이 +일지 -일지...
         # 0-1 knapsack 문제였음
-        stone_sum = sum(stones)
-        half_sum = stone_sum // 2
-        dp = [0 for i in range(half_sum + 1)]
+        total = sum(stones)
+        max_weight = total // 2
+        dp = [0] * (max_weight + 1)
 
         for stone in stones:
-            for w in range(half_sum, stone - 1, -1):
+            for w in range(max_weight, stone - 1, -1):
                 dp[w] = max(dp[w - stone] + stone, dp[w])
         
-        return stone_sum - 2 * dp[half_sum]
+        return total - 2 * dp[max_weight]
