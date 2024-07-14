5c7a7ea37d64480ac5bf58d3a3afef32575ee69f
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-14 13:15:23 +0900

    Time: 622 ms (37.22%), Space: 16.3 MB (99.92%) - LeetHub

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        # LIS는 dp인데...
+        # count를 따로 관리하기?
+        N = len(nums)
+        dp = [1] * N
+        count = [1] * N
+
+        for i in range(N):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        count[i] = 0
+                    if dp[j] + 1 == dp[i]:
+                        count[i] += count[j]
+        
+        ans = 0
+        lis_length = max(dp)
+        for i in range(N):
+            if dp[i] == lis_length:
+                ans += count[i]
+
+        return ans
