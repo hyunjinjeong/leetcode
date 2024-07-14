39224d95e6a527c3f44cceabb5d1253f4e6d7a74
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-14 14:38:48 +0900

    Time: 53 ms (23.18%), Space: 16.6 MB (10.12%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,21 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        dt = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900 }
-        
-        answer = 0
-        i = 0
-        while i < len(s):
-            c = s[i]
-            if c in ("I", "X", "C") and i < len(s)-1:
-                cc = s[i]+s[i+1]
-                if cc in dt:
-                    answer += dt[cc]
-                    i += 1
-                else:
-                    answer += dt[c]
+        dt = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
+
+        ans = dt[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if dt[s[i]] < dt[s[i + 1]]:
+                ans -= dt[s[i]]
             else:
-                answer += dt[c]
-            
-            i += 1
+                ans += dt[s[i]]
         
-        return answer
+        return ans
