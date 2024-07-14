411345f5ff61175d54cdf71b66283bfd54d6abfd
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-14 14:24:18 +0900

    Time: 42 ms (90.06%), Space: 16.5 MB (95.43%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,40 +1,29 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         # 이것도 greedy 같은데
-        # 각각 전체 count를 센 뒤에
-        # loop 돌면서 상대 count를 깎아보면 될 듯?
-        # RRDD
-        # 2 -2
-        # RRDDD
-        # 2 -2 => 2 0 => 1 1
-        # 이건 아니넹
-
         r_count, d_count = 0, 0
         for c in senate:
             if c == "R":
                 r_count += 1
-                if r_count > 0:
-                    d_count -= 1
-            elif c == "D":
+            else:
                 d_count += 1
-                if d_count > 0:
+
+        q = collections.deque(senate)
+
+        curr = senate[0]
+        curr_count = 0
+        while r_count and d_count:
+            c = q.popleft()
+
+            if curr_count == 0 or c == curr:
+                curr = c
+                q.append(c)
+                curr_count += 1
+            elif curr_count > 0:
+                if c == "R":
                     r_count -= 1
+                else:
+                    d_count -= 1
+                curr_count -= 1
         
-        if r_count > d_count:
-            return "Radiant"
-        if r_count < d_count:
-            return "Dire"
-        
-        # 같으면 먼저 나온게...
-        return "Radiant" if senate[0] == "R" else "Dire"
-        
-        # for c in senate:
-        #     if c == "R":
-        #         d_count -= 1
-        #     elif c == "D":
-        #         r_count -= 1
-            
-        #     if d_count == 0:
-        #         return "Radiant"
-        #     if r_count == 0:
-        #         return "Dire"
+        return "Radiant" if r_count else "Dire"
