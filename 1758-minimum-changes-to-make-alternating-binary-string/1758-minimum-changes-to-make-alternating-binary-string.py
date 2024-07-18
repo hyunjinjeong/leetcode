482b49c4ab1d8c50ed2b72a37f96102e8fc7c2ec
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-18 22:57:39 +0900

    Time: 32 ms (98.16%), Space: 16.7 MB (30.47%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,18 +1,16 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        # 이게 easy..?
-        # greedy로 되는 건가
-        start_one = "1"
-        start_zero = "0"
-
         start_one_count, start_zero_count = 0, 0
-        for c in s:
-            if c != start_one:
-                start_one_count += 1
-            if c != start_zero:
-                start_zero_count += 1
-            
-            start_one = "0" if start_one == "1" else "1"
-            start_zero = "0" if start_zero == "1" else "1"
+        for i, c in enumerate(s):
+            if i % 2 == 0:
+                if c == "1":
+                    start_zero_count += 1
+                else:
+                    start_one_count += 1
+            else:
+                if c == "1":
+                    start_one_count += 1
+                else:
+                    start_zero_count += 1
         
         return min(start_one_count, start_zero_count)
