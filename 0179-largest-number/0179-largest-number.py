6816f3128fe20e0585466e27ead1ceace88b8b4f
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-20 17:30:11 +0900

    Time: 46 ms (27.03%), Space: 16.5 MB (19.09%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -3,15 +3,15 @@ class Solution:
         # sorting & greedy라..?
         # 그럼 무슨 방법을 정해서 그 순서대로 쭉 하면 된다는 것.
         # string으로 변환 후 비교까진 맞았는데... 단순히 두개 비교하면 됐었음.
+        for i in range(len(nums)):
+            nums[i] = str(nums[i])
         
         def compare(a, b):
-            str_a, str_b = str(a), str(b)
-            
-            if str_a + str_b < str_b + str_a:
+            if a + b < b + a:
                 return 1 # 1이면 sorting 후 후순위. 즉 뒤로 온다는 뜻
             else:
                 return -1
         
         sorted_nums = sorted(nums, key=functools.cmp_to_key(compare))
         # int로 한번 더 묶는 건 엣지 케이스 [0, 0] 같은 경우 처리.
-        return str(int("".join([str(item) for item in sorted_nums])))
+        return str(int("".join(sorted_nums)))
