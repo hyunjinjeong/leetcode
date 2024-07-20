f1d3954f0362104c28806ad263831deb2402cc5e
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-20 12:49:22 +0900

    Time: 64 ms (77.31%), Space: 17.3 MB (49.04%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,13 +1,9 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        # two pointer로 스왑하면 될 듯?
-        left, right = 0, 0
-        while right < len(nums):
+        left = 0
+        for right in range(len(nums)):
             if nums[right] % 2 == 0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-                right += 1
-            else:
-                right += 1
         
         return nums
