7a6cdc78238f3cf42c21ddca0eb426118ecc02ce
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-25 23:14:36 +0900

    Time: 653 ms (26.8%), Space: 32.2 MB (72.91%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        # 괴물마다 걸리는 시간을 구해서
+        # greedy로 풀기?
+        times = []
+        for i in range(len(dist)):
+            times.append(dist[i] / speed[i])
+        heapq.heapify(times)
+
+        heapq.heappop(times) # 0초에 하나 죽일 수 있음
+        res = 1
+
+        curr_time = 1
+        while times:
+            time = heapq.heappop(times)
+            if curr_time >= time:
+                break
+            curr_time += 1
+            res += 1
+        
+        return res
