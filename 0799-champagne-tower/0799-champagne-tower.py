c8e4b28fb6c1ddf51f7855420780064acecb4eef
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-27 19:34:06 +0900

    Time: 61 ms (86.84%), Space: 16.6 MB (48.38%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -0,0 +1,17 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        # 파스칼의 삼각형.. 비슷한 거 같은데
+        prev, curr = [0] * 101, [0] * 101
+        prev[0] = poured
+
+        row = [poured]
+        for _ in range(query_row):
+            new_row = [0] * (len(row) + 1)
+            for i in range(len(row)):
+                pour = (row[i] - 1) / 2
+                if pour > 0:
+                    new_row[i] += pour
+                    new_row[i + 1] += pour
+            row = new_row
+        
+        return row[query_glass] if row[query_glass] < 1 else 1
