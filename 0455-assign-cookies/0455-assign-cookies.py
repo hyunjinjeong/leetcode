4a1f481673b5222caae165ac77fb73a576131e5f
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-29 17:53:47 +0900

    Time: 141 ms (17.91%), Space: 18.6 MB (14.77%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -3,18 +3,10 @@ class Solution:
         g.sort()
         s.sort()
 
-        res = 0
-
         child, cookie = 0, 0
         while child < len(g) and cookie < len(s):
-            while cookie < len(s) and s[cookie] < g[child]:
-                cookie += 1
-
-            if cookie >= len(s):
-                break
-
-            child += 1
+            if s[cookie] >= g[child]:
+                child += 1
             cookie += 1
-            res += 1
         
-        return res
+        return child
