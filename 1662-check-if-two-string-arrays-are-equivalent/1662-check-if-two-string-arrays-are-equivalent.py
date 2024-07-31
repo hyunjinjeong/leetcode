485826e0f11b55a66e08a4ca70a4d301b6937baf
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-07-31 22:00:09 +0900

    Time: 43 ms (18.05%), Space: 16.6 MB (11.09%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -3,7 +3,10 @@ class Solution:
         l1, l2 = 0, 0
         r1, r2 = 0, 0
 
-        while l1 < len(word1) and l2 < len(word2):
+        while l1 < len(word1) or r1 < len(word2):
+            if l1 == len(word1) and r1 < len(word2) or l1 < len(word1) and r1 == len(word2):
+                return False
+
             if word1[l1][l2] != word2[r1][r2]:
                 return False
             
