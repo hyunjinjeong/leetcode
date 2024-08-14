69f2c3c5dea4ce17426ab250a2eb3d2ace766152
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-08-14 22:51:31 +0900

    Time: 92 ms (94.97%), Space: 17 MB (72.74%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -3,25 +3,24 @@ class Solution:
         M, N = len(grid), len(grid[0])
 
         def dfs(i, j):
-            if i == 0 or i == M - 1:
-                return False
-            if j == 0 or j == N - 1:
-                return False
+            if i < 0 or i > M - 1 or j < 0 or j > N - 1:
+                return 0
+            if grid[i][j] == 1:
+                return 1
 
             grid[i][j] = 1 # visited
-            is_closed_island = True
-            for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                next_i, next_j = i + di, j + dj
-                if 0 <= next_i < M and 0 <= next_j < N and grid[next_i][next_j] == 0:
-                    is_closed_island &= dfs(next_i, next_j)
 
-            return is_closed_island                    
+            left = dfs(i, j - 1)
+            right = dfs(i, j + 1)
+            up = dfs(i - 1, j)
+            down = dfs(i + 1, j)
+
+            return 1 if left and right and up and down else 0
         
         res = 0
-        for i in range(1, len(grid)):
-            for j in range(1, len(grid[0])):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 0:
-                    if dfs(i, j):
-                        res += 1
+                    res += dfs(i, j)
         
         return res
