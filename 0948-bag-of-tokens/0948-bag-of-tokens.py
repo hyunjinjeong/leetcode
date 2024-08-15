84ca25bcb843517fee13f21b23ea8422090a8b69
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-08-15 18:21:53 +0900

    Time: 54 ms (47.11%), Space: 16.7 MB (24.65%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -1,25 +1,24 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        # ??
         # 일단 토큰을 정렬해야하는 것 같음
         tokens.sort()
 
         # 보니까 항상 play를 해야하는 건 아닌 것 같다.
         lo, hi = 0, len(tokens) - 1
-        res, cur = 0, 0
+        score = 0
 
         # power >= tokens[lo]면 왼쪽 face up 하고
         # 아니면 face down 하고?
-        while lo <= hi and cur >= 0:
+        while lo <= hi:
             if power >= tokens[lo]:
                 power -= tokens[lo]
                 lo += 1
-                cur += 1
-            else:
+                score += 1
+            elif lo < hi and score > 0:
                 power += tokens[hi]
                 hi -= 1
-                cur -= 1
-            
-            res = max(res, cur)
+                score -= 1
+            else:
+                return score
 
-        return res
+        return score
