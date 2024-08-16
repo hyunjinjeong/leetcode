85a5af89cab809339221e69b8baf7f9eb1bb3308
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-08-16 23:40:58 +0900

    Time: 33 ms (97.28%), Space: 16.5 MB (72.12%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -11,11 +11,13 @@ class Solution:
             if day not in days_set:
                 dp[day] = dp[day - 1]
             else:
-                if day < 7:
-                    dp[day] = min(dp[day - 1] + costs[0], costs[1], costs[2])
-                elif day < 30:
-                    dp[day] = min(dp[day - 1] + costs[0], dp[day - 7] + costs[1], costs[2])
-                else:
-                    dp[day] = min(dp[day - 1] + costs[0], dp[day - 7] + costs[1], dp[day - 30] + costs[2])
+                # 오 이렇게 할 수도 있구나
+                dp[day] = min(dp[day - 1] + costs[0], dp[max(day - 7, 0)] + costs[1], dp[max(day - 30, 0)] + costs[2])
+                # if day < 7:
+                #     dp[day] = min(dp[day - 1] + costs[0], costs[1], costs[2])
+                # elif day < 30:
+                #     dp[day] = min(dp[day - 1] + costs[0], dp[day - 7] + costs[1], costs[2])
+                # else:
+                #     dp[day] = min(dp[day - 1] + costs[0], dp[day - 7] + costs[1], dp[day - 30] + costs[2])
 
         return dp[-1]
