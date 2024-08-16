f2e3560f38b4885155b0aecc2d21da71578f82bc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-08-16 23:36:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = [0] * (days[-1] + 1)
+        days_set = set(days)
+
+        # DP인가?
+        # 1일짜리, 7일짜리, 30일짜리 할 수 있고
+        # 슉슉 넘어가면 될거 같은데
+        # dp[i]가 있을 때.. dp[i] = min(dp[i-30] + costs[2], dp[i-7] + costs[1], dp[i-1] + costs[0]) 인가?
+        for day in range(1, len(dp)):
+            if day not in days_set:
+                dp[day] = dp[day - 1]
+            else:
+                if day < 7:
+                    dp[day] = min(dp[day - 1] + costs[0], costs[1])
+                elif day < 30:
+                    dp[day] = min(dp[day - 1] + costs[0], dp[day - 7] + costs[1], costs[2])
+                else:
+                    dp[day] = min(dp[day - 1] + costs[0], dp[day - 7] + costs[1], dp[day - 30] + costs[2])
+
+        return dp[-1]
