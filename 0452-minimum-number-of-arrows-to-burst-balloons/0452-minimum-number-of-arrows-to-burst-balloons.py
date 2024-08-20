bb746a051397792f476753196feb2b08ee977573
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-08-20 23:12:59 +0900

    Time: 1017 ms (79.35%), Space: 62.7 MB (60.89%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,43 +1,16 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        # 중복 인터벌을 다 합치는 문제는 아니네.. 그러면 문제 정의가 흠
-        # 뭔가 중복된 인터벌을 처리하는 건 맞는데...
-        # 그럼 다시 1 8 / 2 6 / 7 12 / 10 16 예시로 가보면
-        # 2 6 / 1 8 / 7 12 / 10 16 에서
-        
-        # start로 정렬을 하면?
-        # 1 8 / 2 6 / 7 12 / 10 16
+        # 아 이렇게 풀 수도 있음.
+        # end로 정렬해서, point[start] > curr[end]면 하나씩 늘려줄 수 있음.
+        res = 1
 
-        # 겹치는 부분만 남겨볼까?
-        # 1 6이 되고 / 새로 시작해서 10 12가 될거임
-        # 그럼 2개고
-
-        # 1 2 / 2 3 / 3 4 / 4 5 는
-        # 2 2 / 4 4 -> 2개.
-        # 이거 말이 되는데?
-
-        # 그럼 겹치는 부분은 어떻게 구하지? start나 end 둘 중 하나로 정렬을 해야 하는데
-        # start로 정렬이 말이 될 것 같음
-        # start로 정렬한 다음에
-        # curr가 있으면, curr[end] >= point[start] 이면 겹치는거
-        # 겹친다고 판정이 되면, 범위는 [point[start], min(curr[end], point[end])]
-        # 이렇게 하나 넣고, 다음에 또 현재 curr에 대해서 하면 될 듯?
-        # 1 8 / 2 6 / 3 7 / 7 12 이라면
-        # 2 6이 됨. 그 다음 3 6이 됨
-        # 그러면 3 6에서 7 12가 되니까 이건 그냥 넘어가고
-        # 7 12 에서 10 16은 10 12가 되고.
         START, END = 0, 1
-        points.sort(key=lambda point: point[START])
-        points.append([float("inf"), float("inf")])
+        points.sort(key=lambda point: point[END])
 
-        overwrapping_points = []
-        curr_start, curr_end = points[0]
-        for i in range(1, len(points)):
-            point_start, point_end = points[i]
-            if point_start <= curr_end:
-                curr_start, curr_end = point_start, min(curr_end, point_end)
-            else:
-                overwrapping_points.append([curr_start, curr_end])
-                curr_start, curr_end = points[i]
+        curr_end = points[0][END]
+        for i, point in enumerate(points, start=1):
+            if point[START] > curr_end:
+                res += 1
+                curr_end = point[END]
 
-        return len(overwrapping_points)
+        return res
