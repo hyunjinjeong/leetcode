819297572e0d8bb21990df001e91ced77e49b1d6
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-08-24 23:00:26 +0900

    Time: 595 ms (78.36%), Space: 21.5 MB (52.89%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,12 +1,12 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         # 노드에 들어오는 엣지가 n-1개고 나가는 엣지가 0개면 될 듯
-        in_edges, out_edges = [0] * n, [0] * n
-        for u, v in trust: # 1부터 시작이라...
-            out_edges[u - 1] += 1
-            in_edges[v - 1] += 1
+        in_degrees, out_degrees = [0] * (n + 1), [0] * (n + 1)
+        for u, v in trust:
+            out_degrees[u] += 1
+            in_degrees[v] += 1
         
-        for i in range(n):
-            if in_edges[i] == n - 1 and out_edges[i] == 0:
-                return i + 1
+        for i in range(1, n + 1):
+            if in_degrees[i] == n - 1 and out_degrees[i] == 0:
+                return i
         return -1
