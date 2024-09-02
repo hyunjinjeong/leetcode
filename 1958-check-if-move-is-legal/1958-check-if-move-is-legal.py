9234d598106b494c4af2c774bac801ddf9699f2c
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-02 17:53:43 +0900

    Time: 45 ms (95.68%), Space: 16.3 MB (100%) - LeetHub

--- 1958-check-if-move-is-legal/1958-check-if-move-is-legal.py
+++ 1958-check-if-move-is-legal/1958-check-if-move-is-legal.py
@@ -1,29 +1,15 @@
 class Solution:
     def checkMove(self, board: List[List[str]], rMove: int, cMove: int, color: str) -> bool:
-        # (rMove, cMove) 값을 color로 바꿨을 때
-        # (rMove, cMove)가 good line의 endpoint여야 legal임
-        # good line은 길이 3 이상으로, 양 끝 cell의 색이 같고, 나머지 cell들의 색은 반대로 같아야 함
-        # 그럼 상하좌우 그리고 대각선 상하좌우... 8방향으로 해당 조건을 만족하는지 보면 되려나?
-        def check(r, c, dr, dc, length):
-            if r >= 8 or r < 0 or c >= 8 or c < 0:
-                return False
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
+            r, c = rMove + dr, cMove + dc
+            size = 2
+            while 0 <= r < 8 and 0 <= c < 8:
+                if board[r][c] == "." or board[r][c] == color and size < 3:
+                    break
+                if board[r][c] == color:
+                    return True
+                r += dr
+                c += dc
+                size += 1
 
-            res = True
-            if board[r][c] == ".":
-                res = False
-            elif board[r][c] == color:
-                res = length >= 2
-            else:
-                res = check(r + dr, c + dc, dr, dc, length + 1)
-            return res
-
-        res = (check(rMove, cMove + 1, 0, 1, 1)
-            or check(rMove, cMove - 1, 0, -1, 1)
-            or check(rMove + 1, cMove, 1, 0, 1)
-            or check(rMove - 1, cMove, -1, 0, 1)
-            or check(rMove + 1, cMove + 1, 1, 1, 1)
-            or check(rMove + 1, cMove - 1, 1, -1, 1)
-            or check(rMove - 1, cMove + 1, -1, 1, 1)
-            or check(rMove - 1, cMove - 1, -1, -1, 1))
-
-        return res
+        return False
