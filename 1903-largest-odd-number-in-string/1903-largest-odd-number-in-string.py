27dd3e886603046828fec32683ed845f30043d0d
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-05 19:07:45 +0900

    Time: 49 ms (65.35%), Space: 17.8 MB (12.33%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -1,10 +1,8 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
         # 걍 num을 돌면서 홀수가 있으면 가장 오른쪽 인덱스만 계쏙 저장하면 됨
-        right = -1
-        for i in range(len(num)):
-            c = num[i]
-            if int(c) % 2 == 1:
-                right = i
-        
-        return num[:right+1] if right > -1 else ""
+        # 거꾸로 돌면 바로 리턴할 수 있구나
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 == 1:
+                return num[:i + 1]
+        return ""
