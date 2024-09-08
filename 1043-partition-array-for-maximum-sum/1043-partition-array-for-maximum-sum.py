15395e7cb1bff78e2a0d64b2faf4c8963887905e
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-08 16:07:13 +0900

    Time: 279 ms (71.97%), Space: 16.7 MB (58.88%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -3,18 +3,14 @@ class Solution:
         # subarray마다 길이가 k개까지 갈 수 있는데...
         # 그냥 모든 경우의 수를 다 생성하는 건가?
         # 그 subarray의 합은 최댓값 * 길이가 될거고..
-        
-        @cache
-        def dfs(start):
-            if start == len(arr):
-                return 0
-            
-            res = 0 
-            for i in range(start, min(start + k, len(arr))):
-                max_value = max(arr[start:i + 1])
-                size = i - start + 1
-                res = max(res, (max_value * size) + dfs(i + 1))
-            
-            return res
-        
-        return dfs(0)
+        # 그러면 DP로 풀 수 있겠다
+        N = len(arr)
+        dp = [0] * (N + 1)
+
+        for i in range(N - 1, -1, -1):
+            max_value = 0
+            for j in range(i, min(i + k, len(arr))):
+                max_value = max(max_value, arr[j])
+                dp[i] = max(dp[i], dp[j + 1] + max_value * (j - i + 1))
+
+        return dp[0]
