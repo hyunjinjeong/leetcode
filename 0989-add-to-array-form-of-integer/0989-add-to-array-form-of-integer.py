218cf3b147d08f6229bbe7afe0041b55ac42ae67
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-25 20:55:59 +0900

    Time: 183 ms (93.96%), Space: 17.5 MB (97.3%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -3,14 +3,24 @@ class Solution:
         # num이 k보다 크면 문제가 없지만.. 작으면 문제가 된다
         # linked list로 바꾸고 계산하는 방법도 있고
         # 숫자로 바꾸고 다시 list로 바꾸는 방법도 있을 듯
-        num_int = 0
-        for n in num:
-            num_int = num_int * 10 + n
+        # reverse 해서 계산하는 방법도 있구나
 
-        added_num = num_int + k
-        res = []
-        while added_num:
-            res.append(added_num % 10)
-            added_num //= 10
+        num.reverse()
+        i = 0
+
+        while k:
+            digit = k % 10
+            if i < len(num):
+                num[i] += digit
+            else:
+                num.append(digit)
+            
+            carry = num[i] // 10
+            num[i] = num[i] % 10
+
+            k //= 10
+            k += carry
+            i += 1
         
-        return res[::-1]
+        num.reverse()
+        return num
