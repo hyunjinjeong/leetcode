74d08872b876fdbca4f1c78c7f80547781998cf7
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-27 22:34:55 +0900

    Time: 47 ms (83.22%), Space: 16.8 MB (7.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,14 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
-            return False # 음수면 항상 False
+            return False
+
+        reversed_x = 0
+        n = x
+        while n:
+            reversed_x = reversed_x * 10 + n % 10
+            n //= 10
         
-        tmp = x
-        reversed_num = 0
-        
-        # Reverse 해서 비교.
-        while tmp > 0:
-            reversed_num = reversed_num * 10 + tmp % 10 # 여기가 reverse하는 중요한 로직이구만.
-            tmp = tmp // 10
-            
-        return x == reversed_num
+        return x == reversed_x
