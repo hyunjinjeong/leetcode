d2c4444d7af2cdcc59f35d40c127d73ca01d59dc
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-27 22:31:46 +0900

    Time: 40 ms (26.71%), Space: 16.7 MB (35.8%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -3,7 +3,5 @@ class Solution:
         # loop, recursion 없이는 bit 연산인거 같은데
         # 1 / 100 / 10000 / 1000000 이런 식으로 감.
         # 그러면 일단 n - 1 이랑 and를 해서 0이 되어야 함. 그러면 2의 제곱이 되고
-        # 여기서 4의 제곱은 2진수로 나타냈을 때 길이가 홀수면 됨
-        if n == 0:
-            return False
-        return n & (n - 1) == 0 and len(bin(n)) % 2 == 1
+        # 여기서 4의 제곱은.. 그냥 숫자를 활용하면 되는구나
+        return n & (n - 1) == 0 and (n - 1) % 3 == 0
