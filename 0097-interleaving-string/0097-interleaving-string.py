45073a7946ad667c4610b030e18b45c1fe00cbed
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-09-27 11:09:04 +0900

    Time: 41 ms (63.62%), Space: 18.3 MB (5.83%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,17 +1,13 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        # dp...
-        # 엣지 케이스
+        # interleaving이니까... s1 다음에 s2가 오고... 그 다음은 s1이 오고 이런 식
         if len(s1) + len(s2) != len(s3):
             return False
         
-        dp = {}
-
+        @cache
         def dfs(i1, i2):
             if i1 == len(s1) and i2 == len(s2):
-                dp[(i1, i2)] = True
-            if (i1, i2) in dp:
-                return dp[(i1, i2)]
+                return True
             
             use_s1, use_s2 = False, False
             if i1 < len(s1) and s1[i1] == s3[i1+i2]:
@@ -19,7 +15,6 @@ class Solution:
             if i2 < len(s2) and s2[i2] == s3[i1+i2]:
                 use_s2 = dfs(i1, i2 + 1)
             
-            dp[(i1, i2)] = use_s1 or use_s2
-            return dp[(i1, i2)]
+            return use_s1 or use_s2
         
         return dfs(0, 0)
