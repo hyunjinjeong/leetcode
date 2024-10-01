2e148ff1146037cd8cc0e95367210f02136b2a8d
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-10-01 22:25:57 +0900

    Time: 2128 ms (21.17%), Space: 21.2 MB (13.46%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -3,21 +3,19 @@ class Solution:
         # 나 dp요 하고 있는 문제인데..
         # i번째를 사용하려면 이전 dp에서 조건을 만족하는 character 중 최댓값을 구해야 함..
         # 그러면 hashmap으로 index를 저장해둘까?
-        
-        def diff(a, b):
-            return abs(ord(a) - ord(b))
+        def get_candidates(c):
+            for num in range(ord(c) - k, ord(c) + k + 1):
+                yield chr(num)
         
         dp = [1] * len(s)
         index_map = {}
 
-        for i in range(len(s)):
-            # k가 있으면.. -k ~ k까지 범위의 character를 모두 확인해봐야 함
-            # 일단 간단하게 그냥 모든 케이스를 확인해보자..
-            for c in "abcdefghijklmnopqrstuvwxyz":
-                if diff(s[i], c) <= k and c in index_map:
-                    dp[i] = max(dp[i], dp[index_map[c]] + 1)
+        for i, c in enumerate(s):
+            for candidate in get_candidates(c):
+                if candidate in index_map:
+                    dp[i] = max(dp[i], dp[index_map[candidate]] + 1)
 
-            index_map[s[i]] = i
+            index_map[c] = i
         
         return max(dp)
             
