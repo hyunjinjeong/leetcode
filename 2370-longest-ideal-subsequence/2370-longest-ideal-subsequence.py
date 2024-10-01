bea341d1539cedd892bb95f2a63bf8f6839a41af
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-10-01 22:27:52 +0900

    Time: 1178 ms (59.75%), Space: 17.3 MB (49.17%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -3,19 +3,17 @@ class Solution:
         # 나 dp요 하고 있는 문제인데..
         # i번째를 사용하려면 이전 dp에서 조건을 만족하는 character 중 최댓값을 구해야 함..
         # 그러면 hashmap으로 index를 저장해둘까?
-        def get_candidates(c):
-            for num in range(ord(c) - k, ord(c) + k + 1):
-                yield chr(num)
-        
-        dp = [1] * len(s)
-        index_map = {}
+        # dp를 줄일 수 있구나
+        dp = [0] * 26
 
         for i, c in enumerate(s):
-            for candidate in get_candidates(c):
-                if candidate in index_map:
-                    dp[i] = max(dp[i], dp[index_map[candidate]] + 1)
+            index = ord(c) - ord("a")
 
-            index_map[c] = i
+            best = 0
+            for prev in range(max(0, index - k), min(26, index + k + 1)):
+                best = max(best, dp[prev])
+            
+            dp[index] = best + 1
         
         return max(dp)
             
