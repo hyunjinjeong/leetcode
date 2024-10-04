caf311b50495fa60d60e6a8e65fe59ad8aabbe14
Hyun-Jin Jeong <hyunjin.dev@gmail.com>
2024-10-04 23:43:15 +0900

    Time: 1123 ms (13.06%), Space: 42.7 MB (61.46%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -5,16 +5,16 @@ class Solution:
         # 그리고 row 단위로 정렬해서 계산..
         # 이게 어케 medium이지
         M, N = len(matrix), len(matrix[0])
-
-        for c in range(N):
-            for r in range(1, M):
-                if matrix[r][c] == 1:
-                    matrix[r][c] += matrix[r - 1][c]
-        
         res = 0
+
         for r in range(M):
+            for c in range(N):
+                if r > 0 and matrix[r][c] == 1:
+                    matrix[r][c] += matrix[r - 1][c]
+            
             curr_row = sorted(matrix[r], reverse=True)
             height = curr_row[0]
+            
             for c in range(N):
                 height = min(curr_row[c], height)
                 res = max(res, height * (c + 1))
