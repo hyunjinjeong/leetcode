67b62d7da7defe0c52d5e58d603bf482bb67f004
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-08 22:35:32 +0900

    Time: 1712 ms (45.55%), Space: 16.8 MB (82.93%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -5,27 +5,21 @@ class Solution:
         # i번째가 있으면 i를 포함하거나, 포함하지 않거나.
         # 반대로 그냥 전체 subset을 다 구해서 조건에 맞는 subset들만 필터링해서 보면 됨
         # TLE가 뜨는데.. DP인 것 같음. 어떻게 DP로 전환하지. curr를 안 쓰도록 해보자.
-
-        @cache
-        def dfs(i, m_left, n_left):
-            if i == len(strs) or m_left <= 0 and n_left <= 0:
-                return 0
-
-            if self.zeros[i] > m_left or self.ones[i] > n_left:
-                return dfs(i + 1, m_left, n_left)
-            
-            pick = 1 + dfs(i + 1, m_left - self.zeros[i], n_left - self.ones[i])
-            not_pick = dfs(i + 1, m_left, n_left)
-
-            return max(pick, not_pick)
+        # 다음은 bottom up
+        def count_zeros(s):
+            count = 0
+            for c in s:
+                if c == "0":
+                    count += 1
+            return count
         
-        self.ones = [0] * len(strs)
-        self.zeros = [0] * len(strs)
-        for i in range(len(strs)):
-            for c in strs[i]:
-                if c == "1":
-                    self.ones[i] += 1
-                else:
-                    self.zeros[i] += 1
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for s in strs:
+            zeros = count_zeros(s)
+            ones = len(s) - zeros
+
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
         
-        return dfs(0, m, n)
+        return dp[m][n]
