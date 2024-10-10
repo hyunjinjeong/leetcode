ea344f20d0431170b7feb5814f637cca0080e7bd
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-10 23:53:35 +0900

    Time: 60 ms (52.93%), Space: 18.5 MB (63.61%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
@@ -9,17 +9,19 @@ class Solution:
         # node.val보다 큰 값을 다 더해야 함
         # 즉.. 본인의 right subtree 및 부모가 자신보다 클 경우 부모 + 부모의 right subtree를 다 더해야 함
         # 그것보다.. 가장 오른쪽부터 거꾸로 돌 수 있으면 그동안의 sum을 더해가면서 할 수 있음
-        # 
+        curr_sum = 0
         
-        def dfs(node):
-            if not node:
-                return
- 
-            dfs(node.right)
-            node.val += self.curr_sum
-            self.curr_sum = node.val
-            dfs(node.left)
+        node = root
+        stack = []
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.right
+            
+            node = stack.pop()
+            node.val += curr_sum
+            curr_sum = node.val
+
+            node = node.left
         
-        self.curr_sum = 0
-        dfs(root)
         return root
