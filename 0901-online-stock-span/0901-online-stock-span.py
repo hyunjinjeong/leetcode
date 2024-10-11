58e06285dd94e5494b9f8345dac47ef1deb88433
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-11 11:13:53 +0900

    Time: 285 ms (42.65%), Space: 22.3 MB (6.83%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -3,19 +3,16 @@ class StockSpanner:
     def __init__(self):
         # 현재 가격보다 높은 가격의 날을 알아야 함
         # monotonic stack이면 될 듯? decreasing으로 가야 하나
-        self.day = 0
         self.stack = []
 
     def next(self, price: int) -> int:
-        self.day += 1
-
+        # 이전 결과를 재활용할 수 있구나
+        res = 1
         while self.stack and self.stack[-1][0] <= price:
-            self.stack.pop()
-        
-        last_day = self.stack[-1][1] if self.stack else 0
-        self.stack.append((price, self.day))
-        
-        return self.day - last_day
+            res += self.stack.pop()[1]
+    
+        self.stack.append((price, res))    
+        return res
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
