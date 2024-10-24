a3356f866ebcf279a9a50030075b528a46667025
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-24 23:04:31 +0900

    Time: 0 ms (100%), Space: 16.6 MB (97.96%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -4,11 +4,10 @@ class Solution:
         N = len(pushed)
         
         stack = []
-        
-        push_index, pop_index = 0, 0
-        while push_index < N:
+
+        pop_index = 0
+        for push_index in range(N):
             stack.append(pushed[push_index])
-            push_index += 1
             while stack and stack[-1] == popped[pop_index]:
                 stack.pop()
                 pop_index += 1
