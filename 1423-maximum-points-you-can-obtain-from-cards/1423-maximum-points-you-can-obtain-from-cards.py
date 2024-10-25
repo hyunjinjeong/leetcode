f67ad4c70cabbfebec1a443633bc2928fbdd06ae
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-25 23:38:22 +0900

    Time: 26 ms (96.96%), Space: 27.1 MB (93.36%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        # 이거.. 그 반대로 생각하는거 아닌가?
+        # len(cardPoints) - k 길이의 subarray 합 중 minimum을 구해서 sum에서 빼는?
+        N = len(cardPoints)
+        all_sum = sum(cardPoints)
+
+        rest_length = N - k
+        if rest_length == 0:
+            return all_sum
+
+        rest_sum = sum(cardPoints[:rest_length - 1])
+        min_rest_sum = all_sum
+
+        for i in range(rest_length - 1, N):
+            rest_sum += cardPoints[i]
+            min_rest_sum = min(min_rest_sum, rest_sum)
+            rest_sum -= cardPoints[i - rest_length + 1]
+        
+        return all_sum - min_rest_sum
