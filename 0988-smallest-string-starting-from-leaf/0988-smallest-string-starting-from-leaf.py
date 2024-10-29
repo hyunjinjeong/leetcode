ce486d96c37ed7fd180354e133a89b2485eed689
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-29 22:46:50 +0900

    Time: 3 ms (73.03%), Space: 17.8 MB (35.27%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -8,39 +8,21 @@ class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
         # backtracking 해서 하나씩 보면 되려나?
 
-        def is_smaller(arr1, arr2):
-            if not arr2:
-                return True
-
-            i, j = 0, 0
-            while i < len(arr1) and j < len(arr2):
-                c1, c2 = arr1[i], arr2[j]
-                if c1 < c2:
-                    return True
-                if c1 > c2:
-                    return False
-                i += 1
-                j += 1
-            
-            return len(arr1) < len(arr2)
-
-        def dfs(node, arr):
-            arr.append(chr(ord('a') + node.val))
+        def dfs(node, string):
+            # array를 써도 어차피 reverse해야 해서 그냥 string 쓰면 되겠다.
+            string = chr(ord('a') + node.val) + string
 
             if node.left and node.right:
-                dfs(node.left, arr)
-                dfs(node.right, arr)
+                dfs(node.left, string)
+                dfs(node.right, string)
             elif node.left:
-                dfs(node.left, arr)
+                dfs(node.left, string)
             elif node.right:
-                dfs(node.right, arr)
+                dfs(node.right, string)
             else:
-                reversed_arr = list(reversed(arr))
-                if is_smaller(reversed_arr, self.smallest):
-                    self.smallest = reversed_arr[:]
-            
-            arr.pop()
+                if not self.smallest or string < self.smallest:
+                    self.smallest = string
 
-        self.smallest = []
-        dfs(root, [])
-        return "".join(self.smallest)
+        self.smallest = ""
+        dfs(root, "")
+        return self.smallest
