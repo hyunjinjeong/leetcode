543377d156ac9b496f709421c0472d32ae88824d
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-10-31 20:31:23 +0900

    Time: 7 ms (42.54%), Space: 16.6 MB (72.34%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,31 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # 큰 수부터 돌리면 되지 않을까?
+        roman = {
+            1: "I",
+            4: "IV",
+            5: "V",
+            9: "IX",
+            10: "X",
+            40: "XL",
+            50: "L",
+            90: "XC",
+            100: "C",
+            400: "CD",
+            500: "D",
+            900: "CM",
+            1000: "M"
+        }
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+        i = 0
+        res = []
+        while num:
+            value = values[i]
+            if num >= value:
+                res.append(roman[value])
+                num -= value
+            else:
+                i += 1
+        
+        return "".join(res)
