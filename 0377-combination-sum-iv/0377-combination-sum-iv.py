abe569b7a27e38fdf7324e185abdc6972cf34a4f
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-03 17:57:01 +0900

    Time: 37 ms (75.45%), Space: 16.9 MB (5.83%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,17 +1,18 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        # dp였구나..
-        # dp[n]: 값이 n일 때의 조합 갯수
-        # dp[i] += dp[i-num]
-        dp = [0] * (target+1)
-
-        for i in range(1, target+1):
-            for num in nums:
-                if i < num:
-                    continue
-                elif i == num: # 엣지 케이스
-                    dp[i] += 1
-                elif i >= num:
-                    dp[i] += dp[i-num]
+        # backtracking?
+        
+        @cache
+        def dfs(current):
+            if current > target:
+                return 0
+            if current == target:
+                return 1
             
-        return dp[-1]
+            res = 0
+            for i in range(len(nums)):
+                res += dfs(current + nums[i])
+            
+            return res
+        
+        return dfs(0)
