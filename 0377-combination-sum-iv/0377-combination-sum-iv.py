399895afc1456b909533a9b61f2f013680578758
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-03 18:02:29 +0900

    Time: 38 ms (69.48%), Space: 16.5 MB (62.7%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,18 +1,29 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        # backtracking?
+        # # backtracking?
         
-        @cache
-        def dfs(current):
-            if current > target:
-                return 0
-            if current == target:
-                return 1
+        # @cache
+        # def dfs(current):
+        #     if current > target:
+        #         return 0
+        #     if current == target:
+        #         return 1
             
-            res = 0
-            for i in range(len(nums)):
-                res += dfs(current + nums[i])
+        #     res = 0
+        #     for i in range(len(nums)):
+        #         res += dfs(current + nums[i])
             
-            return res
+        #     return res
         
-        return dfs(0)
+        # return dfs(0)
+
+        # bottom up dp
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for curr in range(1, target + 1):
+            for num in nums:
+                if curr >= num:
+                    dp[curr] += dp[curr - num]
+        
+        return dp[target]
