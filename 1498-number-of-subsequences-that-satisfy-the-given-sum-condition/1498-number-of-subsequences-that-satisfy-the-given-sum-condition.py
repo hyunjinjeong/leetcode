bca4fbf03e831d18b01648658bc606700eee475d
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-07 16:51:51 +0900

    Time: 748 ms (55.93%), Space: 27.5 MB (79.13%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -8,7 +8,7 @@ class Solution:
         left, right = 0, len(nums) - 1
         while left <= right:
             if nums[left] + nums[right] <= target:
-                res = (res + (2 ** (right - left))) % MOD
+                res = (res + (1 << (right - left))) % MOD
                 left += 1
             else:
                 right -= 1
