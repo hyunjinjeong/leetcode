aab9e18761b4e255376f720a2a010dad090bd809
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-08 16:47:08 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,34 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        # DP나 backtracking 같이 생겼네
+        N = len(board)
+
+        def coordinates(num):
+            length = N - 1
+            index = num - 1
+            quotient, remainder = divmod(index, N)
+
+            row = length - quotient
+            col = remainder if quotient % 2 == 0 else length - remainder
+            return (row, col)
+
+        # 뭔가 BFS가 더 편한 듯
+        q = collections.deque([(1, 0)])
+        visited = set()
+        while q:
+            for _ in range(len(q)):
+                curr, count = q.popleft()
+                if curr == N ** 2:
+                    return count
+                
+                visited.add(curr)
+                for next_cell in range(curr + 1, min(curr + 6, N ** 2) + 1):
+                    if next_cell in visited:
+                        continue
+                    r, c = coordinates(next_cell)
+                    if board[r][c] != -1:
+                        q.append((board[r][c], count + 1))
+                    else:
+                        q.append((next_cell, count + 1))
+                    visited.add(next_cell)
+        return -1
