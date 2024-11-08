d952e771e0c19be2938dd74881b35b6ee68278d1
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-08 16:47:11 +0900

    Time: 19 ms (72.94%), Space: 17 MB (5.9%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -13,22 +13,25 @@ class Solution:
             return (row, col)
 
         # 뭔가 BFS가 더 편한 듯
-        q = collections.deque([(1, 0)])
         visited = set()
+
+        q = collections.deque([(1, 0)])
         while q:
             for _ in range(len(q)):
                 curr, count = q.popleft()
                 if curr == N ** 2:
                     return count
                 
-                visited.add(curr)
                 for next_cell in range(curr + 1, min(curr + 6, N ** 2) + 1):
                     if next_cell in visited:
                         continue
+                    
                     r, c = coordinates(next_cell)
                     if board[r][c] != -1:
                         q.append((board[r][c], count + 1))
                     else:
                         q.append((next_cell, count + 1))
+                    
                     visited.add(next_cell)
+        
         return -1
