a047847f118cbeb28a00657ef5e0cd953ac5f2d7
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-08 15:50:08 +0900

    Time: 310 ms (10.59%), Space: 86.7 MB (8.63%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,15 +1,13 @@
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         # root에서 시작하면 가지가 갈라지는 경우가 계산이 안되는구나
-        def dfs(city):
-            visited.add(city)
-
+        def dfs(city, prev):
             sub_cost, sub_count = 0, 0
             for nei in graph[city]:
-                if nei in visited:
+                if nei == prev:
                     continue
                 
-                sub_result = dfs(nei)
+                sub_result = dfs(nei, city)
                 sub_cost += sub_result[0]
                 sub_count += sub_result[1]
             
@@ -25,11 +23,8 @@ class Solution:
             graph[src].append(dst)
             graph[dst].append(src)
 
-        visited = set()
-        visited.add(0)
-
         res = 0
         for nei in graph[0]:
-            res += dfs(nei)[0]
+            res += dfs(nei, 0)[0]
 
         return res
