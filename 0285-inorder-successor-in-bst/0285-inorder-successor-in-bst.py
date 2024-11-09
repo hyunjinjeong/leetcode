c1039c0c9d4eed27b0335a75138857bd1c2748f3
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-09 22:05:57 +0900

    Time: 51 ms (73.77%), Space: 20.3 MB (54.17%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -8,26 +8,15 @@
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
         
-        # def leftmost(node):
-        #     if node.left:
-        #         return leftmost(node.left)
-        #     return node
-
-        # def find_parent(node):
-        #     if not node:
-        #         return
-            
-        #     if node.left is p or node.right is p:
-        #         return node
-            
         def inorder(node):
             if not node:
                 return
             
             inorder(node.left)
             
-            if self.prev is p:
+            if self.prev is p and not self.res:
                 self.res = node
+                return
             self.prev = node
 
             inorder(node.right)
