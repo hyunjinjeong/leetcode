3c2e091408181f9a4d2a3dce1ccb50911c51f678
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-21 21:48:12 +0900

    Time: 3 ms (99.06%), Space: 16.8 MB (62.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dt = collections.defaultdict(int)
-        for c in s:
-            dt[c] += 1
-        
-        for c in t:
-            if c not in dt:
+        s_counter, t_counter = collections.Counter(s), collections.Counter(t)
+
+        for c in "abcdefghijklmnopqrstuvwxyz":
+            if s_counter[c] != t_counter[c]:
                 return False
-            dt[c] -= 1
         
-        return all(v == 0 for v in dt.values())
+        return True
