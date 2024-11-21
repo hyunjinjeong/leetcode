f321f22c99e3a01720f915208ee124b631c117bd
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-21 22:09:18 +0900

    Time: 4 ms (71.28%), Space: 20.4 MB (59.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,39 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # # 1. one-liner ㅎㅎ
-        # return [tup[0] for tup in collections.Counter(nums).most_common(k)]
-    
-#         # 2. 표준 방법. heap을 사용하면 된다!
-#         counter = collections.Counter(nums) # O(N)
-#         heap = []
-        
-#         for num in counter: # O(NlogK)
-#             count = counter[num]
-#             heapq.heappush(heap, (count, num))
-#             if len(heap) == k + 1:
-#                 heapq.heappop(heap)
-        
-#         return [tup[1] for tup in heap] # O(K)
-    
-#         counter = collections.Counter(nums) # O(N)
-#         heap = [(-count, num) for num, count in counter.items()] # O(N)
-#         heapq.heapify(heap) # O(N)
-        
-#         ans = []
-#         for _ in range(k): # O(KlogN)
-#             ans.append(heapq.heappop(heap)[1])
-        
-#         return ans
-        # 3. bucket sort를 이용하면 O(N)에 가능
+
+        # 3 2 4 1 5면 
+        # 3 2 -> 3 4 -> 1 4 -> 4 5
+
+        # Build a counter and use k-sized min-heap
         counter = collections.Counter(nums)
+        min_heap = []
+
+        for num, count in counter.items():
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, (count, num))
+            else:
+                heapq.heappushpop(min_heap, (count, num))
+
+        res = []
+        for _ in range(k):
+            _, num = heapq.heappop(min_heap)
+            res.append(num)
         
-        bucket = [[] for _ in range(len(nums)+1)]
-        for num, freq in counter.items():
-            bucket[freq].append(num)
-        
-        ans = []
-        for i in range(len(nums), -1, -1):
-            for num in bucket[i]:
-                ans.append(num)
-                if len(ans) == k:
-                    return ans
+        return res
+
