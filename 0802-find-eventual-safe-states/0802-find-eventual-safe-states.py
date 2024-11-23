31934ed3cf412f94fd55d595d0a420aa3c422051
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-23 23:11:38 +0900

    Time: 99 ms (12.48%), Space: 23.4 MB (55.74%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -2,36 +2,27 @@ class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         N = len(graph)
 
-        self.safe_nodes = set()
+        reversed_graph = {i: [] for i in range(N)}
+        in_degree = {i: 0 for i in range(N)}
+
         for node in range(N):
-            if not graph[node]:
-                self.safe_nodes.add(node)
-        
-        # dfs 돌리기?
-        # 모든 nei에 대해서
-        # nei가 safe node면 true
-        # 아니면 false
-        # 해서 all()을 만족하면 걔도 safe_nodes에 추가
-        # 이런 식으로...
-
-        def dfs(node, visited):
-            if node in self.safe_nodes:
-                return True
-            
-            visited.add(node)
-
-            is_safe = True
             for nei in graph[node]:
-                if nei in visited:
-                    is_safe = is_safe and nei in self.safe_nodes
-                else:
-                    is_safe = is_safe and dfs(nei, visited)
-                                    
-            if is_safe:
-                self.safe_nodes.add(node)
-            return is_safe
-
-        for node in range(N):
-            dfs(node, set())
+                reversed_graph[nei].append(node)
+                in_degree[node] += 1
         
-        return sorted(list(self.safe_nodes))
+        q = collections.deque()
+        for node in range(N):
+            if in_degree[node] == 0:
+                q.append(node)
+        
+        res = []
+        while q:
+            node = q.popleft()
+            res.append(node)
+
+            for nei in reversed_graph[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    q.append(nei)
+        
+        return sorted(res)
