1d9e49a67a87ca6e693479562f394bf2f0209e7b
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-26 21:44:22 +0900

    Time: 56 ms (29.01%), Space: 30 MB (50.4%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -3,9 +3,19 @@ class Solution:
         # non-constant space면.. set을 만들어서 다 넣고
         # 1부터 len(nums)까지 존재하는지 확인
         # 이거를 constant space로 하려면..
-        num_set = set(nums)
-        for num in range(1, len(nums) + 1):
-            if num not in num_set:
-                return num
+        # num이 1 ~ len(nums)까지라는게 중요하구나... in-place로 마킹하면 됨
+        N = len(nums)
+        for i in range(N):
+            num = nums[i]
+            if num > 2**31:
+                num = num - 2**32
+
+            if 1 <= num <= N and nums[num - 1] <= 2**31:
+                nums[num - 1] = 2**32 + nums[num - 1]
         
-        return len(nums) + 1
+        for i in range(N):
+            num = nums[i]
+            if num <= 2**31:
+                return i + 1
+        
+        return N + 1
