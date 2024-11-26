f49c78856e029d2de1e127e49714e1cea899d80d
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-26 21:49:31 +0900

    Time: 43 ms (60.03%), Space: 28.4 MB (61.22%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,21 +1,35 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # non-constant space면.. set을 만들어서 다 넣고
-        # 1부터 len(nums)까지 존재하는지 확인
-        # 이거를 constant space로 하려면..
-        # num이 1 ~ len(nums)까지라는게 중요하구나... in-place로 마킹하면 됨
-        N = len(nums)
-        for i in range(N):
-            num = nums[i]
-            if num > 2**31:
-                num = num - 2**32
+        # # non-constant space면.. set을 만들어서 다 넣고
+        # # 1부터 len(nums)까지 존재하는지 확인
+        # # 이거를 constant space로 하려면..
+        # # num이 1 ~ len(nums)까지라는게 중요하구나... in-place로 마킹하면 됨
+        # N = len(nums)
+        # for i in range(N):
+        #     num = nums[i]
+        #     if num > 2**31:
+        #         num = num - 2**32
 
-            if 1 <= num <= N and nums[num - 1] <= 2**31:
-                nums[num - 1] = 2**32 + nums[num - 1]
+        #     if 1 <= num <= N and nums[num - 1] <= 2**31:
+        #         nums[num - 1] = 2**32 + nums[num - 1]
         
+        # for i in range(N):
+        #     num = nums[i]
+        #     if num <= 2**31:
+        #         return i + 1
+        
+        # return N + 1
+        N = len(nums)
+
         for i in range(N):
-            num = nums[i]
-            if num <= 2**31:
-                return i + 1
+            while 1 <= nums[i] <= N and nums[i] != nums[nums[i] - 1]:
+                tmp = nums[i] - 1
+                nums[i], nums[tmp] = nums[tmp], nums[i]
         
-        return N + 1
+        res = N + 1
+        for i, num in enumerate(nums):
+            if num != i + 1:
+                res = i + 1
+                break
+        
+        return res
