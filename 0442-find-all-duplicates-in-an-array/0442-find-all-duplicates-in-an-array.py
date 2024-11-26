9f6f447c67567297ac83ba866435f40949125cca
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-26 22:07:23 +0900

    Time: 40 ms (18.88%), Space: 23.4 MB (64.32%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -5,13 +5,26 @@ class Solution:
         # Flip (num - 1)th index
         # If the value has been already fliped, that means the num is duplicated
 
+        # res = []
+        # for i in range(N):
+        #     num = abs(nums[i])
+
+        #     if nums[num - 1] < 0:
+        #         res.append(num)
+        #     else:
+        #         nums[num - 1] *= -1
+        
+        # return res
+
+        # cycle sort도 되네
+        for i in range(N):
+            while i != nums[i] - 1 and nums[i] != nums[nums[i] - 1]:
+                # nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        
         res = []
         for i in range(N):
-            num = abs(nums[i])
-
-            if nums[num - 1] < 0:
-                res.append(num)
-            else:
-                nums[num - 1] *= -1
+            if nums[i] != i + 1:
+                res.append(nums[i])
         
         return res
