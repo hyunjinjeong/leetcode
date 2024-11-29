340854d618956d9bebc67ed5004d6d100d3f3df6
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-11-29 14:52:44 +0900

    Time: 0 ms (100%), Space: 17.2 MB (10.21%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,15 +1,26 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         
-        @cache
-        def dfs(start, end):
-            res = 0
-            for root in range(start, end + 1):
-                left = dfs(start, root - 1) or 1
-                right = dfs(root + 1, end) or 1
-                res += left * right
+        # @cache
+        # def dfs(start, end):
+        #     res = 0
+        #     for root in range(start, end + 1):
+        #         left = dfs(start, root - 1) or 1
+        #         right = dfs(root + 1, end) or 1
+        #         res += left * right
 
-            return res
+        #     return res
 
         
-        return dfs(1, n)
+        # return dfs(1, n)
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for node in range(1, n + 1):
+            for root in range(1, node + 1):
+                left = root - 1
+                right = node - root
+                dp[node] += dp[left] * dp[right]
+
+        return dp[n]
