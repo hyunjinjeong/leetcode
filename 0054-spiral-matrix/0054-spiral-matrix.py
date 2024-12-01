679e01e254898e29e3da337c728a31e1256cc5de
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-01 17:04:08 +0900

    Time: 0 ms (100%), Space: 17.2 MB (6.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,31 +1,16 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         M, N = len(matrix), len(matrix[0])
-        RIGHT, DOWN, LEFT, UP = 0, 1, 2, 3
-        ans = []
+        res = []
 
-        first_row, first_col = 0, 0
-        last_row, last_col = M - 1, N - 1
-        direction = 0
+        x, y, dx, dy = 0, 0, 0, 1
+        for _ in range(M * N):
+            res.append(matrix[x][y])
+            matrix[x][y] = "-"
+            
+            if not 0 <= x + dx < M or not 0 <= y + dy < N or matrix[x + dx][y + dy] == "-":
+                dx, dy = dy, -dx
 
-        while first_row <= last_row and first_col <= last_col:
-            if direction == RIGHT:
-                for col in range(first_col, last_col + 1):
-                    ans.append(matrix[first_row][col])
-                first_row += 1
-            elif direction == DOWN:
-                for row in range(first_row, last_row + 1):
-                    ans.append(matrix[row][last_col])
-                last_col -= 1
-            elif direction == LEFT:
-                for col in range(last_col, first_col - 1, -1):
-                    ans.append(matrix[last_row][col])
-                last_row -= 1
-            else:
-                for row in range(last_row, first_row - 1, -1):
-                    ans.append(matrix[row][first_col])
-                first_col += 1
-
-            direction = (direction + 1) % 4
+            x, y = x + dx, y + dy
         
-        return ans
+        return res
