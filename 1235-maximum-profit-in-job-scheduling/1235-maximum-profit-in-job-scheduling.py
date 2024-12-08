a0813d100e24cbb4eb84ef3497980a0c02955202
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-08 11:36:32 +0900

    Time: 267 ms (36.19%), Space: 29.7 MB (75.71%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,5 +1,19 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+
+        def bisect_left(target):
+            START = 0
+            left, right = 0, len(jobs)
+            
+            while left < right:
+                mid = (left + right) // 2
+                if jobs[mid][START] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            
+            return left
+
         START, END, PROFIT = 0, 1, 2
         
         # startTime을 기준으로 정렬
@@ -8,20 +22,8 @@ class Solution:
         
         for i in range(len(jobs)-1, -1, -1):
             # binary search를 이용해서, 현재 job의 endTime보다 큰 startTime을 가진 job을 찾음. 맨 왼쪽.
-            k = self.bisect_left(jobs, jobs[i][END])
+            k = bisect_left(jobs[i][END])
             dp[i] = max(jobs[i][PROFIT] + dp[k], dp[i+1])
         
         return dp[0]
     
-    def bisect_left(self, jobs, target):
-        START = 0
-        left, right = 0, len(jobs)
-        
-        while left < right:
-            mid = (left + right) // 2
-            if jobs[mid][START] < target:
-                left = mid + 1
-            else:
-                right = mid
-        
-        return left
