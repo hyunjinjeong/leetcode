62805ff8e4ad1c3098b3266ab653ea19b2167ad1
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-09 23:24:14 +0900

    Time: 168 ms (19.29%), Space: 57.5 MB (27.82%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # N이 있을 때, N.val보다 큰 값을 가진 노드가 오른쪽에 있으면 해당 노드를 삭제.
+        # 그러면 원패스로는 일단 불가능함. 왜냐면 오른쪽 노드를 알 수가 없다.
+        # two pass로는? 추가 공간을 쓰면 쉽..나?
+        # monotonic stack을 써서 구성할 수 있겠는데
+        stack = []
+
+        node = head
+        while node:
+            while stack and stack[-1] < node.val:
+                stack.pop()
+            stack.append(node.val)
+            node = node.next
+
+        dummy = ListNode()
+        node = dummy
+        for val in stack:
+            node.next = ListNode(val)
+            node = node.next
+        
+        return dummy.next
