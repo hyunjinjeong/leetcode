29843ce36d51aa380d70b6ebf507017f15b0f8ac
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-21 14:32:00 +0900

    Time: 11 ms (48.62%), Space: 19.5 MB (27.61%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -6,17 +6,29 @@ class Solution:
         # 각 숫자에 대해서 생각해보자. 그러면 nums[i]를 지우면 얻을 수 있는 값은 count[nums[i]] * nums[i]임
         # 대신 기회비용으로 손해보는 값이 생기고...
         # pick & not pick으로 가능하려나? count만 미리 계산해두면 될 듯?
-        
-        count = collections.Counter(nums)
-        min_num, max_num = min(nums), max(nums)
 
-        @cache
-        def dfs(num):
-            if num > max_num:
-                return 0
+        # @cache
+        # def dfs(num):
+        #     if num > max_num:
+        #         return 0
             
-            pick = count[num] * num + dfs(num + 2)
-            not_pick = dfs(num + 1)
-            return max(pick, not_pick)
+        #     pick = count[num] * num + dfs(num + 2)
+        #     not_pick = dfs(num + 1)
+        #     return max(pick, not_pick)
         
-        return dfs(min_num)
+        # return dfs(min_num)
+
+        # bottom up으로 해봅시당
+        N = len(nums)
+
+        count = collections.Counter(nums)
+        max_num = max(nums)
+        dp = [0] * (max_num + 1)
+        dp[1] = count[1]
+
+        for num in range(2, max_num + 1):
+            pick = count[num] * num + dp[num - 2]
+            not_pick = dp[num - 1]
+            dp[num] = max(pick, not_pick)
+        
+        return dp[max_num]
