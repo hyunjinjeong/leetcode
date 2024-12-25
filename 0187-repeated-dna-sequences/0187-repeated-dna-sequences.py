5a94bf3b77d628827c9d9ccc4ef9995a21096dbd
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-25 17:36:49 +0900

    Time: 56 ms (7.93%), Space: 28.9 MB (13.51%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -2,13 +2,14 @@ class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         # two pointer 쓰면 될 듯? 길이 10을 유지하면서
         seen = collections.defaultdict(int)
-        curr = ''
+        curr = collections.deque()
 
-        for right, c in enumerate(s):
-            curr += c
+        for c in s:
+            curr.append(c)
             if len(curr) == 10:
-                seen[curr] += 1
-                curr = curr[1:]
+                word = ''.join(curr)
+                seen[word] += 1
+                curr.popleft()
         
         res = []
         for word in seen:
