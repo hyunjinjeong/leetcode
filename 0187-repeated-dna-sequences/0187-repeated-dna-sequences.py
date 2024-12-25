def0f182176822c0e0cc63db563e71c66ce4233f
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-25 17:43:18 +0900

    Time: 32 ms (26.54%), Space: 28.9 MB (13.51%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -3,7 +3,7 @@ class Solution:
         seen = collections.defaultdict(int)
 
         res = []
-        for i in range(len(s) - 10):
+        for i in range(len(s) - 9): # N이 딱 10이라고 하면 -10을 하면 안 되는구나
             seq = s[i:i + 10]
             seen[seq] += 1
             if seen[seq] == 2:
