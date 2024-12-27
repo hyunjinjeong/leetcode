49ae0448f46fac3cf784786cb04ce665c6969bcc
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-27 09:54:17 +0900

    Time: 0 ms (100%), Space: 17.8 MB (8.17%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        # leaf에서부터 시작해서 모든 노드의 값을 1로 만들면 되는구나
+        # 그럼 4 0 0 0 같은 경우
+        # 4 0 -2 1
+        # 4 -5 1 1
+        # 1 1 1 1 (-6)
+        def post_order(node):
+            if not node:
+                return 0
+            
+            left = post_order(node.left)
+            right = post_order(node.right)
+            self.res += abs(left + right + node.val - 1)
+            return left + right + node.val - 1
+        
+        self.res = 0
+        post_order(root)
+        return self.res
