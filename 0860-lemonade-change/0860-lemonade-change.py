84cb852f2e3e9aab9bda7d6364ed4fcd7f541b39
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-30 22:39:20 +0900

    Time: 7 ms (33.56%), Space: 22 MB (6.14%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -6,11 +6,15 @@ class Solution:
             paid_bills[bill] += 1
 
             change = bill - 5 # could be 0, 5, 15
+            # 아 15일 땐 5 * 3으로도 계산할 수 있음.
             if change == 5:
                 paid_bills[5] -= 1
             if change == 15:
-                paid_bills[5] -= 1
-                paid_bills[10] -= 1
+                if paid_bills[10]:
+                    paid_bills[10] -= 1
+                    paid_bills[5] -= 1
+                else:
+                    paid_bills[5] -= 3
             
             if paid_bills[5] < 0 or paid_bills[10] < 0:
                 return False
