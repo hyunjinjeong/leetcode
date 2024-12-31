e7c2ca1eed540a72bda6bdab4ff1ba6f29d15d48
Hyunjin Jeong <hyunjin.dev@gmail.com>
2024-12-31 14:43:39 +0900

    Time: 3 ms (72.81%), Space: 18 MB (8.86%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -11,30 +11,27 @@ class Solution:
             blue_graph[u].append(v)
 
         res = [-1] * n
+        
         # 노드 0부터 각각 blue 시작, red 시작 각각 돌리면 될 듯?
-        for target in range(n):
-            q = collections.deque()
-            q.append((0, BLUE, 0))
-            q.append((0, RED, 0)) # 이렇게 두 개 넣어야 하나..?
-            red_visited = set()
-            blue_visited = set()
+        q = collections.deque()
+        q.append((0, BLUE, 0))
+        q.append((0, RED, 0)) # 이렇게 두 개 넣어야 하나..?
+        visited = set()
 
-            while q:
-                node, color, length = q.popleft()
+        while q:
+            node, color, length = q.popleft()
+            visited.add((node, color))
 
-                if node == target and res[target] == -1:
-                    res[node] = length
-                    continue
-                
-                if color == BLUE:
-                    blue_visited.add(node)
-                    for adj in red_graph[node]:
-                        if adj not in red_visited:
-                            q.append((adj, RED, length + 1))
-                else:
-                    red_visited.add(node)
-                    for adj in blue_graph[node]:
-                        if adj not in blue_visited:
-                            q.append((adj, BLUE, length + 1))
+            if res[node] == -1:
+                res[node] = length
+            
+            if color == BLUE:
+                for adj in red_graph[node]:
+                    if (adj, RED) not in visited:
+                        q.append((adj, RED, length + 1))
+            else:
+                for adj in blue_graph[node]:
+                    if (adj, BLUE) not in visited:
+                        q.append((adj, BLUE, length + 1))
         
         return res
