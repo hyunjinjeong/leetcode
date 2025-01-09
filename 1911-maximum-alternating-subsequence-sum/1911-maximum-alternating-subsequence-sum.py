bcb0473a211928f219060d6f73c61e426940d6d0
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-01-09 23:10:33 +0900

    Time: 909 ms (64.99%), Space: 32.3 MB (57.23%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -4,16 +4,24 @@ class Solution:
         # 짝수 인덱스는 최대한 작게, 홀수 인덱스는 최대한 크게 해야 함.
         # 이거 그냥 0-1 knapsack 같은데... 
 
-        @cache
-        def dfs(i, is_odd):
-            if i == len(nums):
-                return 0
+        # @cache
+        # def dfs(i, is_odd):
+        #     if i == len(nums):
+        #         return 0
 
-            not_pick = dfs(i + 1, is_odd)
+        #     not_pick = dfs(i + 1, is_odd)
 
-            num = nums[i] if is_odd else -nums[i]
-            pick = num + dfs(i + 1, not is_odd)
+        #     num = nums[i] if is_odd else -nums[i]
+        #     pick = num + dfs(i + 1, not is_odd)
 
-            return max(pick, not_pick)
+        #     return max(pick, not_pick)
         
-        return dfs(0, True)
+        # return dfs(0, True)
+
+        # 이렇게 greedy로도 됨
+        even_sum, odd_sum = 0, 0
+
+        for i, num in enumerate(nums):
+            even_sum, odd_sum = max(odd_sum + num, even_sum), max(even_sum - num, odd_sum)
+        
+        return even_sum
