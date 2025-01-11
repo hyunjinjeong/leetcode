cbdd2676a24062ef0d6a0c17a8c831e735973773
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-01-11 19:48:25 +0900

    Time: 158 ms (93.81%), Space: 59.3 MB (89.6%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -5,21 +5,38 @@ class Solution:
         # 그런데 1-n 사이에 최소 하나의 경로라도 있다는건 1에서 출발해서 들리는 모든 경로가 집합에 포함된다.
         # 그럼 걍 최솟값을 구하면 될 듯?
 
-        graph = {i: [] for i in range(1, n + 1)}
+        # graph = {i: [] for i in range(1, n + 1)}
+        # for start, end, cost in roads:
+        #     graph[start].append((end, cost))
+        #     graph[end].append((start, cost))
+        
+        # res = float("inf")
+        # q = collections.deque([1])
+        # visited = set()
+        
+        # while q:
+        #     city = q.popleft()
+        #     for nei, cost in graph[city]:
+        #         res = min(cost, res)
+        #         if nei not in visited:
+        #             visited.add(nei)
+        #             q.append(nei)
+        
+        # return res
+
+        # union find도 가능
+
+        parent = [i for i in range(n + 1)]
+        rank = {i: float("inf") for i in range(n + 1)}
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
         for start, end, cost in roads:
-            graph[start].append((end, cost))
-            graph[end].append((start, cost))
+            start, end = find(start), find(end)
+            rank[end] = min(rank[start], rank[end], cost)
+            parent[start] = parent[end]
         
-        res = float("inf")
-        q = collections.deque([1])
-        visited = set()
-        
-        while q:
-            city = q.popleft()
-            visited.add(city)
-            for nei, cost in graph[city]:
-                if nei not in visited:
-                    res = min(cost, res)
-                    q.append(nei)
-        
-        return res
+        return rank[find(1)]
