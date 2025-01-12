476de6c5671f207521d877a2545eaf50c03bf116
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-01-12 15:25:52 +0900

    Time: 383 ms (24.02%), Space: 118.7 MB (7.48%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -9,12 +9,8 @@ class Solution:
                 return 1 if i == 0 else 0 # stay
             
             stay = dfs(i, step + 1)
-            
-            left, right = 0, 0
-            if i > 0:
-                left = dfs(i - 1, step + 1)
-            if i < arrLen - 1:
-                right = dfs(i + 1, step + 1)
+            left = dfs(i - 1, step + 1) if i > 0 else 0
+            right = dfs(i + 1, step + 1) if i < arrLen - 1 else 0
             
             return (((stay + left) % MOD) + right) % MOD
 
