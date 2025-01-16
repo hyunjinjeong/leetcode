43868d0eb8c1238997faaae6eaa6a9220ee1f7af
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-01-16 23:27:52 +0900

    Time: 320 ms (90.41%), Space: 18.9 MB (13.65%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
@@ -2,7 +2,7 @@ class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
         ROW, COL = len(matrix), len(matrix[0])
         
-        # 이번엔 MLE네... DP가 아니구나
+        # ...?
         prefix_sum = [[0] * COL for _ in range(ROW)]
         for x in range(ROW):
             for y in range(COL):
@@ -14,13 +14,13 @@ class Solution:
         res = 0
         for r1 in range(ROW):
             for r2 in range(r1, ROW):
-                counter = collections.defaultdict(int)
+                counter = {}
                 counter[0] = 1
                 for col in range(COL):
                     top = prefix_sum[r1 - 1][col] if r1 > 0 else 0
                     curr_sum = prefix_sum[r2][col] - top
-
-                    res += counter[curr_sum - target]
-                    counter[curr_sum] += 1
+                    if curr_sum - target in counter:
+                        res += counter[curr_sum - target]
+                    counter[curr_sum] = counter.get(curr_sum, 0) + 1
 
         return res
