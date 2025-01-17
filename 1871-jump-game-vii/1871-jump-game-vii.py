705c44feeeaea244651c8e8e63a768c012942ea4
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-01-17 22:36:26 +0900

    Time: 219 ms (41.41%), Space: 19.3 MB (83.96%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.py
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -1,21 +1,32 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
-        # 그러니까 i보다 오른쪽에 있는 것 중 minJump ~ maxJump 사이의 구간에 0인 친구로만 점프가 가능하다 이거구만
-        if s[-1] == "1":
-            return False
+        # # 그러니까 i보다 오른쪽에 있는 것 중 minJump ~ maxJump 사이의 구간에 0인 친구로만 점프가 가능하다 이거구만
+        # if s[-1] == "1":
+        #     return False
 
-        # BFS로 가능
-        visited = set()
-        q = collections.deque([0])
-        max_index = 0 # 요게 중요한 트릭이었네... 
+        # # BFS로 가능
+        # visited = set()
+        # q = collections.deque([0])
+        # max_index = 0 # 요게 중요한 트릭이었네... 
         
-        while q:
-            i = q.popleft()
-            for j in range(max(max_index + 1, i + minJump), min(i + maxJump + 1, len(s))):
-                if s[j] == "0":
-                    q.append(j)
-                    if j == len(s) - 1:
-                        return True
-            max_index = i + maxJump
+        # while q:
+        #     i = q.popleft()
+        #     for j in range(max(max_index + 1, i + minJump), min(i + maxJump + 1, len(s))):
+        #         if s[j] == "0":
+        #             q.append(j)
+        #             if j == len(s) - 1:
+        #                 return True
+        #     max_index = i + maxJump
         
-        return False
+        # return False
+
+        # dp + sliding window
+        dp = [c == "0" for c in s]
+        prev = 0
+
+        for i in range(1, len(s)):
+            prev += 1 if i >= minJump and dp[i - minJump] else 0
+            prev -= 1 if i >= maxJump + 1 and dp[i - maxJump - 1] else 0
+            dp[i] = dp[i] and prev > 0
+        
+        return dp[-1]
