081b53dd65fd3bd228e02d54cc7758f1e1b0ee04
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-01-22 23:11:34 +0900

    Time: 365 ms (83.39%), Space: 55.8 MB (74.59%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -1,26 +1,33 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         # 길이 3 이상, 모든 연속된 숫자들의 차이가 같아야 함. 같은 조합이 여러번 나와도 상관 없고...
-        # 베이스가 길이 2짜리여야 하나? nC2니까 O(n^2)이긴 한데 1000개라서 괜춘
-        # 1개씩 고려한다고 하면 어떻게 하지?
-        # 일단 시작은 0에서 n-3까지만 돌아야 함. 길이가 최소 3이니까
 
         N = len(nums)
 
-        @cache
-        def dfs(last, diff):
-            res = 0
-            for i in range(last + 1, N):
-                if nums[i] - nums[last] == diff:
-                    res += 1 + dfs(i, diff)
-            return res
+        # 이렇게 하면 로직은 맞는데 TLE 뜸. 개선이 필요하다.
+        # @cache
+        # def dfs(last, diff):
+        #     res = 0
+        #     for i in range(last + 1, N):
+        #         if nums[i] - nums[last] == diff:
+        #             res += 1 + dfs(i, diff)
+        #     return res
                 
+        # res = 0
+        # for i in range(N - 2):
+        #     for j in range(i + 1, N - 1):
+        #         res += dfs(j, nums[j] - nums[i])
+        # return res
+
+        # ㅇㅎ.. LIS랑 비슷하게 푸는 거였음
         res = 0
-        for i in range(N - 2):
-            for j in range(i + 1, N - 1):
-                # 여기서 top down dp를 적용할 수 있으려나?
-                # i, j 값을 함수에 넣어줘야 하나? ㄴㄴ
-                # diff랑 start 값만 넣고 pick & not pick 넣으면 되지 않을까?
-                # 아 근데 뺄 대상이 필요함. j는 넣어야겠다.
-                res += dfs(j, nums[j] - nums[i])
+        dp = [{} for _ in range(N)]
+
+        for i in range(N):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                prev_count = dp[j][diff] if diff in dp[j] else 0
+                dp[i][diff] = dp[i].get(diff, 0) + prev_count + 1
+                res += prev_count # 이래야 3개짜리부터 res에 더해짐
+
         return res
