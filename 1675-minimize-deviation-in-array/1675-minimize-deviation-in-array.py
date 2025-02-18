b8ff5408d5808d17fb3c01af66bb29b11c8279cf
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-02-19 00:13:30 +0900

    Time: 1022 ms (50.58%), Space: 24.9 MB (71.84%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -1,14 +1,5 @@
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        # max heap을 사용하면 된다?
-        # 1 -> heap에 2 넣기. min은 1
-        # 2 -> heap에 2 넣기. min은 1
-        # 3 -> heap에 6 넣기. min
-        # 4 -> heap에 4 넣기.
-
-        # 그러면 min max를 보자
-        # 6 4 2 2 순으로 뽑혀 나옴
-        # 첨에 6은 min max가 6인데... 3이 되니까 min 3 max 6
         minimum = float("inf")
         evens = []
         for num in nums:
