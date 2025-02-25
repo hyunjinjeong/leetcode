5d7c8694383eb044b0a39df649b1546484b8fab4
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-02-25 23:10:09 +0900

    Time: 2412 ms (42.09%), Space: 21.1 MB (60.59%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,44 +1,17 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        # 이거 그래프 같은데?
-        # 그래프를 만든 다음에 BFS든 다익스트라든 돌려서 최단거리를 찾으면 됨. 일단 간단하게 BFS로...
-        # N = len(grid)
-
-        # graph = collections.defaultdict(list)
-        # for r in range(1, N):
-        #     for c in range(N):
-        #         for new_col in range(N):
-        #             if new_col != c:
-        #                 graph[(r - 1, c)].append((r, new_col, grid[r][new_col]))
-        
-        # q = collections.deque()
-        # for c in range(N):
-        #     q.append((0, c, grid[0][c]))
-        
-        # res = float("inf")
-        # while q:
-        #     r, c, path_sum = q.popleft()
-        #     if r == N - 1: # end
-        #         res = min(path_sum, res)
-            
-        #     for adj_r, adj_c, num in graph[(r, c)]:
-        #         q.append((adj_r, adj_c, path_sum + num))
-        
-        # return res
-
-        # 방법은 맞는데 MLE 뜸. 더 최적화하면 되려나? DP를 쓰라는데.. DP도 되긴 하겠다
+        # 그래프인가? 그래프를 만든 다음에 BFS든 다익스트라든 돌려서 최단거리를 찾으면 됨. 일단 간단하게 BFS로...
+        # 아 근데 음수가 있어서 안되는구나
         N = len(grid)
 
-        def get_min_in_prev_row(r, c):
-            res = float("inf")
-            for prev_c in range(N):
-                if prev_c == c:
-                    continue
-                res = min(grid[r - 1][prev_c], res)
-            return res
+        # def get_first_two_minimum_indices(row):
+        #     first, second = float("inf"), float("inf")
+        #     for col in range(N):
+
+
 
         for r in range(1, N):
             for c in range(N):
-                grid[r][c] += get_min_in_prev_row(r, c)
+                grid[r][c] += min(grid[r - 1][k] for k in range(N) if k != c)
         
         return min(grid[N - 1])
