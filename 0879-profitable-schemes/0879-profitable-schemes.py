bc980f5039494ed17c6cff38741a05b78cf3d61a
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-03-11 23:52:37 +0900

    Time: 1859 ms (49.63%), Space: 53.2 MB (51.09%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -1,26 +1,36 @@
 class Solution:
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        # 순서는 상관 없음. 0, 1, 2를 하든 2, 1, 0을 하든...
-        # pick & not pick DP? 근데 시간 복잡도가 2^100이라 TLE가 뜰 것 같음
-        # 어떤 선택을 할 때... curr_profit을 알아야 하고 또 curr_members를 알아야 함
-
-        # 1번 예제에서
-        # 0 -> profit 2, members 2: X
-        # 0 1 -> profits 5, members 4: O
-        # 1 -> profit 3, members 2: O
-        # 이런 식인데...
         MOD = 10 ** 9 + 7
 
-        @cache
-        def dfs(i, curr_members, curr_profit):
-            if i == len(group):
-                return 1 if curr_profit >= minProfit else 0
+        # @cache
+        # def dfs(i, curr_members, curr_profit):
+        #     if i == len(group):
+        #         return 1 if curr_profit >= minProfit else 0
 
-            # not pick
-            res = dfs(i + 1, curr_members, curr_profit)
-            if curr_members + group[i] <= n: # pick
-                res += dfs(i + 1, curr_members + group[i], min(curr_profit + profit[i], minProfit))
+        #     # not pick
+        #     res = dfs(i + 1, curr_members, curr_profit)
+        #     if curr_members + group[i] <= n: # pick
+        #         res += dfs(i + 1, curr_members + group[i], min(curr_profit + profit[i], minProfit))
             
-            return res % MOD
+        #     return res % MOD
         
-        return dfs(0, 0, 0)
+        # return dfs(0, 0, 0)
+
+        # bottom-up
+        G = len(group)
+
+        dp = [[[0] * (minProfit + 1) for _ in range(n + 1)] for _ in range(G + 1)]
+        for curr_members in range(n + 1):
+            dp[G][curr_members][minProfit] = 1 # i == len(group)일 때
+        
+        for i in range(G - 1, -1, -1):
+            for curr_members in range(n + 1):
+                for curr_profit in range(minProfit + 1):
+                    key = (i, curr_members, curr_profit)
+                    # not pick
+                    dp[i][curr_members][curr_profit] = dp[i + 1][curr_members][curr_profit]
+                    if curr_members + group[i] <= n: # pick
+                        dp[i][curr_members][curr_profit] += dp[i + 1][curr_members + group[i]][min(curr_profit + profit[i], minProfit)]
+                    dp[i][curr_members][curr_profit] %= MOD
+        
+        return dp[0][0][0]
