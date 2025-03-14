3a7064ce4b1e9380fdab9226c81ba290d642c97a
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-03-14 20:35:09 +0900

    Time: 1525 ms (75.9%), Space: 374 MB (19.88%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
         # 이거 결국 pick & not pick DP네
+        # s에서 빼는게 아니라 빈 문자열에서 쌓아나가는 방식으로 생각하면 됨...
         
         @cache
         def dfs(i, char, count, left):
