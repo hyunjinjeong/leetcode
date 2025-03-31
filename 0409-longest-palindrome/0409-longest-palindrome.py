c6f3f5faf2a29ca9c839a770756131c07bd6d6ab
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-03-31 21:50:29 +0900

    Time: 0 ms (100%), Space: 18.1 MB (7.19%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counter = collections.Counter(s)
+        counter = collections.defaultdict(int)
+        for c in s:
+            counter[c] += 1
         
-        # greedy로 되는구낭..
-        # 일단 n (짝수) 혹은 n-1 (홀수) 씩은 다 들어가고, s보다 길이가 작으면 center에 1개짜리가 들어갈 수 있음
-        answer = 0
-        for c in counter:
-            answer += (counter[c] // 2) * 2
+        res = 0
+        has_odd = False
+        for count in counter.values():
+            if count % 2 == 0:
+                res += count
+            else:
+                res += count - 1
+                has_odd = True
         
-        if answer < len(s):
-            answer += 1
-        
-        return answer
+        return res + 1 if has_odd else res
