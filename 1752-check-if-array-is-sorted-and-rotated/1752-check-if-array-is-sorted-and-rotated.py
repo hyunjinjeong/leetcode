a6a83c19a3df4a7f8c725c1dbf80ab2f1e9197d3
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-02 22:06:13 +0900

    Time: 0 ms (100%), Space: 17.8 MB (57.59%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -2,13 +2,13 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         # nums + nums 해서 non decreasing 길이가 len(nums)가 되면 될 듯?
         N = len(nums)
-        nums = nums + nums
-
-        # 2 1 3 4 2 1 3 4
         
         streak = 0
         prev = 0
-        for num in nums:
+        for i in range(N * 2):
+            index = i % N
+            num = nums[index]
+
             streak = streak + 1 if num >= prev else 1
             if streak == N:
                 return True
