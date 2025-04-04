1db8d7382c670b0f19061f7d96b7fe48ea59e11f
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-04 20:08:48 +0900

    Time: 4 ms (30.15%), Space: 17.8 MB (53.37%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -4,10 +4,4 @@ class Solution:
         for num in nums:
             counter[num] = counter.get(num, 0) + 1
         
-        arr = sorted(counter.items(), key=lambda tup: (tup[1], -tup[0]))
-        
-        res = []
-        for num, count in arr:
-            for _ in range(count):
-                res.append(num)
-        return res
+        return sorted(nums, key=lambda num: (counter[num], -num))
