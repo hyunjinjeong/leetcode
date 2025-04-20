2b27eda0fad13cfbd9acedde44cb8d5e66122837
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-20 14:09:47 +0900

    Time: 167 ms (20.93%), Space: 44.4 MB (10.16%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,36 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # trie 만들기?
+        trie = Trie()
+        for path in folder:
+            trie.insert(path)
+
+        return [path for path in folder if trie.is_root(path)]
+
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.children = {}
+        self.is_path = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node(None)
+
+    def insert(self, path):
+        node = self.root
+        for name in path.split("/")[1:]:
+            if name not in node.children:
+                node.children[name] = Node(name)
+            node = node.children[name]
+        node.is_path = True
+
+    def is_root(self, path):
+        node = self.root
+        for name in path.split("/")[1:-1]:
+            node = node.children[name]
+            if node.is_path:
+                return False
+        return True
