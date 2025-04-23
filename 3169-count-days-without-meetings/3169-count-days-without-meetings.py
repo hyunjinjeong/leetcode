f6d708ee1d7f0e65d5f470a5f8a28e2644694145
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-23 17:33:32 +0900

    Time: 208 ms (31.51%), Space: 52.8 MB (56.05%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.py
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.py
@@ -4,20 +4,15 @@ class Solution:
         # 구하고 나면? 돌면서 days에서 각 인터벌 범위만큼 빼주면 될 듯
 
         meetings.sort()
-        
         prev_start, prev_end = meetings[0]
-        new_meetings = [[prev_start, prev_end]]
-
-        for i in range(1, len(meetings)):
-            start, end = meetings[i]
-            prev_start, prev_end = new_meetings[-1]
-
-            if start <= prev_end:
-                new_meetings[-1][1] = max(end, prev_end)
-            else:
-                new_meetings.append([start, end])
-
         res = days
-        for start, end in new_meetings:
-            res -= end - start + 1
+
+        for start, end in meetings:
+            if start <= prev_end:
+                prev_end = max(end, prev_end)
+            else:
+                res -= prev_end - prev_start + 1
+                prev_start, prev_end = start, end
+        
+        res -= prev_end - prev_start + 1
         return res
