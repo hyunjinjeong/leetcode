586db5542c0c6498111b59124cb29bf590ce1b26
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-27 11:58:27 +0900

    Time: 31 ms (64.71%), Space: 20.4 MB (70.95%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,11 +3,9 @@ class Solution:
         ans = 0
         
         prefix_sum = 0
-        count = {}
+        count = {0: 1}
         for num in nums:
             prefix_sum += num
-            if prefix_sum == k:
-                ans += 1
             if prefix_sum - k in count:
                 ans += count[prefix_sum - k]
             
