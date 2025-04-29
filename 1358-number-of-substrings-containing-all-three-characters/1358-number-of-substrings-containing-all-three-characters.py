005354424e9dfcf778f856581181227aa60812ec
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-29 15:52:09 +0900

    Time: 331 ms (15.62%), Space: 18.1 MB (5.44%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -2,15 +2,15 @@ class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         # sliding window 인데
         # 한번 abc를 가지면 오른쪽 구간은 전부 해당됨. 길이만큼 더하면 될 듯.
-        res = 0
+        total = 0
+        freq = [0, 0, 0]
         
-        counts = {"a": 0, "b": 0, "c": 0}
         left = 0
         for right in range(len(s)):
-            counts[s[right]] += 1
-            while all(counts[c] > 0 for c in counts):
-                res += len(s) - right
-                counts[s[left]] -= 1
+            freq[ord(s[right]) - ord("a")] += 1
+            while all(value > 0 for value in freq):
+                total += len(s) - right
+                freq[ord(s[left]) - ord("a")] -= 1
                 left += 1
 
-        return res
+        return total
