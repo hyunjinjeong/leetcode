ea4da6bffc03754da0c510ea5cdf689e07fa84c5
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-04-30 17:30:27 +0900

    Time: 191 ms (78.63%), Space: 25.7 MB (82.64%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -6,21 +6,46 @@ class Solution:
         # 결과적으로 양 끝이 max 혹은 min으로 유지되면서 윈도우가 이동할 것 같음
         # 그러면 2번째 max나 min은 어떻게 구하지..? heap 같은걸 들고 있어야 하나
         
-        max_heap, min_heap = [], []
+        # max_heap, min_heap = [], []
+        # longest = 0
+
+        # left = 0
+        # for right in range(len(nums)):
+        #     heapq.heappush(max_heap, (-nums[right], right))
+        #     heapq.heappush(min_heap, (nums[right], right))
+            
+        #     while -max_heap[0][0] - min_heap[0][0] > limit:
+        #         left = min(max_heap[0][1], min_heap[0][1]) + 1
+        #         while max_heap[0][1] < left:
+        #             heapq.heappop(max_heap)
+        #         while min_heap[0][1] < left:
+        #             heapq.heappop(min_heap)
+
+        #     longest = max(longest, right - left + 1)
+
+        # return longest
+
+        # monotonic deque를 사용하는 방법이 있다.
+        max_deque, min_deque = collections.deque(), collections.deque()
         longest = 0
 
         left = 0
         for right in range(len(nums)):
-            heapq.heappush(max_heap, (-nums[right], right))
-            heapq.heappush(min_heap, (nums[right], right))
+            while max_deque and max_deque[-1] < nums[right]:
+                max_deque.pop()
+            max_deque.append(nums[right])
+
+            while min_deque and min_deque[-1] > nums[right]:
+                min_deque.pop()
+            min_deque.append(nums[right])
+
+            while max_deque[0] - min_deque[0] > limit:
+                if nums[left] == max_deque[0]:
+                    max_deque.popleft()
+                if nums[left] == min_deque[0]:
+                    min_deque.popleft()
+                left += 1
             
-            while -max_heap[0][0] - min_heap[0][0] > limit:
-                left = min(max_heap[0][1], min_heap[0][1]) + 1
-                while max_heap[0][1] < left:
-                    heapq.heappop(max_heap)
-                while min_heap[0][1] < left:
-                    heapq.heappop(min_heap)
-
             longest = max(longest, right - left + 1)
-
+        
         return longest
