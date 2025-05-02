ecaa377c2459518ad1aa07a013e9050fa03b9dda
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-02 16:33:07 +0900

    Time: 0 ms (100%), Space: 17.9 MB (14%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,13 +1,31 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        # digits는 최대 9개니까 모든 경우의 수를 체크해보면 될 듯? 9C2.
-        max_num = num
+        # # digits는 최대 9개니까 모든 경우의 수를 체크해보면 될 듯? 9C2.
+        # max_num = num
         
-        str_num = list(str(num))
-        for i in range(len(str_num)):
-            for j in range(i + 1, len(str_num)):
-                str_num[i], str_num[j] = str_num[j], str_num[i]
-                max_num = max(max_num, int("".join(str_num)))
-                str_num[i], str_num[j] = str_num[j], str_num[i]
+        # str_num = list(str(num))
+        # for i in range(len(str_num)):
+        #     for j in range(i + 1, len(str_num)):
+        #         str_num[i], str_num[j] = str_num[j], str_num[i]
+        #         max_num = max(max_num, int("".join(str_num)))
+        #         str_num[i], str_num[j] = str_num[j], str_num[i]
         
-        return max_num
+        # return max_num
+        
+        # O(N) 쏠루션
+        num_list = list(str(num))
+
+        max_digit = "0"
+        max_index = -1
+        swap_i, swap_j = -1, -1
+
+        for i in reversed(range(len(num_list))):
+            if num_list[i] > max_digit:
+                max_digit = num_list[i]
+                max_index = i
+            elif num_list[i] < max_digit:
+                swap_i, swap_j = i, max_index
+        
+        num_list[swap_i], num_list[swap_j] = num_list[swap_j], num_list[swap_i]
+        return int("".join(num_list))
+
