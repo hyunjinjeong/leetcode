566e4b061d4d389fbea8173fa049b57aad1cf885
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-06 21:48:23 +0900

    Time: 23 ms (40%), Space: 17.9 MB (36.55%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -2,19 +2,26 @@ class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
         # set bit 개수에 따라서 그룹화를 한 다음
         # 정렬을 한 다음에 정렬 이후의 숫자와 기존 숫자가 같은 그룹에 있는지 확인하기?
-        def get_set_bits(n):
+        # 아 adjacent 끼리만 swap할 수 있구나
+        # 그럼 group들을 나눠서 각 group 별로 정렬한 결과가 전체 정렬인지 보면 될 듯? 아니면 groups[i - 1]의 max가 groups[i]의 min보다 작은지 확인.
+        @cache
+        def set_bits(n):
             count = 0
             while n:
                 count += n & 1
                 n >>= 1
             return count
-
-        num_to_set_bit = {num: get_set_bits(num) for num in nums}
         
-        sorted_nums = sorted(nums)
-        for i in range(len(nums)):
-            num, sorted_num = nums[i], sorted_nums[i]
-            if num_to_set_bit[num] != num_to_set_bit[sorted_num]:
+        groups = []
+        index = -1
+        for num in nums:
+            if index == -1 or set_bits(num) != set_bits(groups[index][0]):
+                groups.append([])
+                index += 1
+            groups[index].append(num)
+        
+        for i in range(1, len(groups)):
+            if max(groups[i - 1]) > min(groups[i]):
                 return False
 
         return True
