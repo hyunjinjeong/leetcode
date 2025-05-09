265cdef57c5087bb69289063440ccb17fd618ebb
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-09 15:31:35 +0900

    Time: 137 ms (70.78%), Space: 28.6 MB (52.75%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -12,12 +12,9 @@ class Solution:
         nums.sort()
 
         count = 0
-
-        prev = nums[0]
         for i in range(1, len(nums)):
-            if nums[i] <= prev:
-                count += prev - nums[i] + 1
-                nums[i] = prev + 1
-            prev = nums[i]
+            if nums[i] <= nums[i - 1]:
+                count += nums[i - 1] - nums[i] + 1
+                nums[i] = nums[i - 1] + 1
         
         return count
