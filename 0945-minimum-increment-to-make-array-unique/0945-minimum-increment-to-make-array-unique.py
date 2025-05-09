df70bbbc53aae06c46adfcff418face08c88bee5
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-09 15:36:12 +0900

    Time: 227 ms (10.47%), Space: 38.7 MB (6.11%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -9,12 +9,23 @@ class Solution:
         # 1 1 3 3 3 7
         # 1 2 3 3 3 7 / 1
         # 1 2 3 4 3 3
-        nums.sort()
+        # nums.sort()
+
+        # count = 0
+        # for i in range(1, len(nums)):
+        #     if nums[i] <= nums[i - 1]:
+        #         count += nums[i - 1] - nums[i] + 1
+        #         nums[i] = nums[i - 1] + 1
+        
+        # return count
+        # 이런 것도 되네
+        counter = collections.Counter(nums)
 
         count = 0
-        for i in range(1, len(nums)):
-            if nums[i] <= nums[i - 1]:
-                count += nums[i - 1] - nums[i] + 1
-                nums[i] = nums[i - 1] + 1
-        
+        for num in range(len(nums) + max(nums)):
+            if counter[num] >= 2:
+                extra = counter[num] - 1
+                counter[num + 1] += extra
+                count += extra
+
         return count
