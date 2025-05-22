74fed3479eee55949bad9c19189fb4eaaf852b58
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-22 15:11:20 +0900

    Time: 104 ms (23.46%), Space: 17.7 MB (92.02%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,14 +1,23 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        # c까지 제곱해서 나오는 수를 저장하고 숫자를 돌면서 c - num^2가 있는지 확인하면 된다.
-        square_nums = set()
+        # # c까지 제곱해서 나오는 수를 저장하고 숫자를 돌면서 c - num^2가 있는지 확인하면 된다.
+        # square_nums = set()
         
+        # num = 0
+        # while num * num <= c:
+        #     square_num = num * num
+        #     square_nums.add(square_num)
+        #     if c - square_num in square_nums:
+        #         return True
+        #     num += 1
+
+        # return False
+
+        # 저장할 필요도 없구나
         num = 0
         while num * num <= c:
-            square_num = num * num
-            square_nums.add(square_num)
-            if c - square_num in square_nums:
+            diff = (c - num * num) ** 0.5
+            if diff == int(diff):
                 return True
             num += 1
-
         return False
