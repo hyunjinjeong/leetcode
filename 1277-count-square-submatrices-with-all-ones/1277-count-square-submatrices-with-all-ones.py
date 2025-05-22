00bdaeea17b2ee4c4bf0a1ea53902ba198f6d823
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-22 15:04:53 +0900

    Time: 45 ms (96.58%), Space: 20.1 MB (97.34%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -5,17 +5,34 @@ class Solution:
 
         res = 0
 
-        dp = [[0] * (N + 1) for _ in range(M + 1)]
+        # dp = [[0] * (N) for _ in range(M)]
+        # for i in range(M):
+        #     for j in range(N):
+        #         if matrix[i][j] == 0:
+        #             continue
+                
+        #         up = dp[i - 1][j] if i > 0 else 0
+        #         left = dp[i][j - 1] if j > 0 else 0
+        #         up_left = dp[i - 1][j - 1] if i > 0 and j > 0 else 0
+                
+        #         dp[i][j] = min(up, left, up_left) + 1
+        #         res += dp[i][j]
+        
+        # return res
+
+        dp = [0] * N
         for i in range(M):
+            next_dp = [0] * N
             for j in range(N):
                 if matrix[i][j] == 0:
                     continue
                 
-                up = dp[i - 1][j] if i > 0 else 0
-                left = dp[i][j - 1] if j > 0 else 0
-                up_left = dp[i - 1][j - 1] if i > 0 and j > 0 else 0
+                up = dp[j]
+                left = next_dp[j - 1] if j > 0 else 0
+                up_left = dp[j - 1] if j > 0 else 0
                 
-                dp[i][j] = min(up, left, up_left) + 1
-                res += dp[i][j]
+                next_dp[j] = min(up, left, up_left) + 1
+                res += next_dp[j]
+            dp = next_dp
         
         return res
