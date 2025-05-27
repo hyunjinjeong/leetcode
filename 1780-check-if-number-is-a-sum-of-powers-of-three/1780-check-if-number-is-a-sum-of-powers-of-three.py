9f8f6673c0117d58dda8a79200653d3722634e04
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-27 17:18:00 +0900

    Time: 0 ms (100%), Space: 17.8 MB (62.64%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        # 3의 제곱수들을 구해놓고 큰거부터 빼면서 greedy하게 될 것 같은데
+        # 3^3 = 3 * 3^2 = 3 * 9 이런 식이니까..
+        nums = []
+        
+        num = 1
+        while num <= n:
+            nums.append(num)
+            num *= 3
+        
+        curr = n
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] <= curr:
+                curr -= nums[i]
+            
+            if curr == 0:
+                return True
+        
+        return False
