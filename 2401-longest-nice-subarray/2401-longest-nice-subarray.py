302c7cf4a88ef4e2801e635cb6188004c095ac35
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-27 16:57:48 +0900

    Time: 83 ms (87.88%), Space: 32.8 MB (46.17%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -7,25 +7,16 @@ class Solution:
         # 0이 깨졌다는 건 nums[right]랑 nums[left]에 1이 겹치는 비트가 있다는 뜻
         # 그러면 nums[left], nums[left + 1], .. 오면서 연산으로 알 방법이 없지 않나?
         # 그러면 비트별로 1 카운트를 세기? 비트 하나당 카운트가 2 이상이면 조건이 깨지는 거임.
-
-        def add_to_bit_counts(num, is_plus):
-            bit = 0
-            while num:
-                if num & 1:
-                    bit_counts[bit] += (1 if is_plus else -1)
-                num >>= 1
-                bit += 1
-        
         longest = 1
-        bit_counts = [0] * 32
+        curr_and = nums[0]
 
         left = 0
         for right in range(len(nums)):
-            add_to_bit_counts(nums[right], is_plus=True)
-            while left < right and any([count > 1 for count in bit_counts]):
-                add_to_bit_counts(nums[left], is_plus=False)
+            while left < right and curr_and & nums[right] != 0:
+                curr_and ^= nums[left]
                 left += 1
             
+            curr_and |= nums[right]
             longest = max(longest, right - left + 1)
 
         return longest
