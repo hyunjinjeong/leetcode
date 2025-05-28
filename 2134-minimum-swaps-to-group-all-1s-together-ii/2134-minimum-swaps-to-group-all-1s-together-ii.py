4d35f105d20585ae6e10e910e5b47bbbb8c4a420
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-05-28 12:31:26 +0900

    Time: 215 ms (29.43%), Space: 21.2 MB (92.92%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -2,25 +2,22 @@ class Solution:
     def minSwaps(self, nums: List[int]) -> int:
         # circular를 고려하지 않으면 어떻게 풀지? fixed window로 sliding 하면 될 듯
         # circular는? 그냥 nums + nums 배열로 계산하면 된다
-        one_count, zero_count = 0, 0
-        for num in nums:
-            if num == 1:
-                one_count += 1
-            else:
-                zero_count += 1
+        N = len(nums)
             
-        window_zero_count = 0
-        for i in range(one_count):
-            if nums[i] == 0:
-                window_zero_count += 1
+        one_count = nums.count(1)
+        res = one_count
         
-        res = window_zero_count
-        for i in range(one_count, len(nums) * 2):
-            if nums[(i - one_count) % len(nums)] == 0: # prev_left
-                window_zero_count -= 1
-            if nums[i % len(nums)] == 0:
+        window_zero_count = 0
+        left = 0
+        for right in range(N * 2):
+            if nums[right % N] == 0:
                 window_zero_count += 1
             
-            res = min(res, window_zero_count)
+            if right - left + 1 > one_count:
+                if nums[left % N] == 0: # lower bound
+                    window_zero_count -= 1
+                
+                res = min(res, window_zero_count)
+                left += 1
 
         return res
