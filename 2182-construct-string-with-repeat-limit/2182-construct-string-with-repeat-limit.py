87e9d8d2b2ab92ddf5f8ffa11b01d366ddcd5b8f
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-06-01 15:35:15 +0900

    Time: 203 ms (81.89%), Space: 19.7 MB (54.02%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -10,17 +10,16 @@ class Solution:
             ord_c, count = heapq.heappop(heap)
             c = chr(-1 * ord_c)
             
-            if count <= repeatLimit:
-                res.append(c * count)
-            else:
-                res.append(c * repeatLimit)
-                if heap:
-                    next_ord_c, next_count = heapq.heappop(heap)
-                    next_c = chr(-1 * next_ord_c)
-                    res.append(next_c)
-                    if next_count > 1:
-                        heapq.heappush(heap, (next_ord_c, count - 1))
+            use_count = min(count, repeatLimit)
+            res.append(c * use_count)
 
-                heapq.heappush(heap, (ord_c, count - repeatLimit))
+            if count > use_count and heap:
+                next_ord_c, next_count = heapq.heappop(heap)
+                next_c = chr(-1 * next_ord_c)
+                res.append(next_c)
+                if next_count > 1:
+                    heapq.heappush(heap, (next_ord_c, next_count - 1))
+
+                heapq.heappush(heap, (ord_c, count - repeatLimit))                
 
         return "".join(res)
