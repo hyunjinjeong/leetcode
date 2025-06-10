5be1a3cf49806276fb08a2050987a106d87a4ffc
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-06-10 14:54:05 +0900

    Time: 145 ms (13.17%), Space: 18.3 MB (71.5%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -3,22 +3,22 @@ class Solution:
         if len(s) % 2 == 1:
             return False
         
-        max_open_parentheses, min_open_parentheses = 0, 0
+        max_open_parenthesis, min_open_parenthesis = 0, 0
         for i in range(len(s)):
             if locked[i] == "0":
-                max_open_parentheses += 1 # "("
-                min_open_parentheses -= 1 # ")"
+                max_open_parenthesis += 1 # "("
+                min_open_parenthesis -= 1 # ")"
             else:
                 if s[i] == "(":
-                    max_open_parentheses += 1
-                    min_open_parentheses += 1
+                    max_open_parenthesis += 1
+                    min_open_parenthesis += 1
                 else:
-                    max_open_parentheses -= 1
-                    min_open_parentheses -= 1
+                    max_open_parenthesis -= 1
+                    min_open_parenthesis -= 1
 
-            if max_open_parentheses < 0:
+            if max_open_parenthesis < 0:
                 return False
             
-            min_open_parentheses = max(min_open_parentheses, 0)
+            min_open_parenthesis = max(min_open_parenthesis, 0)
         
-        return min_open_parentheses == 0
+        return min_open_parenthesis == 0
