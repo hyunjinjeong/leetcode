320c153c25e82e398c770848432748fb532c5411
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-06-13 10:42:20 +0900

    Time: 28 ms (36.67%), Space: 22.9 MB (81.02%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -8,17 +8,31 @@ class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         # 흠 3부터는 어떻게 하지
         # pair가 되는 노드를 같이 넘겨주자
-        # 좌우를 한번씩 들려서 초기화가 되는데 어떻게 한번만 넣지
 
-        def dfs(node1, node2, level):
-            if not node1:
-                return
+        # def dfs(node1, node2, level):
+        #     if not node1:
+        #         return
             
+        #     if level % 2 == 1:
+        #         node1.val, node2.val = node2.val, node1.val
+
+        #     dfs(node1.left, node2.right, level + 1)
+        #     dfs(node1.right, node2.left, level + 1)
+
+        # dfs(root.left, root.right, 1)
+        # return root
+
+        q = collections.deque()
+        q.append((root.left, root.right, 1))
+
+        while q:
+            node1, node2, level = q.popleft()
+            if not node1:
+                continue
             if level % 2 == 1:
                 node1.val, node2.val = node2.val, node1.val
 
-            dfs(node1.left, node2.right, level + 1)
-            dfs(node1.right, node2.left, level + 1)
-
-        dfs(root.left, root.right, 1)
+            q.append((node1.left, node2.right, level + 1))
+            q.append((node1.right, node2.left, level + 1))
+        
         return root
