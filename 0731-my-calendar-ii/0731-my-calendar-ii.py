c34555f102ab69c510e9d0e399180b62492088c0
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-06-14 15:16:23 +0900

    Time: 2071 ms (8.02%), Space: 18.9 MB (9.49%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,23 +1,36 @@
 class MyCalendarTwo:
-    # line sweep 이군
+    # line sweep 이라는게 있네
     def __init__(self):
-        self.bookings = []
-        self.overlap_bookings = []
+        # self.bookings = []
+        # self.overlap_bookings = []
+        self.events = collections.defaultdict(int)
 
     def book(self, startTime: int, endTime: int) -> bool:
-        for s1, e1 in self.overlap_bookings:
-            if self.does_overlap(s1, e1, startTime, endTime):
+        # for s1, e1 in self.overlap_bookings:
+        #     if self.does_overlap(s1, e1, startTime, endTime):
+        #         return False
+        
+        # for s1, e1 in self.bookings:
+        #     if self.does_overlap(s1, e1, startTime, endTime):
+        #         self.overlap_bookings.append((max(s1, startTime), min(e1, endTime)))
+
+        # self.bookings.append((startTime, endTime))
+        # return True
+        self.events[startTime] += 1
+        self.events[endTime] -= 1
+
+        count = 0
+        for event in sorted(self.events):
+            count += self.events[event]
+            if count > 2:
+                self.events[startTime] -= 1
+                self.events[endTime] += 1
                 return False
         
-        for s1, e1 in self.bookings:
-            if self.does_overlap(s1, e1, startTime, endTime):
-                self.overlap_bookings.append((max(s1, startTime), min(e1, endTime)))
-
-        self.bookings.append((startTime, endTime))
         return True
     
-    def does_overlap(self, s1, e1, s2, e2):
-        return max(s1, s2) < min(e1, e2)
+    # def does_overlap(self, s1, e1, s2, e2):
+    #     return max(s1, s2) < min(e1, e2)
 
 # Your MyCalendarTwo object will be instantiated and called as such:
 # obj = MyCalendarTwo()
