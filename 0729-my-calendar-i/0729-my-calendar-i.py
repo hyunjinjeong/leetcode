08137767807e3bf8389a828dbbbc7e9c1e925455
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-06-14 16:22:50 +0900

    Time: 1532 ms (5.03%), Space: 18.7 MB (72.82%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,15 +1,20 @@
 class MyCalendar:
 
     def __init__(self):
-        self.bookings = []
+        self.events = sortedcontainers.SortedDict()
 
     def book(self, startTime: int, endTime: int) -> bool:
-        for booking in self.bookings:
-            b1, b2 = sorted([booking, (startTime, endTime)])
-            if b1[1] > b2[0]:
+        self.events[startTime] = self.events.get(startTime, 0) + 1
+        self.events[endTime] = self.events.get(endTime, 0) - 1
+
+        count = 0
+        for event in self.events:
+            count += self.events[event]
+            if count > 1:
+                self.events[startTime] -= 1
+                self.events[endTime] += 1
                 return False
-        
-        self.bookings.append((startTime, endTime))
+
         return True
 
 
