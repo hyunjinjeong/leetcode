f82c0e04f29931263c9ad0b46beee30c8efe9d65
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-06-17 15:08:15 +0900

    Time: 849 ms (5.06%), Space: 29.8 MB (69.27%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        # 흠 [4, 7, 5], k=4 같은 경우엔 4가 아니라 3이구나
+        # 캔디 갯수 C에 대해, candies[i] // C의 값만큼 n명에게 C개를 나눠줄 수 있게 됨
+        # 아 그르면 C의 최댓값을 찾는 문제겠네... binary search다
+
+        def can_allocate(c):
+            if not c:
+                return True
+
+            allocated = 0
+            for candy in candies:
+                allocated += candy // c
+                if allocated >= k:
+                    return True
+
+            return False
+        
+        if len(candies) == 1:
+            return candies[0] // k
+        
+        lo, hi = 0, sum(candies)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+
+            if can_allocate(mid):
+                lo = mid + 1
+            else:
+                hi = mid
+        
+        return lo - 1
