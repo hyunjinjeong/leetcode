b5c74f2696949eb94a9b557fe7d166cf8c43712c
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-07-01 18:20:53 +0900

    Time: 194 ms (69.84%), Space: 19.4 MB (18.45%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        # brute force는 각 city에서 BFS를 돌려서 reachableCities 수를 구하면 될 것 같은데
+        # 아 BFS로 돌다 보면 더 뒤에 있는데 distance가 짧은 애들이 스킵될 수 있음
+        # 그럼 dijkstra로...
+        def get_counts(start):
+            heap = [(0, start)]
+            dist = [float("inf") for _ in range(n)]
+            dist[start] = 0
+
+            visited = [False for _ in range(n)]
+            count = -1 # 자기자신 제외
+            while heap:
+                curr_dist, city = heapq.heappop(heap)
+                
+                if not visited[city]:
+                    count += 1
+                visited[city] = True
+                
+                for adj, adj_dist in graph[city]:
+                    next_dist = curr_dist + adj_dist
+                    if next_dist <= distanceThreshold and next_dist <= dist[adj]:
+                        heapq.heappush(heap, (next_dist, adj))
+                        dist[adj] = next_dist
+            
+            return count
+
+        
+        graph = {i: [] for i in range(n)}
+        for start, end, distance in edges:
+            graph[start].append((end, distance))
+            graph[end].append((start, distance))
+        
+        lowest_city = 0
+        lowest_count = float("inf")
+        for city in range(n):
+            print("city", city)
+            count = get_counts(city)
+            if count > lowest_count:
+                continue
+            lowest_city = city
+            lowest_count = count
+        
+        return lowest_city
