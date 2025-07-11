11a80f29d512cffe9e0653a948b5b86d11533688
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-07-12 08:00:48 +0900

    Time: 0 ms (100%), Space: 18.1 MB (51.57%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,16 +2,31 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         M, N = len(image), len(image[0])
         
-        def dfs(r, c):
-            original_color = image[r][c]
-            if original_color == color:
-                return
+        # def dfs(r, c):
+        #     original_color = image[r][c]
+        #     if original_color == color:
+        #         return
             
-            image[r][c] = color
+        #     image[r][c] = color
 
+        #     for adj_r, adj_c in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
+        #         if 0 <= adj_r < M and 0 <= adj_c < N and image[adj_r][adj_c] == original_color:
+        #             dfs(adj_r, adj_c)
+        
+        # dfs(sr, sc)
+        
+        original_color = image[sr][sc]
+        if original_color == color:
+            return image
+
+        q = collections.deque([(sr, sc)])
+        image[sr][sc] = color
+
+        while q:
+            r, c = q.popleft()
             for adj_r, adj_c in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
                 if 0 <= adj_r < M and 0 <= adj_c < N and image[adj_r][adj_c] == original_color:
-                    dfs(adj_r, adj_c)
-        
-        dfs(sr, sc)
+                    image[adj_r][adj_c] = color
+                    q.append((adj_r, adj_c))
+            
         return image
