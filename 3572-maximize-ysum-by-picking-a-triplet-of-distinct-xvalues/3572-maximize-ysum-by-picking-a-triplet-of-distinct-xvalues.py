9e1bffb05f95df645193e8b027ece9884ba03ce9
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-07-20 13:38:39 +0900

    Time: 97 ms (89.08%), Space: 43.2 MB (68.81%) - LeetHub

---
+++ 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSumDistinctTriplet(self, x: List[int], y: List[int]) -> int:
+        # x[i]를 그룹으로 묶어서 y[i]가 가장 높은 아이템만 남기면 되지 않을까
+        # 그러면 그냥 3개 뽑아서 max를 구하면 됨
+        # 1 2 1 3 2 / 5 3 4 6 2 의 경우
+        # 1 2 3 / 5 3 6
+        N = len(x)
+
+        group = {}
+        for i in range(N):
+            prev_y_i = group.get(x[i], 0)
+            if y[i] > prev_y_i:
+                group[x[i]] = y[i]
+        
+        if len(group) < 3:
+            return -1
+
+        return sum(sorted(group.values(), reverse=True)[:3])
