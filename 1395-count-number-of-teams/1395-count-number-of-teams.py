9fb16164be4524ad0457bfca13d8c998cc9c387f
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-07-20 14:55:08 +0900

    Time: 668 ms (18.81%), Space: 17.9 MB (72.36%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -10,30 +10,63 @@ class Solution:
         # 단조증가랑 단조감소 각각 구해서 더하기
         # 이제 이걸 dp로 바꿔야 하는데
 
-        @cache
-        def get_monotonic_increasing_count(start, team_size):
-            if team_size == 3:
-                return 1
+        # @cache
+        # def get_monotonic_increasing_count(start, team_size):
+        #     if team_size == 3:
+        #         return 1
             
-            count = 0
-            for i in range(start + 1, len(rating)):
-                if rating[i] > rating[start]:
-                    count += get_monotonic_increasing_count(i, team_size + 1)
-            return count
+        #     count = 0
+        #     for i in range(start + 1, len(rating)):
+        #         if rating[i] > rating[start]:
+        #             count += get_monotonic_increasing_count(i, team_size + 1)
+        #     return count
         
-        @cache
-        def get_monotonic_decreasing_count(start, team_size):
-            if team_size == 3:
-                return 1
+        # @cache
+        # def get_monotonic_decreasing_count(start, team_size):
+        #     if team_size == 3:
+        #         return 1
             
-            count = 0
-            for i in range(start + 1, len(rating)):
-                if rating[i] < rating[start]:
-                    count += get_monotonic_decreasing_count(i, team_size + 1)
-            return count
+        #     count = 0
+        #     for i in range(start + 1, len(rating)):
+        #         if rating[i] < rating[start]:
+        #             count += get_monotonic_decreasing_count(i, team_size + 1)
+        #     return count
         
+        # total_count = 0
+        # for i in range(len(rating)):
+        #     total_count += get_monotonic_increasing_count(i, 1) + get_monotonic_decreasing_count(i, 1)
+        
+        # return total_count
+
+        # 중간 원소를 기준으로 좌우를 구하는 식으로 최적화할 수 있음
         total_count = 0
-        for i in range(len(rating)):
-            total_count += get_monotonic_increasing_count(i, 1) + get_monotonic_decreasing_count(i, 1)
+
+        # increasing count
+        for mid in range(len(rating)):
+            left_count, right_count = 0, 0
+
+            for left in range(0, mid):
+                if rating[left] < rating[mid]:
+                    left_count += 1
+            
+            for right in range(mid + 1, len(rating)):
+                if rating[right] > rating[mid]:
+                    right_count += 1
+            
+            total_count += left_count * right_count
         
+        # decreasing count
+        for mid in range(len(rating)):
+            left_count, right_count = 0, 0
+
+            for left in range(0, mid):
+                if rating[left] > rating[mid]:
+                    left_count += 1
+            
+            for right in range(mid + 1, len(rating)):
+                if rating[right] < rating[mid]:
+                    right_count += 1
+            
+            total_count += left_count * right_count
+
         return total_count
