a49af31a47a60cdb3e86b2e2ce6ff072ad7bfe16
Hyunjin Jeong <hyunjin.dev@gmail.com>
2025-08-16 16:03:55 +0900

    Time: 9 ms (9.72%), Space: 18.1 MB (77.3%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # BST 정의는? left.val < curr.val < right.val
+        # O(N)은 쉬움. BST에서는 inorder가 정렬되어 있으니까 쭉 돌고 난 다음에 정렬이 안된거 찾으면 됨
+        # 근데 in place로 바꿔야 함. 어떻게 swap해주지?
+        # value가 unique 한가? no. 그러면 배열에다가 node를 저장하면 될 듯?
+
+        def get_inorder_array(node):
+            if node is None:
+                return []
+            
+            return get_inorder_array(node.left) + [node] + get_inorder_array(node.right)
+        
+        def get_sorted_array(arr):
+            arr.val
+        
+        inorder_array = get_inorder_array(root)
+        sorted_array = sorted(inorder_array, key=lambda item: item.val)
+
+        swap_candidates = []
+        for i in range(len(inorder_array)):
+            if inorder_array[i].val != sorted_array[i].val:
+                swap_candidates.append(inorder_array[i])
+        
+        # 정확히 2개 있어야 함!
+        swap_candidates[0].val, swap_candidates[1].val = swap_candidates[1].val, swap_candidates[0].val
